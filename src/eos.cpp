596f3221b8d2d193a87a7f2a6a6a8f6a31cd1bae
Sven Hesse <drmccoy@users.sourceforge.net>
2010-03-29 08:17:42 +0200

    Small ResourceManager test

--- src/eos.cpp
+++ src/eos.cpp
@@ -18,6 +18,7 @@
 
 #include "aurora/keyfile.h"
 #include "aurora/biffile.h"
+#include "aurora/resman.h"
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
@@ -37,52 +38,11 @@ int main(int argc, char **argv) {
 
 	atexit(SDL_Quit);
 
-	Common::FileList files;
-	files.addDirectory(argv[1], -1);
+	Aurora::ResourceManager resMan;
 
-	Common::FileList keyFiles, bifFiles;
-	files.getSubList(".*\\.key", keyFiles, true);
-	files.getSubList(".*\\.bif", bifFiles, true);
+	bool success = resMan.registerDataBaseDir(argv[1]);
 
-	if (keyFiles.isEmpty() || bifFiles.isEmpty()) {
-		warning("No KEY or BIF files found. Path most probably does not contain an Aurora game.");
-		return 0;
-	}
-
-	warning("Opening \"%s\"", keyFiles.begin()->c_str());
-	Common::SeekableReadStream *keyStream = keyFiles.openFile(*keyFiles.begin());
-	if (keyStream) {
-		Aurora::KeyFile key;
-
-		bool success = key.load(*keyStream);
-
-		const Aurora::KeyFile::BifList &bifs     = key.getBifs();
-		const Aurora::KeyFile::ResourceList &res = key.getResources();
-
-		warning("Success? %d - # bif: %d - # res: %d", success, (int) bifs.size(), (int) res.size());
-
-	} else {
-		warning("Fail :(");
-	}
-
-	delete keyStream;
-
-	warning("Opening \"%s\"", bifFiles.begin()->c_str());
-	Common::SeekableReadStream *bifStream = bifFiles.openFile(*bifFiles.begin());
-	if (bifStream) {
-		Aurora::BifFile bif;
-
-		bool success = bif.load(*bifStream);
-
-		const Aurora::BifFile::ResourceList &res = bif.getResources();
-
-		warning("Success? %d - # res: %d", success, (int) res.size());
-
-	} else {
-		warning("Fail :(");
-	}
-
-	delete bifStream;
+	warning("Useful base dir? %s - %d keys", success ? "Yes" : "No", (int) resMan.getKeyList().size());
 
 	return 0;
 }
