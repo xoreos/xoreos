a54a25e54f1df259e2238595d703c8b0d25fd023
Sven Hesse <drmccoy@users.sourceforge.net>
2010-03-29 03:19:29 +0200

    Adding a ResourceManager-stub

---
+++ src/aurora/resman.h
@@ -0,0 +1,27 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ * Copyright (c) 2010 Sven Hesse (DrMcCoy), Matthew Hoops (clone2727)
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ *
+ * This file is part of eos and is distributed under the terms of
+ * the GNU General Public Licence. See COPYING for more informations.
+ */
+
+#ifndef AURORA_RESMAN_H
+#define AURORA_RESMAN_H
+
+#include "common/types.h"
+
+namespace Common {
+	class SeekableReadStream;
+}
+
+namespace Aurora {
+
+class ResourceManager {
+};
+
+} // End of namespace Aurora
+
+#endif // AURORA_RESMAN_H
