15b9b15b67c7c82e67c4daebe73a4e1ae891ce7d
Sven Hesse <drmccoy@users.sourceforge.net>
2010-03-30 10:08:54 +0200

    Implemented flat parsing of the GFF struct array

--- src/aurora/gfffile.cpp
+++ src/aurora/gfffile.cpp
@@ -9,11 +9,63 @@
  */
 
 #include "common/stream.h"
+#include "common/util.h"
 
 #include "aurora/gfffile.h"
+#include "aurora/aurorafile.h"
 #include "aurora/locstring.h"
 
+static const uint32 kVersion32 = MKID_BE('V3.2');
+static const uint32 kVersion33 = MKID_BE('V3.3'); // Found in The Witcher, different language table
+
 namespace Aurora {
+
+GFFHeader::GFFHeader() {
+	clear();
+}
+
+void GFFHeader::clear() {
+	id                 = 0;
+	version            = 0;
+	structOffset       = 0;
+	structCount        = 0;
+	fieldOffset        = 0;
+	fieldCount         = 0;
+	labelOffset        = 0;
+	labelCount         = 0;
+	fieldDataOffset    = 0;
+	fieldDataCount     = 0;
+	fieldIndicesOffset = 0;
+	fieldIndicesCount  = 0;
+	listIndicesOffset  = 0;
+	listIndicesCount   = 0;
+}
+
+bool GFFHeader::read(Common::SeekableReadStream &gff) {
+	id      = gff.readUint32BE();
+	version = gff.readUint32BE();
+
+	if ((version != kVersion32) && (version != kVersion33)) {
+		warning("GFFHeader::read(): Unsupported file version");
+		return false;
+	}
+
+	structOffset       = gff.readUint32LE();
+	structCount        = gff.readUint32LE();
+	fieldOffset        = gff.readUint32LE();
+	fieldCount         = gff.readUint32LE();
+	labelOffset        = gff.readUint32LE();
+	labelCount         = gff.readUint32LE();
+	fieldDataOffset    = gff.readUint32LE();
+	fieldDataCount     = gff.readUint32LE();
+	fieldIndicesOffset = gff.readUint32LE();
+	fieldIndicesCount  = gff.readUint32LE();
+	listIndicesOffset  = gff.readUint32LE();
+	listIndicesCount   = gff.readUint32LE();
+
+	return true;
+}
+
 
 GFFField::GFFField() {
 	_gffType = kGFFTypeNone;
@@ -32,46 +84,34 @@ void GFFField::clear() {
 		delete _value.typeString;
 	else if (_type == kTypeLocString)
 		delete _value.typeLocString;
-	else if (_type == kTypeStruct) {
-
-		if (_value.typeStruct)
-			for (GFFStruct::iterator it = _value.typeStruct->begin(); it != _value.typeStruct->end(); ++it)
-				delete *it;
-		delete _value.typeStruct;
-
-	} else if (_type == kTypeList) {
-
-		if (_value.typeList)
-			for (GFFList::iterator it1 = _value.typeList->begin(); it1 != _value.typeList->end(); ++it1)
-				for (GFFStruct::iterator it2 = it1->begin(); it2 != it1->end(); ++it2)
-					delete *it2;
-		delete _value.typeList;
-
-	}
+	else if (_type == kTypeData)
+		delete[] _value.typeData;
 
 	_gffType = kGFFTypeNone;
 	_type    = kTypeNone;
 
-	_name.clear();
+	_dataSize = 0;
+
+	_label.clear();
 }
 
 GFFField::Type GFFField::getType() const {
 	return _type;
 }
 
-const std::string &GFFField::getName() const {
-	return _name;
+const std::string &GFFField::getLabel() const {
+	return _label;
 }
 
 char GFFField::getChar() const {
 	return (char) _value.typeInt;
 }
 
-uint64 GFFField::getUnsignedInteger() const {
+uint64 GFFField::getUint() const {
 	return (uint64) _value.typeInt;
 }
 
-int64 GFFField::getSignedInteger() const {
+int64 GFFField::getSint() const {
 	return (int64) _value.typeInt;
 }
 
@@ -87,12 +127,396 @@ const LocString &GFFField::getLocString() const {
 	return *_value.typeLocString;
 }
 
-const GFFField::GFFStruct &GFFField::getStruct() const {
-	return *_value.typeStruct;
+uint32 GFFField::getDataSize() const {
+	return _dataSize;
 }
 
-const GFFField::GFFList &GFFField::getList() const {
-	return *_value.typeList;
+const float *GFFField::getVector() const {
+	return _value.typeVector;
+}
+
+const byte *GFFField::getData() const {
+	return (const byte *) *_value.typeData;
+}
+
+const uint32 GFFField::getIndex() const {
+	return _value.typeIndex;
+}
+
+bool GFFField::read(Common::SeekableReadStream &gff, const GFFHeader &header) {
+	clear();
+
+	_gffType = (GFFType) gff.readUint32LE();
+
+	uint32 labelIndex = gff.readUint32LE();
+	uint32 data       = gff.readUint32LE();
+
+	_type = toType(_gffType);
+	if (_type == kTypeNone) {
+		warning("GFFField::read(): Unknown field type %d", _gffType);
+		return false;
+	}
+
+	if (labelIndex >= header.labelCount) {
+		warning("GFFField::read(): Label index out of range (%d/%d)", labelIndex, header.labelCount);
+		return false;
+	}
+
+	_label = AuroraFile::readRawString(gff, 16, header.labelOffset + labelIndex * 16);
+
+	if (!convertData(gff, header, data))
+		return false;
+
+	return true;
+}
+
+bool GFFField::convertData(Common::SeekableReadStream &gff, const GFFHeader &header, uint32 data) {
+	switch (_gffType) {
+		case kGFFTypeChar:
+			_value.typeInt = (uint64) ((char) data);
+			break;
+
+		case kGFFTypeByte:
+			_value.typeInt = (uint64) ((byte) data);
+			break;
+
+		case kGFFTypeUint16:
+			_value.typeInt = (uint64) ((uint16) data);
+			break;
+
+		case kGFFTypeUint32:
+			_value.typeInt = (uint64) ((uint32) data);
+			break;
+
+		case kGFFTypeUint64:
+			if (!readUint64(gff, header, data))
+				return false;
+			break;
+
+		case kGFFTypeSint16:
+			_value.typeInt = (uint64) ((int64) ((int16) ((uint16) data)));
+			break;
+
+		case kGFFTypeSint32:
+			_value.typeInt = (uint64) ((int64) ((int32) ((uint32) data)));
+			break;
+
+		case kGFFTypeSint64:
+			if (!readSint64(gff, header, data))
+				return false;
+			break;
+
+		case kGFFTypeFloat:
+			_value.typeDouble = (double) AuroraFile::convertFloat(data);
+			break;
+
+		case kGFFTypeDouble:
+			if (!readDouble(gff, header, data))
+				return false;
+			break;
+
+		case kGFFTypeExoString:
+			if (!readExoString(gff, header, data))
+				return false;
+			break;
+
+		case kGFFTypeResRef:
+			if (!readResRef(gff, header, data))
+				return false;
+			break;
+
+		case kGFFTypeLocString:
+			if (!readLocString(gff, header, data))
+				return false;
+			break;
+
+		case kGFFTypeVoid:
+			if (!readVoid(gff, header, data))
+				return false;
+			break;
+
+		case kGFFTypeStruct:
+			_value.typeIndex = data;
+			break;
+
+		case kGFFTypeList:
+			_value.typeIndex = data;
+			break;
+
+		case kGFFTypeOrientation:
+			warning("TODO: kGFFTypeOrientation");
+			break;
+
+		case kGFFTypeVector:
+			if (!readVector(gff, header, data))
+				return false;
+			break;
+
+		default:
+			return false;
+	}
+
+	return true;
+}
+
+inline bool GFFField::seekGFFData(Common::SeekableReadStream &gff,
+		const GFFHeader &header, uint32 data, uint32 &curPos) {
+
+	if (data >= header.fieldDataCount)
+		return false;
+
+	curPos = gff.pos();
+	if (!gff.seek(header.fieldDataOffset + data))
+		return false;
+
+	return true;
+}
+
+inline bool GFFField::readUint64(Common::SeekableReadStream &gff,
+		const GFFHeader &header, uint32 data) {
+
+	uint32 curPos;
+	if (!seekGFFData(gff, header, data, curPos))
+		return false;
+
+	_value.typeInt = gff.readUint64LE();
+
+	gff.seek(curPos);
+	return true;
+}
+
+inline bool GFFField::readSint64(Common::SeekableReadStream &gff,
+		const GFFHeader &header, uint32 data) {
+
+	uint32 curPos;
+	if (!seekGFFData(gff, header, data, curPos))
+		return false;
+
+	_value.typeInt = (uint64) gff.readSint64LE();
+
+	gff.seek(curPos);
+	return true;
+}
+
+inline bool GFFField::readDouble(Common::SeekableReadStream &gff,
+		const GFFHeader &header, uint32 data) {
+
+	uint32 curPos;
+	if (!seekGFFData(gff, header, data, curPos))
+		return false;
+
+	_value.typeDouble = AuroraFile::readDouble(gff);
+
+	gff.seek(curPos);
+	return true;
+}
+
+inline bool GFFField::readExoString(Common::SeekableReadStream &gff,
+		const GFFHeader &header, uint32 data) {
+
+	uint32 curPos;
+	if (!seekGFFData(gff, header, data, curPos))
+		return false;
+
+	uint32 length = gff.readUint32LE();
+
+	_value.typeString = new std::string;
+
+	*_value.typeString = AuroraFile::readRawString(gff, length);
+
+	gff.seek(curPos);
+	return true;
+}
+
+inline bool GFFField::readResRef(Common::SeekableReadStream &gff,
+		const GFFHeader &header, uint32 data) {
+
+	uint32 curPos;
+	if (!seekGFFData(gff, header, data, curPos))
+		return false;
+
+	uint32 length = gff.readByte();
+
+	_value.typeString = new std::string;
+
+	*_value.typeString = AuroraFile::readRawString(gff, length);
+
+	gff.seek(curPos);
+	return true;
+}
+
+inline bool GFFField::readLocString(Common::SeekableReadStream &gff,
+		const GFFHeader &header, uint32 data) {
+
+	uint32 curPos;
+	if (!seekGFFData(gff, header, data, curPos))
+		return false;
+
+	_value.typeLocString = new LocString();
+
+	gff.skip(4); // Size in bytes
+	_value.typeLocString->readLocString(gff);
+
+	gff.seek(curPos);
+	return true;
+}
+
+inline bool GFFField::readVoid(Common::SeekableReadStream &gff,
+		const GFFHeader &header, uint32 data) {
+
+	uint32 curPos;
+	if (!seekGFFData(gff, header, data, curPos))
+		return false;
+
+	_dataSize = gff.readUint32LE();
+
+	_value.typeData = new byte[_dataSize];
+
+	if (gff.read(_value.typeData, _dataSize) != _dataSize) {
+		delete[] _value.typeData;
+
+		_value.typeData = 0;
+		_dataSize = 0;
+
+		return false;
+	}
+
+	gff.seek(curPos);
+	return true;
+}
+
+inline bool GFFField::readVector(Common::SeekableReadStream &gff,
+		const GFFHeader &header, uint32 data) {
+
+	uint32 curPos;
+	if (!seekGFFData(gff, header, data, curPos))
+		return false;
+
+	gff.seek(curPos);
+	return true;
+}
+
+inline GFFField::Type GFFField::toType(GFFType type) {
+	switch (type) {
+		case kGFFTypeChar:
+			return kTypeChar;
+
+		case kGFFTypeByte:
+		case kGFFTypeUint16:
+		case kGFFTypeUint32:
+		case kGFFTypeUint64:
+			return kTypeUint;
+
+		case kGFFTypeSint16:
+		case kGFFTypeSint32:
+		case kGFFTypeSint64:
+			return kTypeSint;
+
+		case kGFFTypeFloat:
+		case kGFFTypeDouble:
+			return kTypeDouble;
+
+		case kGFFTypeExoString:
+		case kGFFTypeResRef:
+		case kGFFTypeStrRef:
+			return kTypeString;
+
+		case kGFFTypeLocString:
+			return kTypeLocString;
+
+		case kGFFTypeVoid:
+			return kTypeData;
+
+		case kGFFTypeStruct:
+			return kTypeStruct;
+
+		case kGFFTypeList:
+			return kTypeList;
+
+		case kGFFTypeOrientation:
+			return kTypeOrientation;
+
+		case kGFFTypeVector:
+			return kTypeVector;
+
+		default:
+			break;
+	}
+
+	return kTypeNone;
+}
+
+
+GFFFile::GFFFile() {
+}
+
+GFFFile::~GFFFile() {
+}
+
+void GFFFile::clear() {
+	_header.clear();
+}
+
+bool GFFFile::load(Common::SeekableReadStream &gff) {
+	if (!_header.read(gff))
+		return false;
+
+	_structArray.resize(_header.structCount);
+	for (uint32 i = 0; i < _header.structCount; i++) {
+		gff.skip(4); // Programmer-defined ID
+
+		uint32 data  = gff.readUint32LE();
+		uint32 count = gff.readUint32LE();
+
+		uint32 curPos = gff.pos();
+
+		_structArray[i].resize(count);
+		if (count > 1) {
+			if (!readFields(gff, _structArray[i], data))
+				return false;
+		} else
+			if (!readField(gff, _structArray[i][0], data))
+				return false;
+
+		gff.seek(curPos);
+	}
+
+	return true;
+}
+
+bool GFFFile::readField(Common::SeekableReadStream &gff, GFFField &field, uint32 fieldIndex) {
+	if (fieldIndex > _header.fieldCount)
+		return false;
+
+	if (!gff.seek(_header.fieldOffset + fieldIndex * 12))
+		return false;
+
+	if (!field.read(gff, _header))
+		return false;
+
+	return true;
+}
+
+bool GFFFile::readFields(Common::SeekableReadStream &gff, GFFStruct &strct, uint32 fieldIndicesIndex) {
+	if (fieldIndicesIndex > _header.fieldIndicesCount)
+		return false;
+
+	if (!gff.seek(_header.fieldIndicesOffset + fieldIndicesIndex))
+		return false;
+
+	uint32 count = strct.size();
+
+	std::vector<uint32> indices;
+
+	indices.resize(count);
+	for (uint32 i = 0; i < count; i++)
+		indices[i] = gff.readUint32LE();
+
+	for (uint32 i = 0; i < count; i++)
+		if (!readField(gff, strct[i], indices[i]))
+			return false;
+
+	return true;
 }
 
 } // End of namespace Aurora
--- src/aurora/gfffile.h
+++ src/aurora/gfffile.h
@@ -25,22 +25,45 @@ namespace Aurora {
 
 class LocString;
 
+struct GFFHeader {
+	uint32 id;
+	uint32 version;
+	uint32 structOffset;
+	uint32 structCount;
+	uint32 fieldOffset;
+	uint32 fieldCount;
+	uint32 labelOffset;
+	uint32 labelCount;
+	uint32 fieldDataOffset;
+	uint32 fieldDataCount;
+	uint32 fieldIndicesOffset;
+	uint32 fieldIndicesCount;
+	uint32 listIndicesOffset;
+	uint32 listIndicesCount;
+
+	GFFHeader();
+
+	void clear();
+
+	bool read(Common::SeekableReadStream &gff);
+};
+
 class GFFField {
 public:
 	enum Type {
-		kTypeNone      = 0,
-		kTypeChar      = 1,
-		kTypeUint      = 2,
-		kTypeSint      = 3,
-		kTypeString    = 4,
-		kTypeLocString = 5,
-		kTypeData      = 6,
-		kTypeStruct    = 7,
-		kTypeList      = 8
+		kTypeNone        = 0,
+		kTypeChar        = 1,
+		kTypeUint        = 2,
+		kTypeSint        = 3,
+		kTypeDouble      = 4,
+		kTypeString      = 5,
+		kTypeLocString   = 6,
+		kTypeData        = 7,
+		kTypeStruct      = 8,
+		kTypeList        = 9,
+		kTypeOrientation = 10,
+		kTypeVector      = 11
 	};
-
-	typedef std::vector<GFFField *> GFFStruct;
-	typedef std::vector<GFFStruct> GFFList;
 
 	GFFField();
 	~GFFField();
@@ -49,56 +72,115 @@ public:
 
 	Type getType() const;
 
-	const std::string &getName() const;
+	const std::string &getLabel() const;
 
 	char   getChar() const;
-	uint64 getUnsignedInteger() const;
-	int64  getSignedInteger()   const;
+	uint64 getUint() const;
+	int64  getSint() const;
 
 	double getDouble() const;
 
 	const std::string &getString()    const;
 	const LocString   &getLocString() const;
 
-	const GFFStruct &getStruct() const;
-	const GFFList   &getList()   const;
+	uint32 getDataSize() const;
+	const byte *getData() const;
+
+	const float *getVector() const;
+
+	const uint32 getIndex() const;
+
+	bool read(Common::SeekableReadStream &gff, const GFFHeader &header);
 
 private:
 	enum GFFType {
-		kGFFTypeNone      = - 1,
-		kGFFTypeByte      =   0,
-		kGFFTypeChar      =   1,
-		kGFFTypeUint16    =   2,
-		kGFFTypeInt16     =   3,
-		kGFFTypeUint32    =   4,
-		kGFFTypeInt32     =   5,
-		kGFFTypeUint64    =   6,
-		kGFFTypeInt64     =   7,
-		kGFFTypeFloat     =   8,
-		kGFFTypeDouble    =   9,
-		kGFFTypeExoString =  10,
-		kGFFTypeResRef    =  11, ///< String, max. 16 characters.
-		kGFFTypeLocString =  12, ///< Localized string.
-		kGFFTypeVoid      =  13, ///< Random data of variable length.
-		kGFFTypeStruct    =  14, ///< Struct containing a number of fields.
-		kGFFTypeList      =  15  ///< List containing a number of structs.
+		kGFFTypeNone        = - 1,
+		kGFFTypeByte        =   0,
+		kGFFTypeChar        =   1,
+		kGFFTypeUint16      =   2,
+		kGFFTypeSint16      =   3,
+		kGFFTypeUint32      =   4,
+		kGFFTypeSint32      =   5,
+		kGFFTypeUint64      =   6,
+		kGFFTypeSint64      =   7,
+		kGFFTypeFloat       =   8,
+		kGFFTypeDouble      =   9,
+		kGFFTypeExoString   =  10,
+		kGFFTypeResRef      =  11, ///< String, max. 16 characters.
+		kGFFTypeLocString   =  12, ///< Localized string.
+		kGFFTypeVoid        =  13, ///< Random data of variable length.
+		kGFFTypeStruct      =  14, ///< Struct containing a number of fields.
+		kGFFTypeList        =  15, ///< List containing a number of structs.
+		kGFFTypeOrientation =  16, // TODO: New in KotOR
+		kGFFTypeVector      =  17, ///< A vector of 3 floats
+		kGFFTypeStrRef      =  18  // TODO: New in Jade Empire
 	};
 
 	GFFType _gffType;
 	   Type _type;
 
-	std::string _name;
+	std::string _label;
+
+	uint32 _dataSize;
 
 	union {
 		uint64 typeInt;
-		double typeDouble;;
+		double typeDouble;
 
 		std::string *typeString;
 		LocString   *typeLocString;
 
-		GFFStruct *typeStruct;
-		GFFList   *typeList;
+		byte *typeData;
+
+		float typeVector[3];
+
+		uint32 typeIndex;
 	} _value;
+
+	bool convertData(Common::SeekableReadStream &gff, const GFFHeader &header, uint32 data);
+
+	inline bool readUint64   (Common::SeekableReadStream &gff,
+			const GFFHeader &header, uint32 data);
+	inline bool readSint64   (Common::SeekableReadStream &gff,
+			const GFFHeader &header, uint32 data);
+	inline bool readDouble   (Common::SeekableReadStream &gff,
+			const GFFHeader &header, uint32 data);
+	inline bool readExoString(Common::SeekableReadStream &gff,
+			const GFFHeader &header, uint32 data);
+	inline bool readResRef   (Common::SeekableReadStream &gff,
+			const GFFHeader &header, uint32 data);
+	inline bool readLocString(Common::SeekableReadStream &gff,
+			const GFFHeader &header, uint32 data);
+	inline bool readVoid     (Common::SeekableReadStream &gff,
+			const GFFHeader &header, uint32 data);
+	inline bool readVector   (Common::SeekableReadStream &gff,
+			const GFFHeader &header, uint32 data);
+
+	inline bool seekGFFData(Common::SeekableReadStream &gff,
+			const GFFHeader &header, uint32 data, uint32 &curPos);
+
+	static inline Type toType(GFFType type);
+};
+
+class GFFFile {
+public:
+	GFFFile();
+	~GFFFile();
+
+	void clear();
+
+	bool load(Common::SeekableReadStream &gff);
+
+private:
+	typedef std::vector<GFFField>  GFFStruct;
+	typedef std::vector<GFFStruct> GFFStructArray;
+
+	GFFHeader _header;
+
+	GFFStructArray _structArray;
+
+	bool readField(Common::SeekableReadStream &gff, GFFField &field, uint32 fieldIndex);
+	bool readFields(Common::SeekableReadStream &gff, GFFStruct &strct, uint32 fieldIndicesIndex);
 };
 
 } // End of namespace Aurora
