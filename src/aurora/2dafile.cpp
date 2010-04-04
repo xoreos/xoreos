1b9ca613af805acbc0b2b7bf6f321f2a690eb7f4
Sven Hesse <drmccoy@users.sourceforge.net>
2010-04-04 14:20:28 +0200

    Adding support for 2DA files

---
+++ src/aurora/2dafile.cpp
@@ -0,0 +1,257 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ * Copyright (c) 2010 Sven Hesse (DrMcCoy), Matthew Hoops (clone2727)
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ *
+ * This file is part of eos and is distributed under the terms of
+ * the GNU General Public Licence. See COPYING for more informations.
+ */
+
+/** @file aurora/2dafile.cpp
+ *  Handling BioWare's 2DAs (two-dimensional array).
+ */
+
+#include "boost/algorithm/string.hpp"
+#include "boost/tokenizer.hpp"
+
+#include "common/stream.h"
+#include "common/util.h"
+#include "common/strutil.h"
+
+#include "aurora/2dafile.h"
+
+static const uint32 k2DAID     = MKID_BE('2DA ');
+static const uint32 kVersion2  = MKID_BE('V2.0');
+
+namespace Aurora {
+
+TwoDAFile::TwoDAFile() {
+}
+
+TwoDAFile::~TwoDAFile() {
+}
+
+void TwoDAFile::clear() {
+	AuroraBase::clear();
+
+	_headers.clear();
+
+	for (Array::iterator row = _array.begin(); row != _array.end(); ++row)
+		delete *row;
+	_array.clear();
+
+	_headerMap.clear();
+
+	_defaultString.clear();
+	_defaultInt   = 0;
+	_defaultFloat = 0.0;
+}
+
+bool TwoDAFile::load(Common::SeekableReadStream &twoda) {
+	clear();
+
+	readHeader(twoda);
+
+	if (_id != k2DAID) {
+		warning("TwoDAFile::load(): Not a 2DA file");
+		return false;
+	}
+
+	if (_version != kVersion2) {
+		warning("TwoDAFile::load(): Unsupported file version");
+		return false;
+	}
+
+	twoda.readLine(); // Skip the rest of the line
+
+	// Read the rest of the 2DA's features
+	readDefault(twoda);
+	readHeaders(twoda);
+	readRows(twoda);
+
+	// Create the map to quickly translate headers to column indices
+	createHeaderMap();
+
+	return true;
+}
+
+void TwoDAFile::readDefault(Common::SeekableReadStream &twoda) {
+	std::string defaultList = twoda.readLine();
+
+	Row defaultRow;
+	defaultRow.reserve(2);
+
+	split(defaultList, defaultRow);
+	if ((defaultRow.size() >= 2) && (defaultRow[0] == "Default:"))
+		_defaultString = defaultRow[1];
+
+	_defaultInt   = parseInt(_defaultString);
+	_defaultFloat = parseFloat(_defaultString);
+}
+
+void TwoDAFile::readHeaders(Common::SeekableReadStream &twoda) {
+	std::string headerLine = twoda.readLine();
+	split(headerLine, _headers);
+}
+
+void TwoDAFile::readRows(Common::SeekableReadStream &twoda) {
+	uint32 columnCount = _headers.size();
+
+	std::string line;
+	while (!twoda.eos()) {
+		Row *row = new Row;
+
+		// We can expect that amount of fields
+		row->reserve(columnCount);
+
+		// Read the row
+		line = twoda.readLine();
+		split(line, *row, 1);
+
+		_array.push_back(row);
+	}
+}
+
+void TwoDAFile::createHeaderMap() {
+	for (uint32 i = 0; i < _headers.size(); i++)
+		_headerMap.insert(std::make_pair(_headers[i], i));
+}
+
+void TwoDAFile::split(const std::string &str, Row &row, uint32 skip) {
+	// Create a tokenizer over the string, splitting at spaces, but accept spaces within quotes
+	boost::tokenizer<boost::escaped_list_separator<char> > tok(str, boost::escaped_list_separator<char>("", " ", "\""));
+
+	for(boost::tokenizer<boost::escaped_list_separator<char> >::iterator it = tok.begin(); it != tok.end(); ++it) {
+		if (it->empty())
+			// Skip empty tokens
+			continue;
+
+		if (skip > 0) {
+			// Skip the specified amount of real tokens
+			skip--;
+			continue;
+		}
+
+		row.push_back(*it);
+	}
+}
+
+uint32 TwoDAFile::rowCount() const {
+	return _array.size();
+}
+
+uint32 TwoDAFile::columnCount() const {
+	return _headers.size();
+}
+
+const TwoDAFile::Row &TwoDAFile::getHeaders() const {
+	return _headers;
+}
+
+uint32 TwoDAFile::headerToColumn(const std::string &header) const {
+	HeaderMap::const_iterator column = _headerMap.find(header);
+	if (column == _headerMap.end())
+		// No such header
+		return kColumnInvalid;
+
+	return column->second;
+}
+
+const TwoDAFile::Row *TwoDAFile::getRow(uint32 row) const {
+	if (row >= _array.size())
+		// No such row
+		return 0;
+
+	return _array[row];
+}
+
+const std::string *TwoDAFile::getCell(uint32 row, uint32 column) const {
+	const Row *r = getRow(row);
+	if (!r)
+		// No such row
+		return 0;
+
+	if (column >= r->size())
+		// No such column
+		return 0;
+
+	return &(*r)[column];
+}
+
+static const std::string kEmptyString;
+
+const std::string &TwoDAFile::getCellString(uint32 row, uint32 column) const {
+	const std::string *cell = getCell(row, column);
+	if (!cell)
+		// Cell does not exist, return default value
+		return _defaultString;
+
+	// Check if the cell is empty
+	if (*cell == "****")
+		return kEmptyString;
+
+	return *cell;
+}
+
+const std::string &TwoDAFile::getCellString(uint32 row, const std::string &column) const {
+	return getCellString(row, headerToColumn(column));
+}
+
+const int32 TwoDAFile::getCellInt(uint32 row, uint32 column) const {
+	const std::string *cell = getCell(row, column);
+	if (!cell)
+		// Cell does not exist, return default value
+		return _defaultInt;
+
+	// Check if the cell is empty
+	if (*cell == "****")
+		return 0;
+
+	return parseInt(*cell);
+}
+
+const int32 TwoDAFile::getCellInt(uint32 row, const std::string &column) const {
+	return getCellInt(row, headerToColumn(column));
+}
+
+const float TwoDAFile::getCellFloat(uint32 row, uint32 column) const {
+	const std::string *cell = getCell(row, column);
+	if (!cell)
+		// Cell does not exist, return default value
+		return _defaultFloat;
+
+	// Check if the cell is empty
+	if (*cell == "****")
+		return 0.0;
+
+	return parseFloat(*cell);
+}
+
+const float TwoDAFile::getCellFloat(uint32 row, const std::string &column) const {
+	return getCellFloat(row, headerToColumn(column));
+}
+
+int32 TwoDAFile::parseInt(const std::string &str) {
+	if (str.empty())
+		return 0;
+
+	int v;
+	if (!Common::stringConvert(str, v))
+		return 0;
+
+	return v;
+}
+
+float TwoDAFile::parseFloat(const std::string &str) {
+	if (str.empty())
+		return 0;
+
+	float v;
+	if (!Common::stringConvert(str, v))
+		return 0.0;
+
+	return v;
+}
+
+} // End of namespace Aurora
---
+++ src/aurora/2dafile.h
@@ -0,0 +1,119 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ * Copyright (c) 2010 Sven Hesse (DrMcCoy), Matthew Hoops (clone2727)
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ *
+ * This file is part of eos and is distributed under the terms of
+ * the GNU General Public Licence. See COPYING for more informations.
+ */
+
+/** @file aurora/2dafile.h
+ *  Handling BioWare's 2DAs (two-dimensional array).
+ */
+
+#ifndef AURORA_2DAFILE_H
+#define AURORA_2DAFILE_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "common/types.h"
+
+#include "aurora/types.h"
+#include "aurora/aurorafile.h"
+
+namespace Common {
+	class SeekableReadStream;
+}
+
+namespace Aurora {
+
+/** Class to hold the two-dimensional array of a 2DA file. */
+class TwoDAFile : public AuroraBase {
+public:
+	static const int kColumnInvalid = 0xFFFFFFFF;
+
+	typedef std::vector<std::string> Row;
+
+	TwoDAFile();
+	~TwoDAFile();
+
+	/** Clear the array. */
+	void clear();
+
+	/** Load a 2DA file.
+	 *
+	 *  @param  twoda A stream of an TwoDA file.
+	 *  @return true if loading was successful, false otherwise.
+	 */
+	bool load(Common::SeekableReadStream &twoda);
+
+	/** Return the number of rows in the array. */
+	uint32 rowCount() const;
+
+	/** Return the number of columns in the array. */
+	uint32 columnCount() const;
+
+	/** Return the columns' headers. */
+	const Row &getHeaders() const;
+
+	/** Translate a column header to a column index. */
+	uint32 headerToColumn(const std::string &header) const;
+
+	/** Get a complete row.
+	 *
+	 *  @param  row The row's index.
+	 *  @return A pointer to the row, or 0 if the row does not exist.
+	 */
+	const Row *getRow(uint32 row) const;
+
+	/** Return the contents of a cell as a string. */
+	const std::string &getCellString(uint32 row, uint32 column) const;
+	/** Return the contents of a cell as a string. */
+	const std::string &getCellString(uint32 row, const std::string &column) const;
+
+	/** Return the contents of a cell as an int. */
+	const int32 getCellInt(uint32 row, uint32 column) const;
+	/** Return the contents of a cell as an int. */
+	const int32 getCellInt(uint32 row, const std::string &column) const;
+
+	/** Return the contents of a cell as a float. */
+	const float getCellFloat(uint32 row, uint32 column) const;
+	/** Return the contents of a cell as a float. */
+	const float getCellFloat(uint32 row, const std::string &column) const;
+
+private:
+	typedef std::vector<Row *> Array;
+
+	typedef std::map<std::string, uint32> HeaderMap;
+
+	std::string _defaultString; ///< The default string to return should a cell not exist.
+	int32       _defaultInt;    ///< The default int to return should a cell not exist.
+	float       _defaultFloat;  ///< The default float to return should a cell not exist.
+
+	Row _headers; ///< The columns' headers.
+	Array _array; ///< The array itself.
+
+	/** Map to translate a column header to an index. */
+	HeaderMap _headerMap;
+
+	// Loading helpers
+	void readDefault(Common::SeekableReadStream &twoda);
+	void readHeaders(Common::SeekableReadStream &twoda);
+	void readRows(Common::SeekableReadStream &twoda);
+	void createHeaderMap();
+
+	/** Split a string into distinct fields, as described by the 2DA specifications. */
+	void split(const std::string &str, Row &row, uint32 skip = 0);
+
+	const std::string *getCell(uint32 row, uint32 column) const;
+
+	static int32 parseInt(const std::string &str);
+	static float parseFloat(const std::string &str);
+};
+
+} // End of namespace Aurora
+
+#endif // AURORA_2DAFILE_H
