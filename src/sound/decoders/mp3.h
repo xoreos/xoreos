d34f18a2c78a8ace2f5bb6044fe4407b5564e481
Matthew Hoops <clone2727@gmail.com>
2010-04-18 15:46:04 -0400

    Add the MP3 AudioStream decoder. We now have a libMAD dependency.

---
+++ src/sound/decoders/mp3.h
@@ -0,0 +1,39 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ * Copyright (c) 2010 Sven Hesse (DrMcCoy), Matthew Hoops (clone2727)
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ *
+ * This file is part of eos and is distributed under the terms of
+ * the GNU General Public Licence. See COPYING for more informations.
+ */
+
+#ifndef SOUND_DECODERS_MP3_H
+#define SOUND_DECODERS_MP3_H
+
+#include "common/types.h"
+
+namespace Common {
+	class SeekableReadStream;
+}
+
+namespace Sound {
+
+class AudioStream;
+class RewindableAudioStream;
+
+/**
+ * Create a new SeekableAudioStream from the MP3 data in the given stream.
+ * Allows for seeking (which is why we require a SeekableReadStream).
+ *
+ * @param stream			the SeekableReadStream from which to read the MP3 data
+ * @param disposeAfterUse	whether to delete the stream after use
+ * @return	a new SeekableAudioStream, or NULL, if an error occured
+ */
+RewindableAudioStream *makeMP3Stream(
+	Common::SeekableReadStream *stream,
+	DisposeAfterUse::Flag disposeAfterUse);
+
+} // End of namespace Sound
+
+#endif
