43685e1db573a634430dcda92e86504def417603
Sven Hesse <drmccoy@users.sourceforge.net>
2010-04-19 19:08:13 +0200

    Yeah. not gonna happen. Dependencies on stuff in ffmpeg is far too high, especially for the IDCT stuff (which, in ffmpeg, also includes implementations in inline assembly)

--- src/graphics/video/bik.cpp
+++ src/graphics/video/bik.cpp
@@ -19,33 +19,16 @@
 #include "common/stream.h"
 
 #include "graphics/video/bik.h"
-#include "graphics/video/binkdata.h"
 
 #include "events/events.h"
 
-static const uint32 kBIKfID = MKID_BE('BIKf');
-static const uint32 kBIKgID = MKID_BE('BIKg');
-static const uint32 kBIKhID = MKID_BE('BIKh');
-static const uint32 kBIKiID = MKID_BE('BIKi');
-
-static const uint32 kVideoFlagAlpha = 0x00100000;
-
 namespace Graphics {
 
-BIK::BIK(Common::SeekableReadStream *bik) : _bik(bik), _curFrame(0) {
-	for (int i = 0; i < kSourceMAX; i++) {
-		_bundles[i].data    = 0;
-		_bundles[i].dataEnd = 0;
-		_bundles[i].curDec  = 0;
-		_bundles[i].curPtr  = 0;
-	}
-
+BIK::BIK(Common::SeekableReadStream *bik) : _bik(bik) {
 	load();
 }
 
 BIK::~BIK() {
-	freeBundles();
-
 	delete _bik;
 }
 
@@ -54,190 +37,10 @@ bool BIK::gotTime() const {
 }
 
 void BIK::processData() {
-	uint32 curTime = EventMan.getTimestamp();
-
-	if (!_started) {
-		_startTime     = curTime;
-		_lastFrameTime = curTime;
-		_started       = true;
-	}
-
-	uint32 frameTime = ((uint64) (_curFrame * 1000 * ((uint64) _fpsDen))) / _fpsNum;
-	if ((curTime - _startTime) < frameTime)
-		return;
-
-	if (_curFrame >= _frames.size()) {
-		_finished = true;
-		return;
-	}
-
-	VideoFrame &frame = _frames[_curFrame];
-
-	if (!_bik->seek(frame.offset))
-		throw Common::Exception(Common::kSeekError);
-
-	uint32 frameSize = frame.size;
-
-	byte *frameData = new byte[frameSize];
-	if (_bik->read(frameData, frameSize) != frameSize)
-		throw Common::Exception(Common::kReadError);
-
-	frame.data     = frameData;
-	frame.dataSize = frameSize;
-
-	for (std::vector<AudioTrack>::iterator audio = _audioTracks.begin(); audio != _audioTracks.end(); ++audio) {
-		uint32 audioPacketLength = READ_LE_UINT32(frame.data);
-
-		frame.data     += 4;
-		frame.dataSize -= 4;
-
-		if (audioPacketLength != 0) {
-			audio->data     = frame.data;
-			audio->dataSize = audioPacketLength;
-
-			frame.data     += audioPacketLength;
-			frame.dataSize -= audioPacketLength;
-
-			audioPacket(*audio);
-		}
-	}
-
-	videoPacket(frame);
-
-	delete[] frameData;
-
-	_needCopy = true;
-
-	warning("Frame %d / %d", _curFrame, (int) _frames.size());
-
-	_curFrame++;
-}
-
-void BIK::audioPacket(AudioTrack &audio) {
-}
-
-void BIK::videoPacket(VideoFrame &video) {
-	uint32 bitCount = video.dataSize << 3;
-
+	_finished = true;
 }
 
 void BIK::load() {
-	_id = _bik->readUint32BE();
-	if ((_id != kBIKfID) && (_id != kBIKgID) && (_id != kBIKhID) && (_id != kBIKiID))
-		throw Common::Exception("Unknown BIK FourCC %04X", _id);
-
-	uint32 fileSize         = _bik->readUint32LE() + 8;
-	uint32 frameCount       = _bik->readUint32LE();
-	uint32 largestFrameSize = _bik->readUint32LE();
-
-	if (largestFrameSize > fileSize)
-		throw Common::Exception("Largest frame size greater than file size");
-
-	_bik->skip(4);
-
-	uint32 width  = _bik->readUint32LE();
-	uint32 height = _bik->readUint32LE();
-
-	createData(width, height);
-
-	_fpsNum = _bik->readUint32LE();
-	_fpsDen = _bik->readUint32LE();
-
-	if ((_fpsNum == 0) || (_fpsDen == 0))
-		throw Common::Exception("Invalid FPS (%d/%d)", _fpsNum, _fpsDen);
-
-	_videoFlags = _bik->readUint32LE();
-
-	uint32 audioTrackCount = _bik->readUint32LE();
-	if (audioTrackCount > 0) {
-		_audioTracks.resize(audioTrackCount);
-
-		_bik->skip(4 * audioTrackCount);
-
-		for (std::vector<AudioTrack>::iterator it = _audioTracks.begin(); it != _audioTracks.end(); ++it) {
-			it->sampleRate = _bik->readUint16LE();
-			it->flags      = _bik->readUint16LE();
-		}
-
-		_bik->skip(4 * audioTrackCount);
-	}
-
-	_frames.resize(frameCount);
-	for (uint32 i = 0; i < frameCount; i++) {
-		_frames[i].offset   = _bik->readUint32LE();
-		_frames[i].keyFrame = _frames[i].offset & 1;
-
-		_frames[i].offset &= ~1;
-
-		if (i != 0)
-			_frames[i - 1].size = _frames[i].offset - _frames[i - 1].offset;
-
-		_frames[i].data     = 0;
-		_frames[i].dataSize = 0;
-	}
-
-	_frames[frameCount - 1].size = _bik->size() - _frames[frameCount - 1].offset;
-
-	_hasAlpha   = _videoFlags & kVideoFlagAlpha;
-	_swapPlanes = (_id == kBIKhID) || (_id == kBIKiID);
-
-	initTrees();
-	initBundles();
-}
-
-void BIK::initTrees() {
-	if (!_trees[15].table) {
-		for (int i = 0; i < 16; i++) {
-			const int maxbits = binkTreeLens[i][15];
-			_trees[i].table = _table + i*128;
-			_trees[i].table_allocated = 1 << maxbits;
-			init_vlc(&_trees[i], maxbits, 16,
-			         binkTreeLens[i], 1, 1,
-			         binkTreeBits[i], 1, 1,
-			         INIT_VLC_USE_NEW_STATIC | INIT_VLC_LE);
-		}
-	}
-}
-
-void BIK::initBundles() {
-	uint32 bw = (_width  + 7) >> 3;
-	uint32 bh = (_height + 7) >> 3;
-
-	uint32 blocks = bw * bh;
-
-	for (int i = 0; i < kSourceMAX; i++) {
-		_bundles[i].data    = new byte[blocks * 64];
-		_bundles[i].dataEnd = _bundles[i].data + blocks * 64;
-	}
-}
-
-void BIK::freeBundles() {
-	for (int i = 0; i < kSourceMAX; i++) {
-		delete[] _bundles[i].data;
-
-		_bundles[i].data    = 0;
-		_bundles[i].dataEnd = 0;
-		_bundles[i].curDec  = 0;
-		_bundles[i].curPtr  = 0;
-	}
-}
-
-// TODO: Don't use the standard slow log2()?
-void BIK::initLengths(uint32 width, uint32 bw) {
-	_bundles[kSourceBlockTypes].length    = log2((width >> 3)    + 511) + 1;
-
-	_bundles[kSourceSubBlockTypes].length = log2((width >> 4)    + 511) + 1;
-
-	_bundles[kSourceColors].length        = log2((width >> 3)*64 + 511) + 1;
-
-	_bundles[kSourceIntraDC].length       = log2((width >> 3)    + 511) + 1;
-	_bundles[kSourceInterDC].length       = log2((width >> 3)    + 511) + 1;
-	_bundles[kSourceXOff].length          = log2((width >> 3)    + 511) + 1;
-	_bundles[kSourceYOff].length          = log2((width >> 3)    + 511) + 1;
-
-	_bundles[kSourcePattern].length       = log2((bw    << 3)    + 511) + 1;
-
-	_bundles[kSourceRun].length           = log2((width >> 3)*48 + 511) + 1;
 }
 
 } // End of namespace Graphics
--- src/graphics/video/bik.h
+++ src/graphics/video/bik.h
@@ -19,9 +19,6 @@
 
 #include "graphics/video/decoder.h"
 
-#define ALT_BITSTREAM_READER_LE
-#include "graphics/video/get_bits.h"
-
 namespace Common {
 	class SeekableReadStream;
 }
@@ -40,117 +37,9 @@ protected:
 	void processData();
 
 private:
-	/** IDs for different data types used in Bink video codec. */
-	enum Source {
-		kSourceBlockTypes    = 0, ///< 8x8 block types.
-		kSourceSubBlockTypes    , ///< 16x16 block types (a subset of 8x8 block types).
-		kSourceColors           , ///< Pixel values used for different block types.
-		kSourcePattern          , ///< 8-bit values for 2-colour pattern fill.
-		kSourceXOff             , ///< X components of motion value.
-		kSourceYOff             , ///< Y components of motion value.
-		kSourceIntraDC          , ///< DC values for intrablocks with DCT.
-		kSourceInterDC          , ///< DC values for interblocks with DCT.
-		kSourceRun              , ///< Run lengths for special fill block:
-
-		kSourceMAX
-	};
-
-	/** Bink video block types. */
-	enum BlockTypes {
-		kBlockSkip    = 0,  ///< Skipped block.
-		kBlockScaled     ,  ///< Block has size 16x16.
-		kBlockMotion     ,  ///< Block is copied from previous frame with some offset.
-		kBlockRun        ,  ///< Block is composed from runs of colours with custom scan order.
-		kBlockResidue    ,  ///< Motion block with some difference added.
-		kBlockIntra      ,  ///< Intra DCT block.
-		kBlockFill       ,  ///< Block is filled with single colour.
-		kBlockInter      ,  ///< Motion block with DCT applied to the difference.
-		kBlockPattern    ,  ///< Block is filled with two colours following custom pattern.
-		kBlockRaw        ,  ///< Uncoded 8x8 block.
-	};
-
-	/** Data needed to decode 4-bit Huffman-coded value. */
-	struct Tree {
-		int   vlcNum;   ///< Tree number (in bink_trees[]).
-		uint8 syms[16]; ///< Leaf value to symbol mapping.
-	};
-
-	/** Data structure used for decoding single Bink data type. */
-	struct Bundle {
-		uint32 length;  ///< Length of number of entries to decode (in bits).
-		Tree   tree;    ///< Huffman tree-related data
-		byte  *data;    ///< Buffer for decoded symbols.
-		byte  *dataEnd; ///< Buffer end.
-		byte  *curDec;  ///< Pointer to the not yet decoded part of the buffer.
-		byte  *curPtr;  ///< Pointer to the data that is not read from buffer yet.
-	};
-
-	/** An audio track. */
-	struct AudioTrack {
-		uint16 sampleRate;
-		uint16 flags;
-
-		byte  *data;
-		uint32 dataSize;
-	};
-
-	/** A video frame. */
-	struct VideoFrame {
-		bool keyFrame;
-
-		uint32 offset;
-		uint32 size;
-
-		byte  *data;
-		uint32 dataSize;
-	};
-
 	Common::SeekableReadStream *_bik;
 
-	uint32 _id; ///< The BIK FourCC.
-
-	uint32 _fpsNum; ///< Dividend of the FPS value.
-	uint32 _fpsDen; ///< Dividor of the FPS value.
-
-	uint32 _videoFlags; ///< Video frame features.
-
-	bool _hasAlpha;   ///< Do video frames have alpha?
-	bool _swapPlanes;
-
-	uint32 _curFrame; ///< Current Frame.
-
-	uint32 _startTime;     ///< Timestamp of when the video was started.
-	uint32 _lastFrameTime; ///< Timestamp of when the last frame was decoded.
-
-	std::vector<AudioTrack> _audioTracks; ///< All audio tracks.
-	std::vector<VideoFrame> _frames;      ///< All video frames.
-
-	Bundle _bundles[kSourceMAX]; ///< Bundles for decoding all data types.
-
-	VLC _trees[16];
-	VLC_TYPE _table[16 * 128][2];
-
 	void load();
-
-	/** Decode an audio packet. */
-	void audioPacket(AudioTrack &audio);
-	/** Decode a video packet. */
-	void videoPacket(VideoFrame &video);
-
-	void initTrees();
-
-	/** Allocates memory for bundles. */
-	void initBundles();
-	/** Frees memory used by bundles. */
-	void freeBundles();
-
-	/** Initializes length in all bundles.
-	 *
-	 *  @param c Decoder context.
-	 *  @param width Plane width.
-	 *  @param bw Plane width in 8x8 blocks.
-	 */
-	void initLengths(uint32 width, uint32 bw);
 };
 
 } // End of namespace Graphics
