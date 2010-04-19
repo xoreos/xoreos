f31dcd4e4b19a477d4c839c366479614d017cb06
Sven Hesse <drmccoy@users.sourceforge.net>
2010-04-19 20:37:16 +0200

    Adding the Bink stubs again

--- src/graphics/video/bik.cpp
+++ src/graphics/video/bik.cpp
@@ -22,9 +22,16 @@
 
 #include "events/events.h"
 
+static const uint32 kBIKfID = MKID_BE('BIKf');
+static const uint32 kBIKgID = MKID_BE('BIKg');
+static const uint32 kBIKhID = MKID_BE('BIKh');
+static const uint32 kBIKiID = MKID_BE('BIKi');
+
+static const uint32 kVideoFlagAlpha = 0x00100000;
+
 namespace Graphics {
 
-BIK::BIK(Common::SeekableReadStream *bik) : _bik(bik) {
+BIK::BIK(Common::SeekableReadStream *bik) : _bik(bik), _curFrame(0) {
 	load();
 }
 
@@ -37,10 +44,132 @@ bool BIK::gotTime() const {
 }
 
 void BIK::processData() {
-	_finished = true;
+	uint32 curTime = EventMan.getTimestamp();
+
+	if (!_started) {
+		_startTime     = curTime;
+		_lastFrameTime = curTime;
+		_started       = true;
+	}
+
+	uint32 frameTime = ((uint64) (_curFrame * 1000 * ((uint64) _fpsDen))) / _fpsNum;
+	if ((curTime - _startTime) < frameTime)
+		return;
+
+	if (_curFrame >= _frames.size()) {
+		_finished = true;
+		return;
+	}
+
+	VideoFrame &frame = _frames[_curFrame];
+
+	if (!_bik->seek(frame.offset))
+		throw Common::Exception(Common::kSeekError);
+
+	uint32 frameSize = frame.size;
+
+	byte *frameData = new byte[frameSize];
+	if (_bik->read(frameData, frameSize) != frameSize)
+		throw Common::Exception(Common::kReadError);
+
+	frame.data     = frameData;
+	frame.dataSize = frameSize;
+
+	for (std::vector<AudioTrack>::iterator audio = _audioTracks.begin(); audio != _audioTracks.end(); ++audio) {
+		uint32 audioPacketLength = READ_LE_UINT32(frame.data);
+
+		frame.data     += 4;
+		frame.dataSize -= 4;
+
+		if (audioPacketLength != 0) {
+			audio->data     = frame.data;
+			audio->dataSize = audioPacketLength;
+
+			frame.data     += audioPacketLength;
+			frame.dataSize -= audioPacketLength;
+
+			audioPacket(*audio);
+		}
+	}
+
+	videoPacket(frame);
+
+	delete[] frameData;
+
+	_needCopy = true;
+
+	warning("Frame %d / %d", _curFrame, (int) _frames.size());
+
+	_curFrame++;
+}
+
+void BIK::audioPacket(AudioTrack &audio) {
+}
+
+void BIK::videoPacket(VideoFrame &video) {
+	uint32 bitCount = video.dataSize << 3;
+
 }
 
 void BIK::load() {
+	_id = _bik->readUint32BE();
+	if ((_id != kBIKfID) && (_id != kBIKgID) && (_id != kBIKhID) && (_id != kBIKiID))
+		throw Common::Exception("Unknown BIK FourCC %04X", _id);
+
+	uint32 fileSize         = _bik->readUint32LE() + 8;
+	uint32 frameCount       = _bik->readUint32LE();
+	uint32 largestFrameSize = _bik->readUint32LE();
+
+	if (largestFrameSize > fileSize)
+		throw Common::Exception("Largest frame size greater than file size");
+
+	_bik->skip(4);
+
+	uint32 width  = _bik->readUint32LE();
+	uint32 height = _bik->readUint32LE();
+
+	createData(width, height);
+
+	_fpsNum = _bik->readUint32LE();
+	_fpsDen = _bik->readUint32LE();
+
+	if ((_fpsNum == 0) || (_fpsDen == 0))
+		throw Common::Exception("Invalid FPS (%d/%d)", _fpsNum, _fpsDen);
+
+	_videoFlags = _bik->readUint32LE();
+
+	uint32 audioTrackCount = _bik->readUint32LE();
+	if (audioTrackCount > 0) {
+		_audioTracks.resize(audioTrackCount);
+
+		_bik->skip(4 * audioTrackCount);
+
+		for (std::vector<AudioTrack>::iterator it = _audioTracks.begin(); it != _audioTracks.end(); ++it) {
+			it->sampleRate = _bik->readUint16LE();
+			it->flags      = _bik->readUint16LE();
+		}
+
+		_bik->skip(4 * audioTrackCount);
+	}
+
+	_frames.resize(frameCount);
+	for (uint32 i = 0; i < frameCount; i++) {
+		_frames[i].offset   = _bik->readUint32LE();
+		_frames[i].keyFrame = _frames[i].offset & 1;
+
+		_frames[i].offset &= ~1;
+
+		if (i != 0)
+			_frames[i - 1].size = _frames[i].offset - _frames[i - 1].offset;
+
+		_frames[i].data     = 0;
+		_frames[i].dataSize = 0;
+	}
+
+	_frames[frameCount - 1].size = _bik->size() - _frames[frameCount - 1].offset;
+
+	_hasAlpha   = _videoFlags & kVideoFlagAlpha;
+	_swapPlanes = (_id == kBIKhID) || (_id == kBIKiID);
 }
 
 } // End of namespace Graphics
--- src/graphics/video/bik.h
+++ src/graphics/video/bik.h
@@ -37,9 +37,52 @@ protected:
 	void processData();
 
 private:
+	/** An audio track. */
+	struct AudioTrack {
+		uint16 sampleRate;
+		uint16 flags;
+
+		byte  *data;
+		uint32 dataSize;
+	};
+
+	/** A video frame. */
+	struct VideoFrame {
+		bool keyFrame;
+
+		uint32 offset;
+		uint32 size;
+
+		byte  *data;
+		uint32 dataSize;
+	};
+
 	Common::SeekableReadStream *_bik;
 
+	uint32 _id; ///< The BIK FourCC.
+
+	uint32 _fpsNum; ///< Dividend of the FPS value.
+	uint32 _fpsDen; ///< Dividor of the FPS value.
+
+	uint32 _videoFlags; ///< Video frame features.
+
+	bool _hasAlpha;   ///< Do video frames have alpha?
+	bool _swapPlanes;
+
+	uint32 _curFrame; ///< Current Frame.
+
+	uint32 _startTime;     ///< Timestamp of when the video was started.
+	uint32 _lastFrameTime; ///< Timestamp of when the last frame was decoded.
+
+	std::vector<AudioTrack> _audioTracks; ///< All audio tracks.
+	std::vector<VideoFrame> _frames;      ///< All video frames.
+
 	void load();
+
+	/** Decode an audio packet. */
+	void audioPacket(AudioTrack &audio);
+	/** Decode a video packet. */
+	void videoPacket(VideoFrame &video);
 };
 
 } // End of namespace Graphics
