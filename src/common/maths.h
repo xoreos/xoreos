3e26b78a855d1c464fe2d10e42ab52c94b1ed951
Sven Hesse <drmccoy@users.sourceforge.net>
2010-04-27 18:56:42 +0200

    Adding cosine tables

---
+++ src/common/maths.h
@@ -0,0 +1,24 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ * Copyright (c) 2010 Sven Hesse (DrMcCoy), Matthew Hoops (clone2727)
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ *
+ * This file is part of eos and is distributed under the terms of
+ * the GNU General Public Licence. See COPYING for more informations.
+ */
+
+/** @file common/maths.h
+ *  Mathematical helpers.
+ */
+
+#ifndef COMMON_MATHS_H
+#define COMMON_MATHS_H
+
+namespace Common {
+
+const float *getCosineTable(uint8 bits);
+
+} // End of namespace Common
+
+#endif // COMMON_MATHS_H
