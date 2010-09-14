a62177ec5d9030a4b61ef5bdbaad166fcee7f01b
Sven Hesse <drmccoy@users.sourceforge.net>
2010-09-14 20:54:52 +0200

    UTF8: Correct extra utf8cpp includes

--- utf8cpp/utf8/checked.h
+++ utf8cpp/utf8/checked.h
@@ -28,7 +28,7 @@ DEALINGS IN THE SOFTWARE.
 #ifndef UTF8_FOR_CPP_CHECKED_H_2675DCD0_9480_4c0c_B92A_CC14C027B731
 #define UTF8_FOR_CPP_CHECKED_H_2675DCD0_9480_4c0c_B92A_CC14C027B731
 
-#include "core.h"
+#include "utf8cpp/utf8/core.h"
 #include <stdexcept>
 
 namespace utf8
