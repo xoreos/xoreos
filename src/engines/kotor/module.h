ed833e0bc48920da1bf331f95b30d9d34268c229
Sven Hesse <drmccoy@users.sourceforge.net>
2011-02-04 23:18:42 +0100

    DOCS: Update copyright notices

--- src/engines/kotor/module.h
+++ src/engines/kotor/module.h
@@ -1,5 +1,5 @@
 /* eos - A reimplementation of BioWare's Aurora engine
- * Copyright (c) 2010 Sven Hesse (DrMcCoy), Matthew Hoops (clone2727)
+ * Copyright (c) 2010-2011 Sven Hesse (DrMcCoy), Matthew Hoops (clone2727)
  *
  * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
  * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
