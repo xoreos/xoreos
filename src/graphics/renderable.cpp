985768bda5266d9387e0bd15f3cb87980d48ed25
Sven Hesse <drmccoy@users.sourceforge.net>
2011-03-27 09:49:56 +0200

    GRAPHICS: Add an isIn() for line/renderable intersection checks

--- src/graphics/renderable.cpp
+++ src/graphics/renderable.cpp
@@ -72,4 +72,8 @@ bool Renderable::isIn(float x, float y, float z) const {
 	return false;
 }
 
+bool Renderable::isIn(float x1, float y1, float z1, float x2, float y2, float z2) const {
+	return false;
+}
+
 } // End of namespace Graphics
--- src/graphics/renderable.h
+++ src/graphics/renderable.h
@@ -56,9 +56,10 @@ public:
 
 	/** Is that point within the object? */
 	virtual bool isIn(float x, float y) const;
-
 	/** Is that point within the object? */
 	virtual bool isIn(float x, float y, float z) const;
+	/** Does the line from x1.y1.z1 to x2.y2.z2 intersect with the object? */
+	virtual bool isIn(float x1, float y1, float z1, float x2, float y2, float z2) const;
 
 protected:
 	bool _clickable;
