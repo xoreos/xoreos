bc201fb4667c0b5d7b2b0fa089198911438d4b6d
Sven Hesse <drmccoy@users.sourceforge.net>
2011-04-25 00:14:57 +0200

    ALL: Relicense eos from GPLv2+ to GPLv3+
    Since boost.m4 is GPLv3+, we need to be GPLv3(+) too.
    All GPLv2 code we include has a "or later" clause, so no problems
    there.

--- src/engines/nwn/gui/widgets/tooltip.cpp
+++ src/engines/nwn/gui/widgets/tooltip.cpp
@@ -1,11 +1,26 @@
 /* eos - A reimplementation of BioWare's Aurora engine
- * Copyright (c) 2010-2011 Sven Hesse (DrMcCoy), Matthew Hoops (clone2727)
+ *
+ * eos is the legal property of its developers, whose names can be
+ * found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
  *
  * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
  * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
- *
- * This file is part of eos and is distributed under the terms of
- * the GNU General Public Licence. See COPYING for more informations.
  */
 
 /** @file engines/nwn/gui/widgets/tooltip.cpp
