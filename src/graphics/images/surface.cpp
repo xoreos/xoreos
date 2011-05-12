3116df45d9b97f7dfb977e7269d3100052a19579
Sven Hesse <drmccoy@users.sourceforge.net>
2011-05-13 00:48:26 +0200

    GRAPHICS: Add a small Surface class

---
+++ src/graphics/images/surface.cpp
@@ -0,0 +1,89 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ *
+ * eos is the legal property of its developers, whose names can be
+ * found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ */
+
+/** @file graphics/images/surface.cpp
+ *  An image surface, in BGRA format.
+ */
+
+#include <cassert>
+
+#include "graphics/images/surface.h"
+
+namespace Graphics {
+
+Surface::Surface(int width, int height) {
+	assert((width > 0) && (height > 0));
+
+	_compressed = false;
+	_hasAlpha   = true;
+	_format     = kPixelFormatBGRA;
+	_formatRaw  = kPixelFormatRGBA8;
+	_dataType   = kPixelDataType8;
+
+	_mipMaps.push_back(new MipMap);
+
+	_mipMaps[0]->width  = width;
+	_mipMaps[0]->height = height;
+	_mipMaps[0]->size   = _mipMaps[0]->width * _mipMaps[0]->height * 4;
+
+	_mipMaps[0]->data = new byte[_mipMaps[0]->size];
+}
+
+Surface::~Surface() {
+}
+
+int Surface::getWidth() const {
+	return _mipMaps[0]->width;
+}
+
+int Surface::getHeight() const {
+	return _mipMaps[0]->height;
+}
+
+byte *Surface::getData() {
+	return _mipMaps[0]->data;
+}
+
+const byte *Surface::getData() const {
+	return _mipMaps[0]->data;
+}
+
+void Surface::fill(byte r, byte g, byte b, byte a) {
+	if ((r == g) && (r == b) && (r == a)) {
+		memset(_mipMaps[0]->data, r, _mipMaps[0]->size);
+		return;
+	}
+
+	byte  *data = _mipMaps[0]->data;
+	uint32 size = _mipMaps[0]->size / 4;
+	while (size-- > 0) {
+		*data++ = b;
+		*data++ = g;
+		*data++ = r;
+		*data++ = a;
+	}
+}
+
+} // End of namespace Graphics
---
+++ src/graphics/images/surface.h
@@ -0,0 +1,53 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ *
+ * eos is the legal property of its developers, whose names can be
+ * found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ */
+
+/** @file graphics/images/surface.h
+ *  An image surface, in BGRA format.
+ */
+
+#ifndef GRAPHICS_IMAGES_SURFACE_H
+#define GRAPHICS_IMAGES_SURFACE_H
+
+#include "graphics/images/decoder.h"
+
+namespace Graphics {
+
+class Surface : public ImageDecoder {
+public:
+	Surface(int width, int height);
+	~Surface();
+
+	int getWidth () const;
+	int getHeight() const;
+
+	byte *getData();
+	const byte *getData() const;
+
+	void fill(byte r, byte g, byte b, byte a);
+};
+
+} // End of namespace Graphics
+
+#endif // GRAPHICS_IMAGES_SURFACE_H
