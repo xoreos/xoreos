f24fb7061aa3a8dda8eaba9c6f566ff8a24e0e39
Sven Hesse <drmccoy@users.sourceforge.net>
2011-06-01 04:45:22 +0200

    COMMON: Add a DebugManager
    Manages debug channels. Inspired by ScummVM's debug channels.

---
+++ src/common/debugman.cpp
@@ -0,0 +1,154 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ *
+ * eos is the legal property of its developers, whose names can be
+ * found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ */
+
+// Inspired by ScummVM's debug channels
+
+/** @file common/debugman.cpp
+ *  The debug manager, managing debug channels.
+ */
+
+#include <vector>
+
+#include "common/maths.h"
+#include "common/util.h"
+#include "common/debugman.h"
+
+DECLARE_SINGLETON(Common::DebugManager)
+
+namespace Common {
+
+DebugManager::DebugManager() : _debugLevel(0) {
+	for (uint32 i = 0; i < 30; i++)
+		_channels[i].enabled = false;
+}
+
+DebugManager::~DebugManager() {
+}
+
+bool DebugManager::addDebugChannel(uint32 channel, const UString &name,
+                                   const UString &description) {
+
+	if ((channel == 0) || name.empty())
+		return false;
+
+	uint32 index = log2(channel);
+	if (index > 30)
+		return false;
+
+	if (!_channels[index].name.empty())
+		return false;
+
+	_channels[index].name        = name;
+	_channels[index].description = description;
+	_channels[index].enabled     = false;
+
+	_channelMap[name] = channel;
+
+	return true;
+}
+
+void DebugManager::getDebugChannels(std::vector<UString> &names,
+                                    std::vector<UString> &descriptions,
+                                    uint32 &nameLength) const {
+
+	names.clear();
+	descriptions.clear();
+	nameLength = 0;
+
+	for (uint32 i = 0; i < 30; i++) {
+		const Channel &channel = _channels[i];
+		if (channel.name.empty())
+			continue;
+
+		names.push_back(channel.name);
+		descriptions.push_back(channel.description);
+
+		nameLength = MAX<uint32>(nameLength, channel.name.size());
+	}
+}
+
+void DebugManager::clearEngineChannels() {
+	for (uint32 i = 15; i < 30; i++) {
+		Channel &channel = _channels[i];
+
+		ChannelMap::iterator c = _channelMap.find(channel.name);
+		if (c != _channelMap.end())
+			_channelMap.erase(c);
+
+		channel.name.clear();
+		channel.description.clear();
+		channel.enabled = false;
+	}
+}
+
+uint32 DebugManager::parseChannelList(const UString &list) const {
+	std::vector<UString> channels;
+	UString::split(list, ',', channels);
+
+	uint32 mask = 0;
+	for (std::vector<UString>::const_iterator c = channels.begin(); c != channels.end(); ++c) {
+		if (c->equalsIgnoreCase("all"))
+			return 0xFFFFFFFF;
+
+		ChannelMap::const_iterator channel = _channelMap.find(*c);
+		if (channel == _channelMap.end()) {
+			warning("No such debug channel \"%s\"", c->c_str());
+			continue;
+		}
+
+		mask |= channel->second;
+	}
+
+	return mask;
+}
+
+void DebugManager::setEnabled(uint32 mask) {
+	for (uint32 i = 0; i < 30; i++, mask >>= 1)
+		_channels[i].enabled = (mask & 1) != 0;
+}
+
+bool DebugManager::isEnabled(uint32 level, uint32 channel) const {
+	if (_debugLevel < level)
+		return false;
+
+	if (channel == 0)
+		return false;
+
+	channel = log2(channel);
+	if (channel > 30)
+		return false;
+
+	return _channels[channel].enabled;
+}
+
+uint32 DebugManager::getDebugLevel() const {
+	return _debugLevel;
+}
+
+void DebugManager::setDebugLevel(uint32 level) {
+	_debugLevel = level;
+}
+
+} // End of namespace Common
---
+++ src/common/debugman.h
@@ -0,0 +1,116 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ *
+ * eos is the legal property of its developers, whose names can be
+ * found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ */
+
+// Inspired by ScummVM's debug channels
+
+/** @file common/debugman.h
+ *  The debug manager, managing debug channels.
+ */
+
+#ifndef COMMON_DEBUGMAN_H
+#define COMMON_DEBUGMAN_H
+
+#include <map>
+
+#include "common/types.h"
+#include "common/ustring.h"
+#include "common/singleton.h"
+
+namespace Common {
+
+/** The debug manager, managing debug channels. */
+class DebugManager : public Singleton<DebugManager> {
+public:
+	DebugManager();
+	~DebugManager();
+
+	/** Add a debug channel.
+	 *
+	 *  A debug channel separates debug messages into groups, so debug output
+	 *  doesn't get swamped unecessarily. Only when a debug channel is enabled
+	 *  are debug messages to that channel shown.
+	 *
+	 *  So that several channels can be enabled at the same time, all channel
+	 *  IDs must be OR-able, i.e. the first one should be 1, then 2, 4, ....
+	 *
+	 *  The first 15 channels (IDs 2^1 - 2^15) are reserved for the non-engine
+	 *  code, the next 15 channels (IDs 2^16 - 2^30) are reserved for the
+	 *  engines.
+	 *
+	 *  @param channel     The channel ID, should be OR-able.
+	 *  @param name        The channel's name, used to enable the channel on the
+	 *                     command line (or console, if supported by the engine).
+	 *  @param description A description of the channel, displayed when listing
+	 *                     The available channels.
+	 *
+	 *  @return true on success.
+	 *
+	 */
+	bool addDebugChannel(uint32 channel, const UString &name, const UString &description);
+
+	/** Return the channel names alongside their descriptions. */
+	void getDebugChannels(std::vector<UString> &names,
+	                      std::vector<UString> &descriptions, uint32 &nameLength) const;
+
+	/** Remove all engine-specific debug channels. */
+	void clearEngineChannels();
+
+	/** Parse a comma-separated list of debug channels to a channel mask. */
+	uint32 parseChannelList(const UString &list) const;
+
+	/** Use the specified mask to set the enabled state for all channels. */
+	void setEnabled(uint32 mask);
+
+	/** Is the specified channel and level enabled? */
+	bool isEnabled(uint32 level, uint32 channel) const;
+
+	/** Return the current debug level. */
+	uint32 getDebugLevel() const;
+	/** Set the current debug level. */
+	void   setDebugLevel(uint32 level);
+
+private:
+	/** A debug channel. */
+	struct Channel {
+		UString name;        ///< The channel's name.
+		UString description; ///< The channel's description.
+
+		bool enabled; ///< Is the channel enabled?
+	};
+
+	typedef std::map<UString, uint32> ChannelMap;
+
+	Channel    _channels[30]; ///< All debug channels.
+	ChannelMap _channelMap;   ///< Debug channels indexed by name.
+
+	uint32 _debugLevel; ///< The current debug level.
+};
+
+} // End of namespace Common
+
+/** Shortcut for accessing the debug manager. */
+#define DebugMan Common::DebugManager::instance()
+
+#endif // COMMON_DEBUGMAN_H
