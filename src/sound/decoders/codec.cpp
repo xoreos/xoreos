95e81cb12aa9a3f2399a03ba077bba3f362d7a10
Sven Hesse <drmccoy@users.sourceforge.net>
2011-07-09 13:59:43 +0200

    SOUND: Add a Sound::Codec base class

---
+++ src/sound/decoders/codec.cpp
@@ -0,0 +1,40 @@
+/* eos - A reimplementation of BioWare's Aurora engine
+ *
+ * eos is the legal property of its developers, whose names can be
+ * found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ *
+ * The Infinity, Aurora, Odyssey and Eclipse engines, Copyright (c) BioWare corp.
+ * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
+ */
+
+/** @file sound/decoders/codec.cpp
+ *  Audio codec base class.
+ */
+
+#include "sound/decoders/codec.h"
+
+namespace Sound {
+
+Codec::Codec() {
+}
+
+Codec::~Codec() {
+}
+
+} // End of namespace Sound
