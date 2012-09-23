d717ec305792c431069bc64ff976a1f93b0603ab
Beren Minor <beren.minor+git@gmail.com>
2012-09-23 12:27:40 +0200

    COMMON: Find integer types from boost/integer.hpp instead of checking the availability of int headers.

--- src/common/types.h
+++ src/common/types.h
@@ -34,47 +34,16 @@
 	#include "config.h"
 #endif
 
-#ifdef HAVE_STDINT_H
-	#include <stdint.h>
-#endif // HAVE_STDINT_H
-#ifdef HAVE_INTTYPES_H
-	#include <inttypes.h>
-#endif // HAVE_INTTYPES_H
-#ifdef HAVE_SYS_TYPES_H
-	#include <sys/types.h>
-#endif // HAVE_SYS_TYPES_H
+#include <boost/integer.hpp>
 
-#if defined (HAVE_STDINT_H)
-	typedef int8_t int8;
-	typedef uint8_t uint8;
-	typedef int16_t int16;
-	typedef uint16_t uint16;
-	typedef int32_t int32;
-	typedef uint32_t uint32;
-	typedef int64_t int64;
-	typedef uint64_t uint64;
-#elif defined (HAVE_INTTYPES_H)
-	typedef int8_t int8;
-	typedef uint8_t uint8;
-	typedef int_16_t int16;
-	typedef uint16_t uint16;
-	typedef int32_t int32;
-	typedef uint32_t uint32;
-	typedef int64_t int64;
-	typedef uint64_t uint64;
-#elif defined (HAVE_SYS_TYPES_H)
-	typedef int8_t int8;
-	typedef u_int8_t uint8;
-	typedef int16_t int16;
-	typedef u_int16_t uint16;
-	typedef int32_t int32;
-	typedef u_int32_t uint32;
-	typedef int64_t int64;
-	typedef u_int64_t uint64;
-#else
-	// TODO: Add a fall-back type detection to the configure script
-	#error No way to derive fixed-width variable types
-#endif
+typedef boost::int8_t int8;
+typedef boost::uint8_t uint8;
+typedef boost::int16_t int16;
+typedef boost::uint16_t uint16;
+typedef boost::int32_t int32;
+typedef boost::uint32_t uint32;
+typedef boost::int64_t int64;
+typedef boost::uint64_t uint64;
 
 typedef uint8 byte;
 typedef unsigned int uint;
