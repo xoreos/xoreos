0c25bf7eba27db2b4845230cddd51edcbd1de4b1
Matthew Hoops <clone2727@gmail.com>
2012-12-21 14:18:10 -0500

    COMMON: Fix formatting on a bracket

--- src/common/ustring.h
+++ src/common/ustring.h
@@ -56,8 +56,7 @@ public:
 	typedef utf8::iterator<std::string::const_iterator> iterator;
 
 	// Case insensitive compare
-	struct iless : std::binary_function<UString, UString, bool>
-	{
+	struct iless : std::binary_function<UString, UString, bool> {
 		bool operator() (const UString &str1, const UString &str2) const {
 			return str1.lessIgnoreCase(str2);
 		}
