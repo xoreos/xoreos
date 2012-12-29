72b503cbdf08de03482403ba7df6f32203e34b9e
Sven Hesse <drmccoy@users.sourceforge.net>
2012-12-29 18:41:32 +0100

    DRAGONAGE2: Load resources, show movie and textured cube

--- src/engines/dragonage2/dragonage2.cpp
+++ src/engines/dragonage2/dragonage2.cpp
@@ -103,6 +103,39 @@ void DragonAge2Engine::run(const Common::UString &target) {
 		return;
 
 	status("Successfully initialized the engine");
+
+	CursorMan.hideCursor();
+	CursorMan.set();
+
+	playVideo("dragon_age_ea_logo");
+	if (EventMan.quitRequested())
+		return;
+
+	CursorMan.showCursor();
+
+	bool showFPS = ConfigMan.getBool("showfps", false);
+
+	Graphics::Aurora::FPS *fps = 0;
+	if (showFPS) {
+		fps = new Graphics::Aurora::FPS(FontMan.get(Graphics::Aurora::kSystemFontMono, 13));
+		fps->show();
+	}
+
+	Graphics::Aurora::Cube *cube = 0;
+	try {
+
+		cube = new Graphics::Aurora::Cube("xx_wood");
+
+	} catch (Common::Exception &e) {
+		Common::printException(e);
+	}
+
+	while (!EventMan.quitRequested()) {
+		EventMan.delay(10);
+	}
+
+	delete cube;
+	delete fps;
 }
 
 void DragonAge2Engine::init() {
@@ -110,9 +143,136 @@ void DragonAge2Engine::init() {
 
 	status("Setting base directory");
 	ResMan.registerDataBaseDir(_baseDirectory);
+
+	status("Adding extra archive directories");
+	ResMan.addArchiveDir(Aurora::kArchiveERF, "packages/core/data");
+	ResMan.addArchiveDir(Aurora::kArchiveERF, "packages/core/textures/medium");
+	ResMan.addArchiveDir(Aurora::kArchiveERF, "packages/core/textures/high");
+	ResMan.addArchiveDir(Aurora::kArchiveERF, "packages/core/audio/sound/");
+	ResMan.addArchiveDir(Aurora::kArchiveERF, "packages/core/patch");
+
+	status("Loading core resource files");
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/2da.rim"                      ,  1);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/al_char_stage.rim"            ,  2);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/artfp.erf"                    ,  3);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/artfpwin32.erf"               ,  4);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/chargen.gpu.rim"              ,  5);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/chargen.rim"                  ,  6);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/designerresources.rim"        ,  7);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/designerscripts.rim"          ,  8);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/face.erf"                     ,  9);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/fonts.erf"                    , 10);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/global-uncompressed.rim"      , 11);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/global.rim"                   , 12);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/globalani-core.rim"           , 13);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/globalchargen-core.rim"       , 14);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/globalchargendds-core.gpu.rim", 15);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/globaldds-core.gpu.rim"       , 16);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/globalmao-core.rim"           , 17);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/globalvfx-core.rim"           , 18);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/gui.erf"                      , 19);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/guiexport.erf"                , 20);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/layouts.erf"                  , 21);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/lightprobedata.erf"           , 22);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/maoinit.erf"                  , 23);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/materialdefinitions.erf"      , 24);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/materialobjects.rim"          , 25);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/misc.erf"                     , 26);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/models.erf"                   , 27);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/pathfindingpatches.rim"       , 28);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/resmetrics.erf"               , 29);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/scripts.erf"                  , 30);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/shaders.erf"                  , 31);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/subqueuefiles.erf"            , 32);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/summonwardog.gpu.rim"         , 33);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/summonwardog.rim"             , 34);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/data/tints.rim"                    , 35);
+
+	status("Loading core sound resource files");
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/audio/sound/wwisebanks_core.erf"  , 40);
+	indexMandatoryArchive(Aurora::kArchiveERF, "packages/core/audio/sound/wwisestreams_core.erf", 41);
+
+	status("Indexing extra core movie resources");
+	indexMandatoryDirectory("packages/core/data/movies" , 0, 0, 101);
+	status("Indexing extra core cursors");
+	indexMandatoryDirectory("packages/core/data/cursors", 0, 0, 102);
 }
 
 void DragonAge2Engine::initCursors() {
+	CursorMan.add("area_transition"            , "area_transition"    , "up"      );
+	CursorMan.add("area_transition_pressed"    , "area_transition"    , "down"    );
+	CursorMan.add("attack"                     , "attack"             , "up"      );
+	CursorMan.add("attack_pressed"             , "attack"             , "down"    );
+	CursorMan.add("chest"                      , "chest"              , "up"      );
+	CursorMan.add("chest_pressed"              , "chest"              , "down"    );
+	CursorMan.add("close"                      , "close"              , "up"      );
+	CursorMan.add("close_pressed"              , "close"              , "down"    );
+	CursorMan.add("converse"                   , "converse"           , "up"      );
+	CursorMan.add("converse_pressed"           , "converse"           , "down"    );
+	CursorMan.add("destroy"                    , "destroy"            , "up"      );
+	CursorMan.add("destroy_pressed"            , "destroy"            , "down"    );
+	CursorMan.add("dialog"                     , "dialog"             , "up"      );
+	CursorMan.add("dialog_pressed"             , "dialog"             , "down"    );
+	CursorMan.add("disabled_disarm"            , "disabled_disarm"    , "up"      );
+	CursorMan.add("disabled_disarm_pressed"    , "disabled_disarm"    , "down"    );
+	CursorMan.add("disabled_unlock"            , "disabled_unlock"    , "up"      );
+	CursorMan.add("disabled_unlock_pressed"    , "disabled_unlock"    , "down"    );
+	CursorMan.add("disarm"                     , "disarm"             , "up"      );
+	CursorMan.add("disarm_pressed"             , "disarm"             , "down"    );
+	CursorMan.add("door"                       , "door"               , "up"      );
+	CursorMan.add("door_pressed"               , "door"               , "down"    );
+	CursorMan.add("dragging"                   , "dragging"           , ""        );
+	CursorMan.add("examine"                    , "examine"            , "up"      );
+	CursorMan.add("examine_pressed"            , "examine"            , "down"    );
+	CursorMan.add("flip_cover"                 , "flip_cover"         , "up"      );
+	CursorMan.add("flip_cover_pressed"         , "flip_cover"         , "down"    );
+	CursorMan.add("hand"                       , "hand"               , "up"      );
+	CursorMan.add("hand2"                      , "hand"               , "down"    );
+	CursorMan.add("hidden_cursor"              , "hidden_cursor"      , ""        );
+	CursorMan.add("ibeam"                      , "ibeam"              , "up"      );
+	CursorMan.add("ibeam_pressed"              , "ibeam"              , "down"    );
+	CursorMan.add("invalid"                    , "invalid"            , "up"      );
+	CursorMan.add("invalid_pressed"            , "invalid"            , "down"    );
+	CursorMan.add("invalid_move"               , "invalid_move"       , ""        );
+	CursorMan.add("open"                       , "open"               , "up"      );
+	CursorMan.add("open_pressed"               , "open"               , "down"    );
+	CursorMan.add("open_inventory"             , "open_inventory"     , "up"      );
+	CursorMan.add("open_inventory_pressed"     , "open_inventory"     , "down"    );
+	CursorMan.add("panning"                    , "panning"            , "down"    );
+	CursorMan.add("panning1"                   , "panning"            , "up"      );
+	CursorMan.add("panning1_h"                 , "panning_h"          , "up"      );
+	CursorMan.add("panning1_v"                 , "panning_v"          , "up"      );
+	CursorMan.add("panning_h"                  , "panning_h"          , "down"    );
+	CursorMan.add("panning_v"                  , "panning_v"          , "down"    );
+	CursorMan.add("party_member"               , "party_member"       , ""        );
+	CursorMan.add("standard"                   , "standard"           , "up"      );
+	CursorMan.add("standard_pressed"           , "standard"           , "down"    );
+	CursorMan.add("standard_targeting"         , "standard_targeting" , "up"      );
+	CursorMan.add("standard_targeting_pressed" , "standard_targeting" , "down"    );
+	CursorMan.add("target"                     , "target"             , "up"      );
+	CursorMan.add("target_pressed"             , "target"             , "down"    );
+	CursorMan.add("target_inactive"            , "target"             , "inactive");
+	CursorMan.add("target_circle"              , "target_circle"      , "up"      );
+	CursorMan.add("target_circle_pressed"      , "target_circle"      , "down"    );
+	CursorMan.add("target_circle_invalid"      , "target_circle"      , "invalid" );
+	CursorMan.add("target_cone"                , "target_cone"        , "up"      );
+	CursorMan.add("target_cone_pressed"        , "target_cone"        , "down"    );
+	CursorMan.add("target_cone_invalid"        , "target_cone"        , "invalid" );
+	CursorMan.add("target_creature"            , "target_creature"    , "up"      );
+	CursorMan.add("target_creature_pressed"    , "target_creature"    , "down"    );
+	CursorMan.add("target_creature_invalid"    , "target_creature"    , "invalid" );
+	CursorMan.add("target_creaturelock"        , "target_creaturelock", "up"      );
+	CursorMan.add("target_creaturelock_pressed", "target_creaturelock", "down"    );
+	CursorMan.add("topple"                     , "topple"             , "up"      );
+	CursorMan.add("topple_pressed"             , "topple"             , "down"    );
+	CursorMan.add("trigger_trap"               , "trigger_trap"       , "up"      );
+	CursorMan.add("trigger_trap_pressed"       , "trigger_trap"       , "down"    );
+	CursorMan.add("unlock"                     , "unlock"             , "up"      );
+	CursorMan.add("unlock_pressed"             , "unlock"             , "down"    );
+	CursorMan.add("use"                        , "use"                , "up"      );
+	CursorMan.add("use_pressed"                , "use"                , "down"    );
+
+	CursorMan.setDefault("standard", "up");
 }
 
 } // End of namespace DragonAge2
