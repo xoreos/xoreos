c87f22ce3ba13ba14678df09e66b849c8078bc25
Michael D <michael@drueing.de>
2013-01-15 19:22:15 +0100

    COMMON: add a better workaround for {,v}snprintf
    Since Microsoft's functions are not C99 compliant, we use a
    workaround by Valentin Milea found here:
    http://stackoverflow.com/questions/2915672/snprintf-and-visual-studio-2010

--- src/common/system.h
+++ src/common/system.h
@@ -38,8 +38,11 @@
 
 #if defined(_MSC_VER)
 
-	#define snprintf _snprintf
-	#define vsnprintf _vsnprintf
+	#include <cstdarg>
+	#include <cstdio>
+
+	#define snprintf c99_snprintf
+	#define vsnprintf c99_vsnprintf
 
 	#define XOREOS_LITTLE_ENDIAN
 
@@ -50,6 +53,30 @@
 	#define FORCEINLINE __forceinline
 	#define NORETURN_PRE __declspec(noreturn)
 	#define PLUGIN_EXPORT __declspec(dllexport)
+
+	static FORCEINLINE int c99_vsnprintf(char* str, size_t size, const char* format, va_list ap)
+	{
+		int count = -1;
+
+		if (size != 0)
+			count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
+		if (count == -1)
+			count = _vscprintf(format, ap);
+
+		return count;
+	}
+
+	static FORCEINLINE int c99_snprintf(char* str, size_t size, const char* format, ...)
+	{
+		int count;
+		va_list ap;
+
+		va_start(ap, format);
+		count = c99_vsnprintf(str, size, format, ap);
+		va_end(ap);
+
+		return count;
+	}
 
 	#ifndef WIN32
 		#define WIN32
