c6c02af49d3d21da4b85649d0eec17fefb9d26c2
Beren Minor <beren.minor+git@gmail.com>
2013-01-23 01:13:35 +0100

    BUILD: Add scripts to parse all Makefile.am and build with CMake.
    CMake itself parses the configure.ac file to get the list of Makefile.am, and
    parses each Makefile to get the libraries/binaries and their sources and link
    dependencies.
    
    The target are registered in CMake and it can be used to generate whatever
    CMake can generate.
    
    No change is required regarding autotools, and as long as the Makefile.am files
    stay as simple as they are, the CMake script that parses them should handle any
    source addition/removal.
    
    There's still duplication in term of library checks and platform flags, which
    might be more difficult to replace, but this is probably not going to change
    that often.

---
+++ cmake/toolchain/i686-windows-mingw-clang.cmake
@@ -0,0 +1,28 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Windows)
+
+# Choose an appropriate compiler prefix
+set(CMAKE_TOOLCHAIN_PREFIX "i686-w64-mingw32")
+
+
+# which compilers to use for C and C++
+find_program(CMAKE_RC_COMPILER  NAMES ${CMAKE_TOOLCHAIN_PREFIX}-windres)
+find_program(CMAKE_C_COMPILER   NAMES ${CMAKE_TOOLCHAIN_PREFIX}-gcc)
+find_program(CMAKE_CXX_COMPILER NAMES ${CMAKE_TOOLCHAIN_PREFIX}-g++)
+find_program(CMAKE_ASM_COMPILER NAMES ${CMAKE_TOOLCHAIN_PREFIX}-as)
+
+set(CMAKE_C_COMPILER   "clang"   "-target ${CMAKE_TOOLCHAIN_PREFIX} -isystem /usr/${CMAKE_TOOLCHAIN_PREFIX}/include -isystem /usr/local/${CMAKE_TOOLCHAIN_PREFIX}/include")
+set(CMAKE_CXX_COMPILER "clang++" "-target ${CMAKE_TOOLCHAIN_PREFIX} -isystem /usr/${CMAKE_TOOLCHAIN_PREFIX}/include -isystem /usr/local/${CMAKE_TOOLCHAIN_PREFIX}/include")
+
+
+# here is the target environment located
+set(CMAKE_FIND_ROOT_PATH /usr/${CMAKE_TOOLCHAIN_PREFIX} /usr/local/${CMAKE_TOOLCHAIN_PREFIX})
+
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_CROSS_COMPILING TRUE)
