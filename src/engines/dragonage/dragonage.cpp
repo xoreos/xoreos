a83aeef2e8eee654ade6ded73de97d4053ffc8c9
Sven Hesse <drmccoy@users.sourceforge.net>
2014-01-22 01:03:24 +0100

    DRAGONAGE: Display a rotating textured cube

--- src/engines/dragonage/dragonage.cpp
+++ src/engines/dragonage/dragonage.cpp
@@ -35,8 +35,11 @@
 #include "aurora/resman.h"
 
 #include "graphics/cursorman.h"
+#include "graphics/cameraman.h"
 
+#include "graphics/aurora/sceneman.h"
 #include "graphics/aurora/fps.h"
+#include "graphics/aurora/cube.h"
 
 #include "sound/sound.h"
 
@@ -118,9 +121,20 @@ void DragonAgeEngine::run(const Common::UString &target) {
 	if (ConfigMan.getBool("showfps", false))
 		fps = new Graphics::Aurora::FPS;
 
+	CameraMan.setPosition(0.0, 0.0, 5.0);
+	CameraMan.lookAt(0.0, 0.0, 0.0);
+
+	Graphics::Aurora::Cube *cube = SceneMan.createCube("ach_abi_accomplish_rog");
+
+	cube->setPosition(0.0, 0.0, 0.0);
+	cube->startRotate();
+	cube->setVisible(true);
+
 	while (!EventMan.quitRequested()) {
 		EventMan.delay(10);
 	}
+
+	SceneMan.destroy(cube);
 
 	delete fps;
 }
