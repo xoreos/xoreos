03bafddd5a229c1dcf083a869f112892d1fccddf
Sven Hesse <drmccoy@users.sourceforge.net>
2014-08-19 23:53:24 +0200

    ALL: Update the copyright header
    Replace "this software" with "xoreos" to make it more explicit
    and link to the GPL on the web instead of suggesting to write
    a letter to the FSF.

--- src/common/configman.h
+++ src/common/configman.h
@@ -1,22 +1,21 @@
 /* xoreos - A reimplementation of BioWare's Aurora engine
  *
- * xoreos is the legal property of its developers, whose names can be
- * found in the AUTHORS file distributed with this source
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
  * distribution.
  *
- * This program is free software; you can redistribute it and/or
+ * xoreos is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * xoreos is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Inspired by ScummVM's config file and manager code
