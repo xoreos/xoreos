7e0da5fe776c8d3fa54c99929226df50f34ae83f
Sven Hesse <drmccoy@users.sourceforge.net>
2014-08-19 23:49:05 +0200

    ALL: Remove wrong-headed copyright note
    See issue #47.
    Instead, we list the trademarks in the AUTHORS now.

--- src/engines/nwn/script/functions_300.cpp
+++ src/engines/nwn/script/functions_300.cpp
@@ -17,10 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- *
- * The Infinity, Aurora, Odyssey, Eclipse and Lycium engines, Copyright (c) BioWare corp.
- * The Electron engine, Copyright (c) Obsidian Entertainment and BioWare corp.
  */
 
 /** @file engines/nwn/script/functions_300.cpp
