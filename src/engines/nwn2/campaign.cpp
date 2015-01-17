0aece5ff708725e7d767c3e7a95290a19be5f7db
Sven Hesse <drmccoy@users.sourceforge.net>
2015-01-17 08:46:24 +0100

    NWN2: Add a class managing Campaigns
    Right now, it finds and reads the names of all campaigns.

---
+++ src/engines/nwn2/campaign.cpp
@@ -0,0 +1,101 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file engines/nwn2/campaign.cpp
+ *  The context holding an NWN2 campaign.
+ */
+
+#include "common/util.h"
+#include "common/file.h"
+#include "common/filepath.h"
+#include "common/filelist.h"
+
+#include "aurora/resman.h"
+#include "aurora/gfffile.h"
+
+#include "engines/nwn2/campaign.h"
+
+namespace Engines {
+
+namespace NWN2 {
+
+Campaign::Campaign() {
+	findCampaigns();
+}
+
+Campaign::~Campaign() {
+}
+
+const std::list<CampaignDescription> &Campaign::getCampaigns() const {
+	return _campaigns;
+}
+
+void Campaign::findCampaigns() {
+	Common::UString baseDir = ResMan.getDataBaseDir();
+
+	Common::UString campaignBaseDir = Common::FilePath::findSubDirectory(baseDir, "campaigns", true);
+	if (campaignBaseDir.empty())
+		return;
+
+	Common::FileList campaignFiles;
+	if (!campaignFiles.addDirectory(campaignBaseDir, -1))
+		return;
+
+	Common::FileList camFiles;
+	if (!campaignFiles.getSubList("campaign.cam", true, camFiles))
+		return;
+
+	for (Common::FileList::const_iterator c = camFiles.begin(); c != camFiles.end(); ++c) {
+		CampaignDescription desc;
+
+		desc.directory = Common::FilePath::relativize(baseDir, Common::FilePath::getDirectory(*c));
+		if (!readCampaign(*c, desc))
+			continue;
+
+		_campaigns.push_back(desc);
+	}
+}
+
+bool Campaign::readCampaign(const Common::UString &camFile, CampaignDescription &desc) {
+	Common::File *file = new Common::File;
+	if (!file->open(camFile)) {
+		delete file;
+		return false;
+	}
+
+	Aurora::GFFFile *gff = 0;
+	try {
+		gff = new Aurora::GFFFile(file, MKTAG('C', 'A', 'M', ' '));
+	} catch (...) {
+		delete gff;
+		return false;
+	}
+
+	gff->getTopLevel().getLocString("DisplayName", desc.name);
+	gff->getTopLevel().getLocString("Description", desc.description);
+
+	delete gff;
+
+	return true;
+}
+
+} // End of namespace NWN2
+
+} // End of namespace Engines
---
+++ src/engines/nwn2/campaign.h
@@ -0,0 +1,63 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file engines/nwn2/campaign.h
+ *  The context holding an NWN2 campaign.
+ */
+
+#ifndef ENGINES_NWN2_CAMPAIGN_H
+#define ENGINES_NWN2_CAMPAIGN_H
+
+#include <list>
+
+#include "common/ustring.h"
+
+#include "aurora/locstring.h"
+
+namespace Engines {
+
+namespace NWN2 {
+
+struct CampaignDescription {
+	Common::UString directory;
+
+	Aurora::LocString name;
+	Aurora::LocString description;
+};
+
+class Campaign {
+public:
+	Campaign();
+	~Campaign();
+
+	const std::list<CampaignDescription> &getCampaigns() const;
+
+private:
+	std::list<CampaignDescription> _campaigns;
+
+	void findCampaigns();
+	bool readCampaign(const Common::UString &camFile, CampaignDescription &desc);
+};
+
+} // End of namespace NWN2
+
+} // End of namespace Engines
+
+#endif // ENGINES_NWN2_CAMPAIGN_H
