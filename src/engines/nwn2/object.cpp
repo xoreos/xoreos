c2510b236f093f15558d35d608646d45730ba5a2
Sven Hesse <drmccoy@users.sourceforge.net>
2015-01-19 04:59:07 +0100

    NWN2: Add an Object class
    Mostly copied from NWN.

---
+++ src/engines/nwn2/object.cpp
@@ -0,0 +1,193 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file engines/nwn2/object.h
+ *  An object within a NWN2 area.
+ */
+
+#include "common/util.h"
+#include "common/error.h"
+
+#include "aurora/ssffile.h"
+#include "aurora/2dafile.h"
+#include "aurora/2dareg.h"
+#include "aurora/dlgfile.h"
+
+#include "aurora/nwscript/types.h"
+#include "aurora/nwscript/util.h"
+#include "aurora/nwscript/functioncontext.h"
+#include "aurora/nwscript/functionman.h"
+
+#include "sound/sound.h"
+
+#include "engines/aurora/util.h"
+#include "engines/aurora/tokenman.h"
+
+#include "engines/nwn2/types.h"
+#include "engines/nwn2/object.h"
+
+namespace Engines {
+
+namespace NWN2 {
+
+Object::Object(ObjectType type) : _type(type),
+	_soundSet(Aurora::kFieldIDInvalid), _ssf(0), _static(false), _usable(true),
+	_area(0) {
+
+	_position   [0] = 0.0;
+	_position   [1] = 0.0;
+	_position   [2] = 0.0;
+	_orientation[0] = 0.0;
+	_orientation[1] = 0.0;
+	_orientation[2] = 0.0;
+}
+
+Object::~Object() {
+	delete _ssf;
+}
+
+ObjectType Object::getType() const {
+	return _type;
+}
+
+void Object::loadModel() {
+}
+
+void Object::unloadModel() {
+}
+
+void Object::show() {
+}
+
+void Object::hide() {
+}
+
+const Common::UString &Object::getName() const {
+	return _name;
+}
+
+const Common::UString &Object::getDescription() const {
+	return _description;
+}
+
+const Common::UString &Object::getConversation() const {
+	return _conversation;
+}
+
+const Aurora::SSFFile *Object::getSSF() {
+	loadSSF();
+
+	return _ssf;
+}
+
+bool Object::isStatic() const {
+	return _static;
+}
+
+bool Object::isUsable() const {
+	return _usable;
+}
+
+bool Object::isClickable() const {
+	return !_static && _usable;
+}
+
+const std::list<uint32> &Object::getIDs() const {
+	return _ids;
+}
+
+Area *Object::getArea() const {
+	return _area;
+}
+
+void Object::setArea(Area *area) {
+	_area = area;
+}
+
+Location Object::getLocation() const {
+	// TODO: Object::getLocation(): Facing
+
+	Location location;
+
+	location.setArea(_area);
+	location.setPosition(_position[0], _position[1], _position[2]);
+	location.setFacing(0.0f);
+
+	return location;
+}
+
+void Object::getPosition(float &x, float &y, float &z) const {
+	x = _position[0];
+	y = _position[1];
+	z = _position[2];
+}
+
+void Object::getOrientation(float &x, float &y, float &z) const {
+	x = _orientation[0];
+	y = _orientation[1];
+	z = _orientation[2];
+}
+
+void Object::setPosition(float x, float y, float z) {
+	_position[0] = x;
+	_position[1] = y;
+	_position[2] = z;
+}
+
+void Object::setOrientation(float x, float y, float z) {
+	_orientation[0] = x;
+	_orientation[1] = y;
+	_orientation[2] = z;
+}
+
+void Object::loadSSF() {
+	if (_ssf || (_soundSet == Aurora::kFieldIDInvalid))
+		return;
+
+	const Aurora::TwoDAFile &soundSets = TwoDAReg.get("soundset");
+
+	Common::UString ssfFile = soundSets.getRow(_soundSet).getString("RESREF");
+	if (ssfFile.empty())
+		return;
+
+	try {
+		_ssf = new Aurora::SSFFile(ssfFile);
+	} catch (...) {
+		warning("Failed to load SSF \"%s\" (object \"%s\")", ssfFile.c_str(), _tag.c_str());
+		delete _ssf;
+		_ssf = 0;
+	}
+}
+
+void Object::stopSound() {
+	SoundMan.stopChannel(_sound);
+}
+
+void Object::playSound(const Common::UString &sound, bool pitchVariance) {
+	stopSound();
+	if (sound.empty())
+		return;
+
+	_sound = ::Engines::playSound(sound, Sound::kSoundTypeVoice, false, 1.0, pitchVariance);
+}
+
+} // End of namespace NWN2
+
+} // End of namespace Engines
---
+++ src/engines/nwn2/object.h
@@ -0,0 +1,156 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file engines/nwn2/object.h
+ *  An object within a NWN2 area.
+ */
+
+#ifndef ENGINES_NWN2_OBJECT_H
+#define ENGINES_NWN2_OBJECT_H
+
+#include <list>
+
+#include "common/types.h"
+#include "common/ustring.h"
+
+#include "aurora/nwscript/object.h"
+
+#include "sound/types.h"
+
+#include "engines/nwn2/types.h"
+#include "engines/nwn2/location.h"
+
+namespace Aurora {
+	class SSFFile;
+
+	namespace NWScript {
+		class Object;
+	}
+}
+
+namespace Engines {
+
+namespace NWN2 {
+
+class Area;
+
+/** An object within a NWN2 area. */
+class Object : public Aurora::NWScript::Object {
+public:
+	virtual ~Object();
+
+	/** Return the exact type of the object. */
+	ObjectType getType() const;
+
+	// Basic visuals
+
+	virtual void loadModel();   ///< Load the object's model(s).
+	virtual void unloadModel(); ///< Unload the object's model(s).
+
+	virtual void show(); ///< Show the object's model(s).
+	virtual void hide(); ///< Hide the object's model(s).
+
+	/** Return the object's model IDs. */
+	const std::list<uint32> &getIDs() const;
+
+	// Basic properties
+
+	/** Return the object's name. */
+	const Common::UString &getName() const;
+	/** Return the object's description. */
+	const Common::UString &getDescription() const;
+
+	/** Return the object's default conversation (DLG). */
+	const Common::UString &getConversation() const;
+
+	/** Return the object's sound set. */
+	const Aurora::SSFFile *getSSF();
+
+	// Interactive properties
+
+	bool isStatic() const; ///< Is the object static (not manipulatable at all)?
+	bool isUsable() const; ///< Can the object be used by the PC?
+
+	bool isClickable() const; ///< Can the player click the object?
+
+	// Object/Object interactions
+
+	/** Return the area this object is currently in. */
+	Area *getArea() const;
+	/** Set the area this object is currently in. */
+	void setArea(Area *);
+
+	// Positioning
+
+	/** Return the object's position within its area. */
+	virtual void getPosition(float &x, float &y, float &z) const;
+	/** Return the object's orientation. */
+	virtual void getOrientation(float &x, float &y, float &z) const;
+
+	/** Set the object's position within its area. */
+	virtual void setPosition(float x, float y, float z);
+	/** Set the object's orientation. */
+	virtual void setOrientation(float x, float y, float z);
+
+	/** Create a Location out of the object's area, position and orientation. */
+	Location getLocation() const;
+
+	// Sound
+
+	/** Stop the current object sound. */
+	void stopSound();
+	/** Play an object sound. */
+	void playSound(const Common::UString &sound, bool pitchVariance = false);
+
+protected:
+	ObjectType _type; ///< The object's type.
+
+	Common::UString _name;        ///< The object's display name.
+	Common::UString _description; ///< The object's description.
+
+	Common::UString _conversation; ///< The object's default conversation.
+
+	uint32 _soundSet;      ///< The object's sound set, as an index into soundset.2da.
+	Aurora::SSFFile *_ssf; ///< The object's sound set.
+
+	bool _static; ///< Is the object static?
+	bool _usable; ///< Is the object usable?
+
+	std::list<uint32> _ids; ///< The object's model IDs.
+
+	Area *_area; ///< The area the object is currently in.
+
+	float _position[3];    ///< The object's position.
+	float _orientation[3]; ///< The object's orientation.
+
+	Sound::ChannelHandle _sound; ///< The currently playing object sound.
+
+
+	Object(ObjectType type);
+
+	/** Load the object's sound set. */
+	void loadSSF();
+};
+
+} // End of namespace NWN2
+
+} // End of namespace Engines
+
+#endif // ENGINES_NWN2_OBJECT_H
