c0e25bf2870a1d5da995ede08a9a9da4ad9cccca
Sven Hesse <drmccoy@users.sourceforge.net>
2015-01-24 03:39:49 +0100

    GRAPHICS: Add a simple GeometryObject renderable

---
+++ src/graphics/aurora/geometryobject.cpp
@@ -0,0 +1,189 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file graphics/aurora/geometryobject.cpp
+ *  A simple 3D object.
+ */
+
+#include "common/util.h"
+
+#include "graphics/graphics.h"
+
+#include "graphics/aurora/geometryobject.h"
+#include "graphics/aurora/textureman.h"
+
+namespace Graphics {
+
+namespace Aurora {
+
+GeometryObject::GeometryObject(const VertexBuffer &vBuf, const IndexBuffer &iBuf) :
+	Renderable(kRenderableTypeObject), _vertexBuffer(vBuf), _indexBuffer(iBuf) {
+
+	_position[0] = 0.0f;
+	_position[1] = 0.0f;
+	_position[2] = 0.0f;
+	_rotation[0] = 0.0f;
+	_rotation[1] = 0.0f;
+	_rotation[2] = 0.0f;
+}
+
+GeometryObject::~GeometryObject() {
+}
+
+void GeometryObject::getPosition(float &x, float &y, float &z) const {
+	x = _position[0];
+	y = _position[1];
+	z = _position[2];
+}
+
+void GeometryObject::getRotation(float &x, float &y, float &z) const {
+	x = _rotation[0];
+	y = _rotation[1];
+	z = _rotation[2];
+}
+
+void GeometryObject::setPosition(float x, float y, float z) {
+	GfxMan.lockFrame();
+
+	_position[0] = x;
+	_position[1] = y;
+	_position[2] = z;
+
+	calculateDistance();
+
+	resort();
+
+	GfxMan.unlockFrame();
+}
+
+void GeometryObject::setRotation(float x, float y, float z) {
+	GfxMan.lockFrame();
+
+	_rotation[0] = x;
+	_rotation[1] = y;
+	_rotation[2] = z;
+
+	calculateDistance();
+
+	resort();
+
+	GfxMan.unlockFrame();
+}
+
+void GeometryObject::move(float x, float y, float z) {
+	setPosition(_position[0] + x, _position[1] + y, _position[2] + z);
+}
+
+void GeometryObject::rotate(float x, float y, float z) {
+	setRotation(_rotation[0] + x, _rotation[1] + y, _rotation[2] + z);
+}
+
+void GeometryObject::calculateDistance() {
+	_distance = 0;
+}
+
+static void EnableVertexPos(const VertexAttrib &va) {
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(va.size, va.type, va.stride, va.pointer);
+}
+
+static void EnableVertexNorm(const VertexAttrib &va) {
+	assert(va.size == 3);
+	glEnableClientState(GL_NORMAL_ARRAY);
+	glNormalPointer(va.type, va.stride, va.pointer);
+}
+
+static void EnableVertexCol(const VertexAttrib &va) {
+	glEnableClientState(GL_COLOR_ARRAY);
+	glColorPointer(va.size, va.type, va.stride, va.pointer);
+}
+
+static void EnableVertexTex(const VertexAttrib &va) {
+	glClientActiveTextureARB(GL_TEXTURE0 + va.index - VTCOORD);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glTexCoordPointer(va.size, va.type, va.stride, va.pointer);
+}
+
+static void DisableVertexPos(const VertexAttrib &UNUSED(va)) {
+	glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+static void DisableVertexNorm(const VertexAttrib &UNUSED(va)) {
+	glDisableClientState(GL_NORMAL_ARRAY);
+}
+
+static void DisableVertexCol(const VertexAttrib &UNUSED(va)) {
+	glDisableClientState(GL_COLOR_ARRAY);
+}
+
+static void DisableVertexTex(const VertexAttrib &va) {
+	glClientActiveTextureARB(GL_TEXTURE0 + va.index - VTCOORD);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+}
+static void EnableVertexAttrib(const VertexAttrib &va) {
+	if (va.index == VPOSITION)
+		EnableVertexPos(va);
+	else if (va.index == VNORMAL)
+		EnableVertexNorm(va);
+	else if (va.index == VCOLOR)
+		EnableVertexCol(va);
+	else if (va.index >= VTCOORD)
+		EnableVertexTex(va);
+}
+
+static void DisableVertexAttrib(const VertexAttrib &va) {
+	if (va.index == VPOSITION)
+		DisableVertexPos(va);
+	else if (va.index == VNORMAL)
+		DisableVertexNorm(va);
+	else if (va.index == VCOLOR)
+		DisableVertexCol(va);
+	else if (va.index >= VTCOORD)
+		DisableVertexTex(va);
+}
+
+void GeometryObject::render(RenderPass pass) {
+	if (pass == kRenderPassTransparent)
+		return;
+
+	glTranslatef(_position[0], _position[1], _position[2]);
+
+	glRotatef( _rotation[0], 1.0, 0.0, 0.0);
+	glRotatef( _rotation[1], 0.0, 1.0, 0.0);
+	glRotatef(-_rotation[2], 0.0, 0.0, 1.0);
+
+	TextureMan.reset();
+
+	// Render the node's faces
+
+	const VertexDecl &vertexDecl = _vertexBuffer.getVertexDecl();
+
+	for (uint32 i = 0; i < vertexDecl.size(); i++)
+		EnableVertexAttrib(vertexDecl[i]);
+
+	glDrawElements(GL_TRIANGLES, _indexBuffer.getCount(), _indexBuffer.getType(), _indexBuffer.getData());
+
+	for (uint32 i = 0; i < vertexDecl.size(); i++)
+		DisableVertexAttrib(vertexDecl[i]);
+}
+
+} // End of namespace Aurora
+
+} // End of namespace Graphics
---
+++ src/graphics/aurora/geometryobject.h
@@ -0,0 +1,74 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file graphics/aurora/geometryobject.h
+ *  A simple 3D object.
+ */
+
+#ifndef GRAPHICS_AURORA_GEOMETRYOBJECT_H
+#define GRAPHICS_AURORA_GEOMETRYOBJECT_H
+
+#include "graphics/renderable.h"
+#include "graphics/indexbuffer.h"
+#include "graphics/vertexbuffer.h"
+
+namespace Graphics {
+
+namespace Aurora {
+
+class GeometryObject : public Renderable {
+public:
+	GeometryObject(const VertexBuffer &vBuf, const IndexBuffer &iBuf);
+	~GeometryObject();
+
+	// Positioning
+
+	/** Get the current position of the model. */
+	void getPosition(float &x, float &y, float &z) const;
+	/** Get the current rotation of the model. */
+	void getRotation(float &x, float &y, float &z) const;
+
+	/** Set the current position of the model. */
+	void setPosition(float x, float y, float z);
+	/** Set the current rotation of the model. */
+	void setRotation(float x, float y, float z);
+
+	/** Move the model, relative to its current position. */
+	void move  (float x, float y, float z);
+	/** Rotate the model, relative to its current rotation. */
+	void rotate(float x, float y, float z);
+
+	// Renderable
+	void calculateDistance();
+	void render(RenderPass pass);
+
+private:
+	VertexBuffer _vertexBuffer;
+	IndexBuffer  _indexBuffer;
+
+	float _position[3];
+	float _rotation[3];
+};
+
+} // End of namespace Aurora
+
+} // End of namespace Graphics
+
+#endif // GRAPHICS_AURORA_GEOMETRYOBJECT_H
