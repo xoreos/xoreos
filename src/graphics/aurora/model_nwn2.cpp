a7ed9995c3e9504f7ab37c34c54c31a687469c08
Sven Hesse <drmccoy@users.sourceforge.net>
2015-01-27 16:32:19 +0100

    GRAPHICS: Comment ModelNode_NWN2::createTint() a bit better

--- src/graphics/aurora/model_nwn2.cpp
+++ src/graphics/aurora/model_nwn2.cpp
@@ -489,6 +489,15 @@ void ModelNode_NWN2::removeTint() {
 	_tintedMapIndex = -1;
 }
 
+/* Create a tinted texture by combining the tint map with the tint colors.
+ *
+ * This is currently all done here in software and has several drawbacks:
+ * - Slow
+ * - We're creating lots of uncompressed RBGA texture, so it
+ *   takes up a lot of memory, both on the GPU and in system RAM
+ *
+ * TODO: We really need to do this in shaders in the future.
+ */
 void ModelNode_NWN2::createTint() {
 	if (_tintMap.empty())
 		return;
@@ -496,27 +505,31 @@ void ModelNode_NWN2::createTint() {
 	ImageDecoder *tintMap   = 0;
 	Surface      *tintedMap = 0;
 	try {
+		// Load and uncompress the texture
 		tintMap = Texture::loadImage(_tintMap);
-
 		if (tintMap->isCompressed())
 			tintMap->decompress();
 
 		const ImageDecoder::MipMap &tintImg = tintMap->getMipMap(0);
 
+		// Create a new target surface with the same dimensions
 		tintedMap = new Surface(tintImg.width, tintImg.height);
-
 		ImageDecoder::MipMap &tintedImg = tintedMap->getMipMap();
 
+		// Iterate over all pixels: read values, mix tints, write pixel to target surface
 		for (int n = 0; n < tintImg.width * tintImg.height; n++) {
 			float srcColor[4], dstColor[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
 			tintImg.getPixel(n, srcColor[0], srcColor[1], srcColor[2], srcColor[3]);
 
 			if (srcColor[3] != 0.0f) {
+				// Mix using the value and the alpha components as intensities
+				// TODO: Verify how the mixing is actually done in NWN2!
 				for (int i = 0; i < 3; i++) {
 					for (int j = 0; j < 3; j++)
 						dstColor[j] += _tint[i][j] * srcColor[i] * _tint[i][3] * srcColor[3] * _diffuse[i];
 				}
 			} else
+				// Source alpha is 0.0f: No tinting for this pixel
 				for (int i = 0; i < 3; i++)
 					dstColor[i] = _diffuse[i] * _tint[i][3];
 
@@ -531,6 +544,7 @@ void ModelNode_NWN2::createTint() {
 
 	delete tintMap;
 
+	// And add the new texture to the TextureManager
 	TextureHandle tintedTexture = TextureMan.add(new Texture(tintedMap));
 
 	_textures.push_back(tintedTexture);
