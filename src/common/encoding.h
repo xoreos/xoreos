ec3f7a443497eb6cc9327befb679986072e42c4a
Sven Hesse <drmccoy@users.sourceforge.net>
2015-02-20 23:00:37 +0100

    COMMON: Add an Encoding enum

---
+++ src/common/encoding.h
@@ -0,0 +1,40 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file common/encoding.h
+ *  Utility functions for working with differing string encodings.
+ */
+
+#ifndef COMMON_ENCODING_H
+#define COMMON_ENCODING_H
+
+namespace Common {
+
+enum Encoding {
+	kEncodingASCII  ,
+	kEncodingLatin9 ,
+	kEncodingUTF8   ,
+	kEncodingUTF16LE,
+	kEncodingUTF16BE
+};
+
+} // End of namespace Common
+
+#endif // COMMON_ENCODING_H
