a89d7b38ade03623b55ef28fd3121a657129c185
Sven Hesse <drmccoy@users.sourceforge.net>
2015-04-03 18:15:05 +0200

    COMMON: Add fallbacks for strtoll()/strtoull() if necessary
    Based on the OpenBSD functions, released under the terms of the
    3-clause BSD license.

--- src/common/system.h
+++ src/common/system.h
@@ -137,6 +137,212 @@
 	#define HAVE_STRTOF 1
 #endif
 
+#ifndef HAVE_STRTOLL
+	#define strtoll c99_strtoll
+
+	#include <ctype.h>
+	#include <errno.h>
+	#include <limits.h>
+
+	#ifndef LLONG_MAX
+		#define LLONG_MAX   0x7FFFFFFFFFFFFFFFlL
+	#endif
+
+	#ifndef LLONG_MIN
+		#define LLONG_MIN (-0x7FFFFFFFFFFFFFFFLL-1)
+	#endif
+
+	/** Convert a string to a long long.
+	 *
+	 *  Based on OpenBSD's strtoll() function, released under the terms of
+	 *  the 3-clause BSD license.
+	 *
+	 *  Ignores 'locale' stuff.  Assumes that the upper and lower case
+	 *  alphabets and digits are each contiguous.
+	 */
+	long long c99_strtoll(const char *nptr, char **endptr, int base) {
+		const char *s;
+		long long acc, cutoff;
+		int c;
+		int neg, any, cutlim;
+
+		/*
+		 * Skip white space and pick up leading +/- sign if any.
+		 * If base is 0, allow 0x for hex and 0 for octal, else
+		 * assume decimal; if base is already 16, allow 0x.
+		 */
+		s = nptr;
+		do {
+			c = (unsigned char) *s++;
+		} while (isspace(c));
+		if (c == '-') {
+			neg = 1;
+			c = *s++;
+		} else {
+			neg = 0;
+			if (c == '+')
+				c = *s++;
+		}
+		if ((base == 0 || base == 16) &&
+		    c == '0' && (*s == 'x' || *s == 'X')) {
+			c = s[1];
+			s += 2;
+			base = 16;
+		}
+		if (base == 0)
+			base = c == '0' ? 8 : 10;
+
+		/*
+		 * Compute the cutoff value between legal numbers and illegal
+		 * numbers.  That is the largest legal value, divided by the
+		 * base.  An input number that is greater than this value, if
+		 * followed by a legal input character, is too big.  One that
+		 * is equal to this value may be valid or not; the limit
+		 * between valid and invalid numbers is then based on the last
+		 * digit.  For instance, if the range for long longs is
+		 * [-9223372036854775808..9223372036854775807] and the input base
+		 * is 10, cutoff will be set to 922337203685477580 and cutlim to
+		 * either 7 (neg==0) or 8 (neg==1), meaning that if we have
+		 * accumulated a value > 922337203685477580, or equal but the
+		 * next digit is > 7 (or 8), the number is too big, and we will
+		 * return a range error.
+		 *
+		 * Set any if any `digits' consumed; make it negative to indicate
+		 * overflow.
+		 */
+		cutoff = neg ? LLONG_MIN : LLONG_MAX;
+		cutlim = cutoff % base;
+		cutoff /= base;
+		if (neg) {
+			if (cutlim > 0) {
+				cutlim -= base;
+				cutoff += 1;
+			}
+			cutlim = -cutlim;
+		}
+		for (acc = 0, any = 0;; c = (unsigned char) *s++) {
+			if (isdigit(c))
+				c -= '0';
+			else if (isalpha(c))
+				c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+			else
+				break;
+			if (c >= base)
+				break;
+			if (any < 0)
+				continue;
+			if (neg) {
+				if (acc < cutoff || (acc == cutoff && c > cutlim)) {
+					any = -1;
+					acc = LLONG_MIN;
+					errno = ERANGE;
+				} else {
+					any = 1;
+					acc *= base;
+					acc -= c;
+				}
+			} else {
+				if (acc > cutoff || (acc == cutoff && c > cutlim)) {
+					any = -1;
+					acc = LLONG_MAX;
+					errno = ERANGE;
+				} else {
+					any = 1;
+					acc *= base;
+					acc += c;
+				}
+			}
+		}
+		if (endptr != 0)
+			*endptr = (char *) (any ? s - 1 : nptr);
+		return (acc);
+	}
+
+	#define HAVE_STRTOLL 1
+#endif
+
+#ifndef HAVE_STRTOULL
+	#define strtoull c99_strtoull
+
+	#include <ctype.h>
+	#include <errno.h>
+	#include <limits.h>
+
+	#ifndef ULLONG_MAX
+		#define ULLONG_MAX 0xFFFFFFFFFFFFFFFFULL
+	#endif
+
+	/** Convert a string to an unsigned long long.
+	 *
+	 *  Based on OpenBSD's strtoull() function, released under the terms of
+	 *  the 3-clause BSD license.
+	 *
+	 *  Ignores 'locale' stuff.  Assumes that the upper and lower case
+	 *  alphabets and digits are each contiguous.
+	 */
+	unsigned long long c99_strtoull(const char *nptr, char **endptr, int base) {
+		const char *s;
+		unsigned long long acc, cutoff;
+		int c;
+		int neg, any, cutlim;
+
+		/*
+		 * See c99_strtoll() for comments as to the logic used.
+		 */
+		s = nptr;
+		do {
+			c = (unsigned char) *s++;
+		} while (isspace(c));
+		if (c == '-') {
+			neg = 1;
+			c = *s++;
+		} else {
+			neg = 0;
+			if (c == '+')
+				c = *s++;
+		}
+		if ((base == 0 || base == 16) &&
+		    c == '0' && (*s == 'x' || *s == 'X')) {
+			c = s[1];
+			s += 2;
+			base = 16;
+		}
+		if (base == 0)
+			base = c == '0' ? 8 : 10;
+
+		cutoff = ULLONG_MAX / (unsigned long long)base;
+		cutlim = ULLONG_MAX % (unsigned long long)base;
+		for (acc = 0, any = 0;; c = (unsigned char) *s++) {
+			if (isdigit(c))
+				c -= '0';
+			else if (isalpha(c))
+				c -= isupper(c) ? 'A' - 10 : 'a' - 10;
+			else
+				break;
+			if (c >= base)
+				break;
+			if (any < 0)
+				continue;
+			if (acc > cutoff || (acc == cutoff && c > cutlim)) {
+				any = -1;
+				acc = ULLONG_MAX;
+				errno = ERANGE;
+			} else {
+				any = 1;
+				acc *= (unsigned long long)base;
+				acc += c;
+			}
+		}
+		if (neg && any > 0)
+			acc = -acc;
+		if (endptr != 0)
+			*endptr = (char *) (any ? s - 1 : nptr);
+		return (acc);
+	}
+
+	#define HAVE_STRTOULL 1
+#endif
+
 //
 // Fallbacks / default values for various special macros
 //
