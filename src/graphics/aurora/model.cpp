a141f4925ba418e05edaf86b6753ef0dbb3914de
mirv-sillyfish <mirv.sillyfish@gmail.com>
2015-04-17 16:02:11 +0200

    GRAPHICS: Model bounding box render with shaders
    Models render bounding boxes using the new shader approach. This
    includes fixes for the graphics to set _modelview correctly.

--- src/graphics/aurora/model.cpp
+++ src/graphics/aurora/model.cpp
@@ -33,6 +33,10 @@
 #include "src/graphics/aurora/animation.h"
 #include "src/graphics/aurora/modelnode.h"
 
+#include "src/graphics/shader/surfaceman.h"
+#include "src/graphics/shader/materialman.h"
+#include "src/graphics/mesh/meshman.h"
+
 using Common::kDebugGraphics;
 
 namespace Graphics {
@@ -53,6 +57,11 @@ Model::Model(ModelType type) : Renderable((RenderableType) type),
 	_elapsedTime = 0.0;
 
 	_loopAnimation = 0;
+
+	_boundRenderable = new Shader::ShaderRenderable();
+	_boundRenderable->setSurface(SurfaceMan.getSurface("defaultSurface"));
+	_boundRenderable->setMaterial(MaterialMan.getMaterial("defaultWhite"));
+	_boundRenderable->setMesh(MeshMan.getMesh("defaultWireBox"));
 }
 
 Model::~Model() {
@@ -67,6 +76,8 @@ Model::~Model() {
 
 		delete *s;
 	}
+
+	delete _boundRenderable;
 }
 
 ModelType Model::getType() const {
@@ -495,6 +506,7 @@ void Model::doDrawBound() {
 	object.getMin(minX, minY, minZ);
 	object.getMax(maxX, maxY, maxZ);
 
+	/*
 	glBegin(GL_LINE_LOOP);
 		glVertex3f(minX, minY, minZ);
 		glVertex3f(maxX, minY, minZ);
@@ -536,6 +548,12 @@ void Model::doDrawBound() {
 		glVertex3f(maxX, maxY, maxZ);
 		glVertex3f(minX, maxY, maxZ);
 	glEnd();
+	*/
+
+	Common::TransformationMatrix tform = _absolutePosition;
+	tform.translate((maxX + minX) * 0.5f, (maxY + minY) * 0.5f, (maxZ + minZ) * 0.5f);
+	tform.scale((maxX - minX) * 0.5f, (maxY - minY) * 0.5f, (maxZ - minZ) * 0.5f);
+	_boundRenderable->renderImmediate(tform);
 }
 
 void Model::doRebuild() {
