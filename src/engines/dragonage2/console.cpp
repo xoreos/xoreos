4e59f71e182070a55b856dc80f7eb46a03ca8702
Sven Hesse <drmccoy@users.sourceforge.net>
2015-05-01 11:48:48 +0200

    DRAGONAGE2: Properly initialize Console:_engine
    This fixes Coverity Scan issue #1296269.

--- src/engines/dragonage2/console.cpp
+++ src/engines/dragonage2/console.cpp
@@ -30,7 +30,8 @@ namespace Engines {
 namespace DragonAge2 {
 
 Console::Console(DragonAge2Engine &engine) :
-	::Engines::Console(engine, Graphics::Aurora::kSystemFontMono, 13) {
+	::Engines::Console(engine, Graphics::Aurora::kSystemFontMono, 13),
+	_engine(&engine) {
 
 }
 
