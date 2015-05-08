4962d23a317ed7d4945a2191624faaf42b44b5c9
Sven Hesse <drmccoy@users.sourceforge.net>
2015-05-08 12:25:05 +0200

    AURORA: Add NSBTXFile, treating NSBTX as archives of textures
    Based heavily on the BTX0 reader found in the NDS file viewer and
    and editor Tinke by pleoNeX (<https://github.com/pleonex/tinke>),
    which is licensed under the terms of the GPLv3.

---
+++ src/aurora/nsbtxfile.h
@@ -0,0 +1,164 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Treat Nintendo NSBTX files, which contain multiple textures as an
+ *  archive of intermediate textures.
+ */
+
+#ifndef AURORA_NSBTXFILE_H
+#define AURORA_NSBTXFILE_H
+
+#include "src/common/types.h"
+#include "src/common/ustring.h"
+
+#include "src/aurora/types.h"
+#include "src/aurora/archive.h"
+
+namespace Common {
+	class SeekableReadStream;
+	class SeekableSubReadStreamEndian;
+	class WriteStream;
+}
+
+namespace Aurora {
+
+/** Class to hold resource data of an HERF file. */
+class NSBTXFile : public Archive {
+public:
+	NSBTXFile(Common::SeekableReadStream *nsbtx);
+	~NSBTXFile();
+
+	/** Return the list of resources. */
+	const ResourceList &getResources() const;
+
+	/** Return the size of a resource. */
+	uint32 getResourceSize(uint32 index) const;
+
+	/** Return a stream of the resource's contents. */
+	Common::SeekableReadStream *getResource(uint32 index, bool tryNoCopy = false) const;
+
+private:
+	enum Format {
+		kFormatNoTexture     = 0, ///< Empty.
+		kFormatA3I5          = 1, ///< 3bit alpha + 5bit color index.
+		kFormat2bpp          = 2, ///< 2bit color index.
+		kFormat4bpp          = 3, ///< 4bit color index.
+		kFormat8bpp          = 4, ///< 8bit color index.
+		kFormat4x4Compressed = 5, ///< 32bit per 4x4 texel block (Unsupported).
+		kFormatA5I3          = 6, ///< 5bit alpha + 3bit color index.
+		kFormat16bpp         = 7  ///< R5B5G5A1.
+	};
+
+	enum Transform {
+		kTransformNone     = 0, ///< No extra texture coordinate transform.
+		kTransformTexCoord = 1, ///< Transform texture coordinates using TexCoords.
+		kTransformNormal   = 2, ///< Transform texture coordinates using Normals.
+		kTransformVertex   = 3  ///< Transform texture coordinates using Vertices.
+	};
+
+	struct Texture {
+		Common::UString name;
+		uint32 offset;
+
+		Format format;
+
+		uint16 width;
+		uint16 height;
+
+		bool wrapX; ///< true: wrap, false: clamp
+		bool wrapY; ///< true: wrap, false: clamp
+		bool flipX; ///< true: flip on every 2nd texture wrap
+		bool flipY; ///< true: flip on every 2nd texture wrap
+		bool alpha; ///< true: color index 0 is transparent
+
+		Transform coordTransform;
+	};
+
+	struct Palette {
+		Common::UString name;
+		uint32 offset;
+	};
+
+	struct ReadContext {
+		const Texture *texture;
+		const byte    *palette;
+
+		Common::SeekableSubReadStreamEndian *nsbtx;
+		Common::WriteStream *stream;
+
+		ReadContext(Common::SeekableSubReadStreamEndian &n, const Texture &t, Common::WriteStream &s);
+		~ReadContext();
+	};
+
+	typedef std::vector<Texture> Textures;
+	typedef std::vector<Palette> Palettes;
+
+
+	/** The name of the NSBTX file. */
+	Common::SeekableSubReadStreamEndian *_nsbtx;
+
+	/** External list of resource names and types. */
+	ResourceList _resources;
+
+	uint32 _textureOffset;
+
+	uint32 _textureInfoOffset;
+	uint32 _paletteInfoOffset;
+
+	uint32 _textureDataOffset;
+	uint32 _paletteDataOffset;
+
+	Textures _textures;
+	Palettes _palettes;
+
+
+	void load(Common::SeekableSubReadStreamEndian &nsbtx);
+
+	void readHeader    (Common::SeekableSubReadStreamEndian &nsbtx);
+	void readFileHeader(Common::SeekableSubReadStreamEndian &nsbtx);
+	void readInfoHeader(Common::SeekableSubReadStreamEndian &nsbtx);
+	void readTextures  (Common::SeekableSubReadStreamEndian &nsbtx);
+	void readPalettes  (Common::SeekableSubReadStreamEndian &nsbtx);
+
+	void createResourceList();
+
+	Common::SeekableSubReadStreamEndian *open(Common::SeekableReadStream *nsbtx) const;
+
+	const Palette *findPalette(const Texture &texture) const;
+	void getPalette(ReadContext &ctx) const;
+
+	static uint32 getITEXSize(const Texture &texture);
+
+	static void writeITEXHeader(const ReadContext &ctx);
+	static void writePixel(const ReadContext &ctx, byte r, byte g, byte b, byte a);
+
+	static void getTexture     (const ReadContext &ctx);
+	static void getTexture2bpp (const ReadContext &ctx);
+	static void getTexture4bpp (const ReadContext &ctx);
+	static void getTexture8bpp (const ReadContext &ctx);
+	static void getTexture16bpp(const ReadContext &ctx);
+	static void getTextureA3I5 (const ReadContext &ctx);
+	static void getTextureA5I3 (const ReadContext &ctx);
+};
+
+} // End of namespace Aurora
+
+#endif // AURORA_NSBTXFILE_H
