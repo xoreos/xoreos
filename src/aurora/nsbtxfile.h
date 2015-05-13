749dd6751b6cdbbebc7fe97a4f420470fc49fe62
Sven Hesse <drmccoy@users.sourceforge.net>
2015-05-13 17:48:32 +0200

    AURORA: Remove outdated/wrong comment in NSBTXFile

--- src/aurora/nsbtxfile.h
+++ src/aurora/nsbtxfile.h
@@ -112,7 +112,6 @@ private:
 	typedef std::vector<Palette> Palettes;
 
 
-	/** The name of the NSBTX file. */
 	Common::SeekableSubReadStreamEndian *_nsbtx;
 
 	/** External list of resource names and types. */
