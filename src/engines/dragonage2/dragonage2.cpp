b14278435e35cb3ebaf5ddccd3a30dfc2ba89734
Sven Hesse <drmccoy@drmccoy.de>
2015-06-13 14:19:48 +0200

    DRAGONAGE2: Explicitly tell the ResourceManager we use FNV64 hashes

--- src/engines/dragonage2/dragonage2.cpp
+++ src/engines/dragonage2/dragonage2.cpp
@@ -239,6 +239,7 @@ void DragonAge2Engine::declareEncodings() {
 
 void DragonAge2Engine::initResources(LoadProgress &progress) {
 	ResMan.setRIMsAreERFs(true);
+	ResMan.setHashAlgo(Common::kHashFNV64);
 
 	progress.step("Setting base directory");
 	ResMan.registerDataBase(_target);
