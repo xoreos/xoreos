e9a8a005bd7f74c1ce3c3325d8b8b6e9e680c17b
Sven Hesse <drmccoy@drmccoy.de>
2015-06-13 14:14:36 +0200

    DRAGONAGE: Add the usual simple model loader

---
+++ src/engines/dragonage/modelloader.h
@@ -0,0 +1,44 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Dragon Age: Origins model loader.
+ */
+
+#ifndef ENGINES_DRAGONAGE_MODELLOADER_H
+#define ENGINES_DRAGONAGE_MODELLOADER_H
+
+#include "src/engines/aurora/modelloader.h"
+
+namespace Engines {
+
+namespace DragonAge {
+
+class DragonAgeModelLoader : public ModelLoader {
+public:
+	Graphics::Aurora::Model *load(const Common::UString &resref,
+			Graphics::Aurora::ModelType type, const Common::UString &texture);
+};
+
+} // End of namespace DragonAge
+
+} // End of namespace Engines
+
+#endif // ENGINES_DRAGONAGE_MODELLOADER_H
