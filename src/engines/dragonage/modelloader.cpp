eab8bd55835f95fa75a8caf4647abffa1128cc83
Sven Hesse <drmccoy@drmccoy.de>
2015-06-28 14:30:19 +0200

    DRAGONAGE: Make model loader check for LOD models, load the highest

--- src/engines/dragonage/modelloader.cpp
+++ src/engines/dragonage/modelloader.cpp
@@ -27,6 +27,7 @@
 #include "src/common/readstream.h"
 
 #include "src/aurora/types.h"
+#include "src/aurora/resman.h"
 
 #include "src/graphics/aurora/model_dragonage.h"
 
@@ -39,7 +40,13 @@ namespace DragonAge {
 Graphics::Aurora::Model *DragonAgeModelLoader::load(const Common::UString &resref,
 		Graphics::Aurora::ModelType type, const Common::UString &UNUSED(texture)) {
 
-	return new Graphics::Aurora::Model_DragonAge(resref, type);
+	// Check if this model uses LOD. If so, load the highest
+	Common::UString name = resref;
+	if (!ResMan.hasResource(resref, ::Aurora::kFileTypeMMH) &&
+	     ResMan.hasResource(resref + "_0", ::Aurora::kFileTypeMMH))
+		name = resref + "_0";
+
+	return new Graphics::Aurora::Model_DragonAge(name, type);
 }
 
 } // End of namespace DragonAge
