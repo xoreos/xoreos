006acd25e1bb9e02b4a25f7943ae812d59f1e3ae
Sven Hesse <drmccoy@drmccoy.de>
2015-07-02 14:09:09 +0200

    AURORA: Use Common::composeString() in GFF3File

--- src/aurora/gff3file.cpp
+++ src/aurora/gff3file.cpp
@@ -449,7 +449,7 @@ Common::UString GFF3Struct::getString(const Common::UString &field,
 	    (f->type == kFieldTypeUint64) ||
 	    (f->type == kFieldTypeStrRef)) {
 
-		return Common::UString::format("%lu", getUint(field));
+		return Common::composeString(getUint(field));
 	}
 
 	if ((f->type == kFieldTypeChar  ) ||
@@ -457,27 +457,32 @@ Common::UString GFF3Struct::getString(const Common::UString &field,
 	    (f->type == kFieldTypeSint32) ||
 	    (f->type == kFieldTypeSint64)) {
 
-		return Common::UString::format("%ld", getSint(field));
+		return Common::composeString(getSint(field));
 	}
 
 	if ((f->type == kFieldTypeFloat) ||
 	    (f->type == kFieldTypeDouble)) {
 
-		return Common::UString::format("%lf", getDouble(field));
+		return Common::composeString(getDouble(field));
 	}
 
 	if (f->type == kFieldTypeVector) {
 		float x, y, z;
 
 		getVector(field, x, y, z);
-		return Common::UString::format("%f/%f/%f", x, y, z);
+		return Common::composeString(x) + "/" +
+		       Common::composeString(y) + "/" +
+		       Common::composeString(z);
 	}
 
 	if (f->type == kFieldTypeOrientation) {
 		float a, b, c, d;
 
 		getOrientation(field, a, b, c, d);
-		return Common::UString::format("%f/%f/%f/%f", a, b, c, d);
+		return Common::composeString(a) + "/" +
+		       Common::composeString(b) + "/" +
+		       Common::composeString(c) + "/" +
+		       Common::composeString(d);
 	}
 
 	throw Common::Exception("GFF3: Field is not a string(able) type");
