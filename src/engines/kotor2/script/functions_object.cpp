f4b76913144f85af535110c4a2bdba1dad09c429
Sven Hesse <drmccoy@drmccoy.de>
2015-08-18 14:10:46 +0200

    KOTOR2: Implement GetEnteringObject and GetExitingObject

---
+++ src/engines/kotor2/script/functions_object.cpp
@@ -0,0 +1,57 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Star Wars: Knights of the Old Republic engine functions messing with objects.
+ */
+
+#include "src/common/util.h"
+
+#include "src/aurora/nwscript/functioncontext.h"
+
+#include "src/engines/kotor2/types.h"
+#include "src/engines/kotor2/game.h"
+#include "src/engines/kotor2/module.h"
+#include "src/engines/kotor2/objectcontainer.h"
+#include "src/engines/kotor2/object.h"
+
+#include "src/engines/kotor2/script/functions.h"
+
+namespace Engines {
+
+namespace KotOR2 {
+
+void Functions::getClickingObject(Aurora::NWScript::FunctionContext &ctx) {
+	ctx.getReturn() = ctx.getTriggerer();
+}
+
+void Functions::getEnteringObject(Aurora::NWScript::FunctionContext &ctx) {
+	// TODO: This should return the *last* entered object, i.e. it should remember past triggerers.
+	ctx.getReturn() = ctx.getTriggerer();
+}
+
+void Functions::getExitingObject(Aurora::NWScript::FunctionContext &ctx) {
+	// TODO: This should return the *last* exited object, i.e. it should remember past triggerers.
+	ctx.getReturn() = ctx.getTriggerer();
+}
+
+} // End of namespace KotOR2
+
+} // End of namespace Engines
