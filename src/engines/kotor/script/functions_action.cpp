e63dbec88f3a2016ca987f6004849aa589df51e4
Sven Hesse <drmccoy@drmccoy.de>
2015-08-18 14:37:31 +0200

    KOTOR: Implement AssignCommand and DelayCommand

---
+++ src/engines/kotor/script/functions_action.cpp
@@ -0,0 +1,64 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Star Wars: Knights of the Old Republic engine functions assigning actions to objects.
+ */
+
+#include "src/common/error.h"
+
+#include "src/aurora/nwscript/functioncontext.h"
+
+#include "src/engines/kotor/types.h"
+#include "src/engines/kotor/game.h"
+#include "src/engines/kotor/module.h"
+#include "src/engines/kotor/objectcontainer.h"
+
+#include "src/engines/kotor/script/functions.h"
+
+namespace Engines {
+
+namespace KotOR {
+
+void Functions::assignCommand(Aurora::NWScript::FunctionContext &ctx) {
+	Common::UString script = ctx.getScriptName();
+	if (script.empty())
+		throw Common::Exception("Functions::assignCommand(): Script needed");
+
+	const Aurora::NWScript::ScriptState &state = ctx.getParams()[1].getScriptState();
+
+	_game->getModule().delayScript(script, state, getParamObject(ctx, 0), ctx.getTriggerer(), 0);
+}
+
+void Functions::delayCommand(Aurora::NWScript::FunctionContext &ctx) {
+	Common::UString script = ctx.getScriptName();
+	if (script.empty())
+		throw Common::Exception("Functions::assignCommand(): Script needed");
+
+	uint32 delay = ctx.getParams()[0].getFloat() * 1000;
+
+	const Aurora::NWScript::ScriptState &state = ctx.getParams()[1].getScriptState();
+
+	_game->getModule().delayScript(script, state, ctx.getCaller(), ctx.getTriggerer(), delay);
+}
+
+} // End of namespace KotOR
+
+} // End of namespace Engines
