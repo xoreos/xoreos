4de9ec2f340cf972fee12476b6f7d7f08b40d5e4
Sven Hesse <drmccoy@drmccoy.de>
2015-08-18 11:40:56 +0200

    KOTOR2: Use pointer to Variable in the defaults array
    This massively speeds up compilation.

--- src/engines/kotor2/script/functions.h
+++ src/engines/kotor2/script/functions.h
@@ -64,7 +64,7 @@ private:
 
 	struct FunctionDefaults {
 		uint32 id;
-		Aurora::NWScript::Variable defaults[14];
+		const Aurora::NWScript::Variable *defaults[14];
 	};
 
 	static const FunctionPointer kFunctionPointers[];
