17b4bf4c04ef656b0498c4f27a5de3226ec14788
Enrico Horn <farmboy0+git@googlemail.com>
2015-08-19 22:22:22 +0200

    JADE: Add class Creature

---
+++ src/engines/jade/creature.cpp
@@ -0,0 +1,200 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  A creature in a Jade Empire area.
+ */
+
+#include "src/common/util.h"
+#include "src/common/maths.h"
+#include "src/common/error.h"
+#include "src/common/ustring.h"
+
+#include "src/aurora/2dafile.h"
+#include "src/aurora/2dareg.h"
+#include "src/aurora/gff3file.h"
+#include "src/aurora/locstring.h"
+
+#include "src/graphics/aurora/modelnode.h"
+#include "src/graphics/aurora/model.h"
+
+#include "src/engines/aurora/util.h"
+#include "src/engines/aurora/model.h"
+
+#include "src/engines/jade/creature.h"
+
+namespace Engines {
+
+namespace Jade {
+
+Creature::Creature(const Aurora::GFF3Struct &creature) : Object(kObjectTypeCreature), _isPC(false), _appearance(Aurora::kFieldIDInvalid),
+	_headType(0), _model(0) {
+
+	load(creature);
+}
+
+Creature::Creature() : Object(kObjectTypeCreature), _isPC(false), _appearance(Aurora::kFieldIDInvalid),
+	_headType(0), _model(0) {
+}
+
+Creature::~Creature() {
+	delete _model;
+}
+
+void Creature::show() {
+	if (_model)
+		_model->show();
+}
+
+void Creature::hide() {
+	if (_model)
+		_model->hide();
+}
+
+bool Creature::isPC() const {
+	return _isPC;
+}
+
+void Creature::setPosition(float x, float y, float z) {
+	Object::setPosition(x, y, z);
+	Object::getPosition(x, y, z);
+
+	if (_model)
+		_model->setPosition(x, y, z);
+}
+
+void Creature::setOrientation(float x, float y, float z, float angle) {
+	Object::setOrientation(x, y, z, angle);
+	Object::getOrientation(x, y, z, angle);
+
+	if (_model)
+		_model->setOrientation(x, y, z, angle);
+}
+
+void Creature::load(const Aurora::GFF3Struct &creature) {
+	Common::UString temp = creature.getString("ResRef");
+
+	if (!temp.empty()) {
+		Aurora::GFF3File *cre = 0;
+		try {
+			cre = new Aurora::GFF3File(temp, Aurora::kFileTypeCRE, MKTAG('C', 'R', 'E', ' '));
+			loadBlueprint(cre->getTopLevel());
+		} catch (...) {
+			warning("Creature \"%s\" has no blueprint", temp.c_str());
+			delete cre;
+			throw;
+		}
+		delete cre;
+	}
+
+	// Appearance
+
+	if (_appearance == Aurora::kFieldIDInvalid)
+		throw Common::Exception("Creature without an appearance");
+
+	loadAppearance();
+
+	loadInstance(creature);
+}
+
+void Creature::loadBlueprint(const Aurora::GFF3Struct &gff) {
+	if (gff.hasField("Looks")) {
+		const Aurora::GFF3Struct &looks = gff.getStruct("Looks");
+
+		// Appearance
+		_appearance = looks.getSint("Appearance", _appearance);
+
+		// Head
+		if (looks.hasField("HeadType")) {
+			_headType = looks.getSint("HeadType");
+		}
+	}
+
+	// Scripts
+	readScripts(gff);
+}
+
+void Creature::loadInstance(const Aurora::GFF3Struct &instance) {
+
+	loadPositional(instance);
+
+	// Usable
+	_usable = instance.getBool("Active");
+}
+
+void Creature::loadAppearance() {
+	loadBody();
+	loadHead();
+}
+
+void Creature::loadBody() {
+	const Aurora::TwoDARow &appearance = TwoDAReg.get2DA("appearance").getRow(_appearance);
+
+	const Common::UString bodyModel = appearance.getString(Common::UString("MODELA"));
+
+	_model = loadModelObject(bodyModel);
+	if (!_model)
+		return;
+
+	_ids.push_back(_model->getID());
+
+	_model->setTag(_tag);
+	_model->setClickable(isClickable());
+}
+
+void Creature::loadHead() {
+	if (!_model || !_headType)
+		return;
+
+	const Aurora::TwoDARow &chest = TwoDAReg.get2DA("creaturehooks").getRow(3); // TODO row with label Chest
+
+	Graphics::Aurora::ModelNode *chestHook = _model->getNode(chest.getString("hook"));
+	if (!chestHook)
+		return;
+
+	const Aurora::TwoDARow &headtype = TwoDAReg.get2DA("heads").getRow(_headType);
+
+	const Common::UString headModel = headtype.getString("model");
+
+	chestHook->addChild(loadModelObject(headModel));
+}
+
+void Creature::createFakePC() {
+	_name = "Fakoo McFakeston";
+	_tag  = Common::UString::format("[PC: %s]", _name.c_str());
+
+	_isPC = true;
+}
+
+void Creature::enter() {
+	highlight(true);
+}
+
+void Creature::leave() {
+	highlight(false);
+}
+
+void Creature::highlight(bool enabled) {
+	_model->drawBound(enabled);
+}
+
+} // End of namespace Jade
+
+} // End of namespace Engines
---
+++ src/engines/jade/creature.h
@@ -0,0 +1,99 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  A creature in a Jade Empire area.
+ */
+
+#ifndef ENGINES_JADE_CREATURE_H
+#define ENGINES_JADE_CREATURE_H
+
+#include "src/common/types.h"
+#include "src/common/ustring.h"
+
+#include "src/aurora/types.h"
+
+#include "src/graphics/aurora/types.h"
+
+#include "src/engines/jade/object.h"
+
+namespace Engines {
+
+namespace Jade {
+
+class Creature : public Object {
+public:
+	/** Create a dummy creature instance. Not playable as it is.*/
+	Creature();
+	/** Load from a creature instance. */
+	Creature(const Aurora::GFF3Struct &creature);
+	~Creature();
+
+	/** Create a fake player character creature for testing purposes. */
+	void createFakePC();
+
+	// Basic visuals
+
+	void show(); ///< Show the creature's model.
+	void hide(); ///< Hide the creature's model.
+
+	// Basic properties
+
+	bool isPC() const; ///< Is the creature a player character?
+
+	// Positioning
+
+	/** Set the creature's position. */
+	void setPosition(float x, float y, float z);
+	/** Set the creature's orientation. */
+	void setOrientation(float x, float y, float z, float angle);
+
+	// Object/Cursor interactions
+
+	void enter(); ///< The cursor entered the creature. */
+	void leave(); ///< The cursor left the creature. */
+
+	/** (Un)Highlight the creature. */
+	virtual void highlight(bool enabled);
+
+private:
+	bool _isPC; ///< Is the creature a PC?
+
+	uint32 _appearance; ///< The creature's general appearance.
+	uint32 _headType;   ///< The creature's type of head if seperate from the model.
+
+	Graphics::Aurora::Model *_model; ///< The creature's model. */
+
+
+	void load(const Aurora::GFF3Struct &creature);
+
+	void loadBlueprint(const Aurora::GFF3Struct &gff);
+	void loadInstance(const Aurora::GFF3Struct &gff);
+	void loadAppearance();
+
+	void loadBody();
+	void loadHead();
+};
+
+} // End of namespace Jade
+
+} // End of namespace Engines
+
+#endif // ENGINES_JADE_CREATURE_H
