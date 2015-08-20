25e0274b1b2105952a3c8c06c206fe81f1298a7d
Sven Hesse <drmccoy@drmccoy.de>
2015-08-20 20:41:46 +0200

    NWN2: Add and stub all engine functions
    Currently, none of the engine functions are implemented.

---
+++ src/engines/nwn2/script/functions.h
@@ -0,0 +1,101 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Star Wars: Knights of the Old Republic engine functions.
+ */
+
+#ifndef ENGINES_NWN2_SCRIPT_FUNCTIONS_H
+#define ENGINES_NWN2_SCRIPT_FUNCTIONS_H
+
+#include "src/aurora/nwscript/types.h"
+
+namespace Aurora {
+	namespace NWScript {
+		class FunctionContext;
+		class Object;
+	}
+}
+
+namespace Engines {
+
+namespace NWN2 {
+
+class Game;
+class Area;
+class Object;
+
+class Functions {
+public:
+	Functions(Game &game);
+	~Functions();
+
+private:
+	typedef void (Functions::*funcPtr)(Aurora::NWScript::FunctionContext &ctx);
+
+	struct FunctionPointer {
+		uint32 id;
+		const char *name;
+		funcPtr func;
+	};
+
+	struct FunctionSignature {
+		uint32 id;
+		Aurora::NWScript::Type returnType;
+		Aurora::NWScript::Type parameters[13];
+	};
+
+	struct FunctionDefaults {
+		uint32 id;
+		const Aurora::NWScript::Variable *defaults[10];
+	};
+
+	static const FunctionPointer kFunctionPointers[];
+	static const FunctionSignature kFunctionSignatures[];
+	static const FunctionDefaults kFunctionDefaults[];
+
+
+	Game *_game;
+
+	void registerFunctions();
+
+	// .--- Utility methods
+	void jumpTo(NWN2::Object *object, Area *area, float x, float y, float z);
+
+	static int32 getRandom(int min, int max, int32 n = 1);
+
+	static Common::UString formatTag(const Aurora::NWScript::Object *object);
+	static Common::UString formatParams(const Aurora::NWScript::FunctionContext &ctx);
+
+	static Common::UString formatFloat(float f, int width = 18, int decimals = 9);
+
+	static Aurora::NWScript::Object *getParamObject(const Aurora::NWScript::FunctionContext &ctx, size_t n);
+	// '---
+
+	// --- Engine functions ---
+
+	void unimplementedFunction(Aurora::NWScript::FunctionContext &ctx);
+};
+
+} // End of namespace NWN2
+
+} // End of namespace Engines
+
+#endif // ENGINES_NWN2_SCRIPT_FUNCTIONS_H
