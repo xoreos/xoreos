3f01949e283a04d0d44d450787d4181e282c2650
Sven Hesse <drmccoy@drmccoy.de>
2015-08-25 00:35:12 +0200

    KOTOR2: Also put static objects into the module's object container
    The scripts still need to query the position of static objects, for
    example.

--- src/engines/kotor2/area.cpp
+++ src/engines/kotor2/area.cpp
@@ -91,8 +91,7 @@ void Area::load() {
 
 void Area::clear() {
 	for (ObjectList::iterator o = _objects.begin(); o != _objects.end(); ++o) {
-		if (!(*o)->isStatic())
-			_module->removeObject(**o);
+		_module->removeObject(**o);
 
 		delete *o;
 	}
@@ -335,14 +334,13 @@ void Area::loadRooms() {
 
 void Area::loadObject(KotOR2::Object &object) {
 	_objects.push_back(&object);
+	_module->addObject(object);
 
 	if (!object.isStatic()) {
 		const std::list<uint32> &ids = object.getIDs();
 
 		for (std::list<uint32>::const_iterator id = ids.begin(); id != ids.end(); ++id)
 			_objectMap.insert(std::make_pair(*id, &object));
-
-		_module->addObject(object);
 	}
 }
 
