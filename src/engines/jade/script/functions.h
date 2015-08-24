5058912ea9bfd2f018511554c5f17950274d993c
Enrico Horn <farmboy0+git@googlemail.com>
2015-08-25 00:26:00 +0200

    JADE: Stub engine functions

---
+++ src/engines/jade/script/functions.h
@@ -0,0 +1,90 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Jade Empire engine functions.
+ */
+
+#ifndef ENGINES_JADE_SCRIPT_FUNCTIONS_H
+#define ENGINES_JADE_SCRIPT_FUNCTIONS_H
+
+#include "src/aurora/nwscript/types.h"
+
+namespace Aurora {
+	namespace NWScript {
+		class FunctionContext;
+		class Object;
+	}
+}
+
+namespace Engines {
+
+namespace Jade {
+
+class Functions {
+public:
+	Functions();
+	~Functions();
+
+private:
+	typedef void (Functions::*funcPtr)(Aurora::NWScript::FunctionContext &ctx);
+
+	struct FunctionPointer {
+		uint32 id;
+		const char *name;
+		funcPtr func;
+	};
+
+	struct FunctionSignature {
+		uint32 id;
+		Aurora::NWScript::Type returnType;
+		Aurora::NWScript::Type parameters[12];
+	};
+
+	struct FunctionDefaults {
+		uint32 id;
+		const Aurora::NWScript::Variable *defaults[11];
+	};
+
+	static const FunctionPointer kFunctionPointers[];
+	static const FunctionSignature kFunctionSignatures[];
+	static const FunctionDefaults kFunctionDefaults[];
+
+	void registerFunctions();
+
+	// .--- Utility methods
+	static Common::UString formatTag(const Aurora::NWScript::Object *object);
+	static Common::UString formatParams(const Aurora::NWScript::FunctionContext &ctx);
+
+	static Common::UString formatFloat(float f, int width = 18, int decimals = 9);
+
+	static Aurora::NWScript::Object *getParamObject(const Aurora::NWScript::FunctionContext &ctx, size_t n);
+	// '---
+
+	// --- Engine functions ---
+
+	void unimplementedFunction(Aurora::NWScript::FunctionContext &ctx);
+};
+
+} // End of namespace Jade
+
+} // End of namespace Engines
+
+#endif // ENGINES_JADE_SCRIPT_FUNCTIONS_H
