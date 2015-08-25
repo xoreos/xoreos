0f3dc03a4e27c14b1082f91ec7af9101c1405dc5
Sven Hesse <drmccoy@drmccoy.de>
2015-08-25 13:57:39 +0200

    NWN2: Implement placeable open/close/activate
    No animations are played, though.

--- src/engines/nwn2/placeable.h
+++ src/engines/nwn2/placeable.h
@@ -57,6 +57,18 @@ public:
 
 	/** Is the placeable open? */
 	bool isOpen() const;
+	/** Is the placeable activated? */
+	bool isActivated() const;
+
+	/** The opener object opens this placeable. */
+	bool open(Object *opener);
+	/** The closer object closes this placeable. */
+	bool close(Object *closer);
+
+	/** The user object activates this placeable. */
+	bool activate(Object *opener);
+	/** The user object deactivates this placeable. */
+	bool deactivate(Object *closer);
 
 	// Object/Cursor interactions
 
@@ -77,6 +89,8 @@ protected:
 
 private:
 	State _state; ///< The current state of the placeable.
+
+	bool _hasInventory; ///< Does this placeable have an inventory?
 
 	/** Load from a placeable instance. */
 	void load(const Aurora::GFF3Struct &placeable);
