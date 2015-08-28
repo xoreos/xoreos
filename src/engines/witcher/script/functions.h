2429584636e9e35a961d97178d9c84a4b31d4f0a
Sven Hesse <drmccoy@drmccoy.de>
2015-08-29 01:00:39 +0200

    WITCHER: Add and stub all engine functions
    Currently, none of the engine functions are implemented.

---
+++ src/engines/witcher/script/functions.h
@@ -0,0 +1,184 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  The Witcher engine functions.
+ */
+
+#ifndef ENGINES_WITCHER_SCRIPT_FUNCTIONS_H
+#define ENGINES_WITCHER_SCRIPT_FUNCTIONS_H
+
+#include "src/aurora/nwscript/types.h"
+
+namespace Aurora {
+	namespace NWScript {
+		class FunctionContext;
+		class Object;
+	}
+}
+
+namespace Engines {
+
+namespace Witcher {
+
+class Game;
+class Area;
+class Object;
+
+class Functions {
+public:
+	Functions(Game &game);
+	~Functions();
+
+private:
+	typedef void (Functions::*funcPtr)(Aurora::NWScript::FunctionContext &ctx);
+
+	struct FunctionPointer {
+		uint32 id;
+		const char *name;
+		funcPtr func;
+	};
+
+	struct FunctionSignature {
+		uint32 id;
+		Aurora::NWScript::Type returnType;
+		Aurora::NWScript::Type parameters[12];
+	};
+
+	struct FunctionDefaults {
+		uint32 id;
+		const Aurora::NWScript::Variable *defaults[11];
+	};
+
+	static const FunctionPointer kFunctionPointers[];
+	static const FunctionSignature kFunctionSignatures[];
+	static const FunctionDefaults kFunctionDefaults[];
+
+
+	Game *_game;
+
+	void registerFunctions();
+
+	// .--- Utility methods
+	void jumpTo(Witcher::Object *object, float x, float y, float z);
+
+	static int32 getRandom(int min, int max, int32 n = 1);
+
+	static Common::UString formatTag(const Aurora::NWScript::Object *object);
+	static Common::UString formatParams(const Aurora::NWScript::FunctionContext &ctx);
+
+	static Common::UString formatFloat(float f, int width = 18, int decimals = 9);
+
+	static Aurora::NWScript::Object *getParamObject(const Aurora::NWScript::FunctionContext &ctx, size_t n);
+	// '---
+
+	// --- Engine functions ---
+
+	void unimplementedFunction(Aurora::NWScript::FunctionContext &ctx);
+
+	// .--- Math, functions_math.cpp
+	void abs (Aurora::NWScript::FunctionContext &ctx);
+	void fabs(Aurora::NWScript::FunctionContext &ctx);
+
+	void cos(Aurora::NWScript::FunctionContext &ctx);
+	void sin(Aurora::NWScript::FunctionContext &ctx);
+	void tan(Aurora::NWScript::FunctionContext &ctx);
+
+	void acos(Aurora::NWScript::FunctionContext &ctx);
+	void asin(Aurora::NWScript::FunctionContext &ctx);
+	void atan(Aurora::NWScript::FunctionContext &ctx);
+
+	void log (Aurora::NWScript::FunctionContext &ctx);
+	void pow (Aurora::NWScript::FunctionContext &ctx);
+	void sqrt(Aurora::NWScript::FunctionContext &ctx);
+
+	void random(Aurora::NWScript::FunctionContext &ctx);
+
+	void d2  (Aurora::NWScript::FunctionContext &ctx);
+	void d3  (Aurora::NWScript::FunctionContext &ctx);
+	void d4  (Aurora::NWScript::FunctionContext &ctx);
+	void d6  (Aurora::NWScript::FunctionContext &ctx);
+	void d8  (Aurora::NWScript::FunctionContext &ctx);
+	void d10 (Aurora::NWScript::FunctionContext &ctx);
+	void d12 (Aurora::NWScript::FunctionContext &ctx);
+	void d20 (Aurora::NWScript::FunctionContext &ctx);
+	void d100(Aurora::NWScript::FunctionContext &ctx);
+
+	void intToFloat(Aurora::NWScript::FunctionContext &ctx);
+	void floatToInt(Aurora::NWScript::FunctionContext &ctx);
+
+	void vector         (Aurora::NWScript::FunctionContext &ctx);
+	void vectorMagnitude(Aurora::NWScript::FunctionContext &ctx);
+	void vectorNormalize(Aurora::NWScript::FunctionContext &ctx);
+	// '---
+
+	// .--- Module functions, functions_module.cpp
+	void getModule(Aurora::NWScript::FunctionContext &ctx);
+
+	void getFirstPC(Aurora::NWScript::FunctionContext &ctx);
+	void getNextPC(Aurora::NWScript::FunctionContext &ctx);
+	// '---
+
+	// .--- General object functions, functions_object.cpp
+	void getClickingObject(Aurora::NWScript::FunctionContext &ctx);
+	void getEnteringObject(Aurora::NWScript::FunctionContext &ctx);
+	void getExitingObject (Aurora::NWScript::FunctionContext &ctx);
+
+	void getIsObjectValid(Aurora::NWScript::FunctionContext &ctx);
+
+	void getIsPC(Aurora::NWScript::FunctionContext &ctx);
+	// '---
+
+	// .--- Situated objects, functions_situated.cpp
+	void getLocked(Aurora::NWScript::FunctionContext &ctx);
+	void setLocked(Aurora::NWScript::FunctionContext &ctx);
+
+	void getIsOpen(Aurora::NWScript::FunctionContext &ctx);
+
+	void getLastOpenedBy(Aurora::NWScript::FunctionContext &ctx);
+	void getLastClosedBy(Aurora::NWScript::FunctionContext &ctx);
+	void getLastUsedBy  (Aurora::NWScript::FunctionContext &ctx);
+	// '---
+
+	// .--- Actions, functions_action.cpp
+	void assignCommand(Aurora::NWScript::FunctionContext &ctx);
+	void delayCommand (Aurora::NWScript::FunctionContext &ctx);
+	// '---
+
+	// .--- Sound, functions_sound.cpp
+	void musicBackgroundPlay(Aurora::NWScript::FunctionContext &ctx);
+	void musicBackgroundStop(Aurora::NWScript::FunctionContext &ctx);
+	void musicBackgroundChangeDay(Aurora::NWScript::FunctionContext &ctx);
+	void musicBackgroundChangeNight(Aurora::NWScript::FunctionContext &ctx);
+
+	void musicBackgroundGetDayTrack(Aurora::NWScript::FunctionContext &ctx);
+	void musicBackgroundGetNightTrack(Aurora::NWScript::FunctionContext &ctx);
+	// '---
+
+	// .--- Movies, functions_movie.cpp
+	void playMovie(Aurora::NWScript::FunctionContext &ctx);
+	// '---
+};
+
+} // End of namespace Witcher
+
+} // End of namespace Engines
+
+#endif // ENGINES_WITCHER_SCRIPT_FUNCTIONS_H
