bd428af8eb44e3ea9ea1d984a82c4e52838c3aa8
Sven Hesse <drmccoy@drmccoy.de>
2015-09-27 16:37:09 +0200

    DRAGONAGE: Implement SetEventType and SetEventCreator

--- src/engines/dragonage/script/functions_event.cpp
+++ src/engines/dragonage/script/functions_event.cpp
@@ -83,6 +83,26 @@ void Functions::getEventTarget(Aurora::NWScript::FunctionContext &ctx) {
 	ctx.getReturn() = event->getTarget();
 }
 
+void Functions::setEventType(Aurora::NWScript::FunctionContext &ctx) {
+	ctx.getReturn() = ctx.getParams()[0].getEngineType();
+
+	Event *event = DragonAge::ObjectContainer::toEvent(ctx.getReturn().getEngineType());
+	if (!event)
+		return;
+
+	event->setType((EventType) ctx.getParams()[1].getInt());
+}
+
+void Functions::setEventCreator(Aurora::NWScript::FunctionContext &ctx) {
+	ctx.getReturn() = ctx.getParams()[0].getEngineType();
+
+	Event *event = DragonAge::ObjectContainer::toEvent(ctx.getReturn().getEngineType());
+	if (!event)
+		return;
+
+	event->setCreator(ctx.getParams()[1].getObject());
+}
+
 } // End of namespace DragonAge
 
 } // End of namespace Engines
