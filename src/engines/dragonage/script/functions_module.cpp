05bd74b46138e564bdf0832e830a1425fedf7e5f
Sven Hesse <drmccoy@drmccoy.de>
2015-09-27 16:18:29 +0200

    DRAGONAGE: Implement GetModule and GetHero

---
+++ src/engines/dragonage/script/functions_module.cpp
@@ -0,0 +1,55 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Dragon Age: Origins engine functions operating on the current module/campaign.
+ */
+
+#include "src/common/util.h"
+
+#include "src/aurora/nwscript/functioncontext.h"
+
+#include "src/engines/dragonage/game.h"
+#include "src/engines/dragonage/campaigns.h"
+#include "src/engines/dragonage/campaign.h"
+
+#include "src/engines/dragonage/script/functions.h"
+
+namespace Engines {
+
+namespace DragonAge {
+
+void Functions::getModule(Aurora::NWScript::FunctionContext &ctx) {
+	ctx.getReturn() = (Aurora::NWScript::Object *) _game->getCampaigns().getCurrentCampaign();
+}
+
+void Functions::getHero(Aurora::NWScript::FunctionContext &ctx) {
+	ctx.getReturn() = (Aurora::NWScript::Object *) 0;
+
+	Campaign *campaign = _game->getCampaigns().getCurrentCampaign();
+	if (!campaign)
+		return;
+
+	ctx.getReturn() = (Aurora::NWScript::Object *) campaign->getPC();
+}
+
+} // End of namespace DragonAge
+
+} // End of namespace Engines
