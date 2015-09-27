ccf1b456a2d3130e60e0d1d61caaab8616707a80
Sven Hesse <drmccoy@drmccoy.de>
2015-09-27 03:12:07 +0200

    DRAGONAGE2: Add and stub all engine functions
    Currently, none of the engine functions are implemented.

---
+++ src/engines/dragonage2/script/functions.cpp
@@ -0,0 +1,130 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Dragon Age II engine functions.
+ */
+
+#include <cassert>
+#include <cstdlib>
+
+#include <boost/bind.hpp>
+
+#include "src/common/util.h"
+
+#include "src/aurora/nwscript/functionman.h"
+#include "src/aurora/nwscript/util.h"
+
+#include "src/engines/dragonage2/types.h"
+#include "src/engines/dragonage2/game.h"
+#include "src/engines/dragonage2/objectcontainer.h"
+#include "src/engines/dragonage2/object.h"
+
+#include "src/engines/dragonage2/script/functions.h"
+
+#include "src/engines/dragonage2/script/function_tables.h"
+
+namespace Engines {
+
+namespace DragonAge2 {
+
+Functions::Functions(Game &game) : _game(&game) {
+	registerFunctions();
+}
+
+Functions::~Functions() {
+	FunctionMan.clear();
+}
+
+void Functions::registerFunctions() {
+	assert(ARRAYSIZE(kFunctionPointers) == ARRAYSIZE(kFunctionSignatures));
+	assert(ARRAYSIZE(kFunctionPointers) == ARRAYSIZE(kFunctionDefaults));
+
+	for (size_t i = 0; i < ARRAYSIZE(kFunctionPointers); i++) {
+		const FunctionPointer   &fPtr = kFunctionPointers[i];
+		const FunctionSignature &fSig = kFunctionSignatures[i];
+		const FunctionDefaults  &fDef = kFunctionDefaults[i];
+
+		const uint32 id = fPtr.id;
+
+		assert((fSig.id == id) && (fDef.id == id));
+
+		Aurora::NWScript::Signature signature;
+		signature.push_back(fSig.returnType);
+		for (size_t j = 0; j < ARRAYSIZE(fSig.parameters); j++) {
+			if (fSig.parameters[j] == kTypeVoid)
+				break;
+
+			signature.push_back(fSig.parameters[j]);
+		}
+
+		Aurora::NWScript::Parameters defaults;
+		for (size_t j = 0; j < ARRAYSIZE(fDef.defaults); j++) {
+			if (!fDef.defaults[j])
+				break;
+
+			defaults.push_back(Aurora::NWScript::Variable(*fDef.defaults[j]));
+		}
+
+		const funcPtr f = fPtr.func ? fPtr.func : &Functions::unimplementedFunction;
+
+		FunctionMan.registerFunction(fPtr.name, id, boost::bind(f, this, _1), signature, defaults);
+	}
+}
+
+void Functions::unimplementedFunction(Aurora::NWScript::FunctionContext &ctx) {
+	warning("TODO: %s %s(%s)", Aurora::NWScript::formatType(ctx.getReturn().getType()).c_str(),
+	                           ctx.getName().c_str(), Aurora::NWScript::formatParams(ctx).c_str());
+}
+
+int32 Functions::getRandom(int min, int max, int32 n) {
+	if (n < 1)
+		n = 1;
+
+	int32 r = 0;
+
+	while (n-- > 0)
+		r += std::rand() % (max - min + 1) + min;
+
+	return r;
+}
+
+Common::UString Functions::formatFloat(float f, int width, int decimals) {
+	return Common::UString::format("%*.*f", width, decimals, f);
+}
+
+Aurora::NWScript::Object *Functions::getParamObject(const Aurora::NWScript::FunctionContext &ctx, size_t n) {
+	DragonAge2::Object *object = DragonAge2::ObjectContainer::toObject(ctx.getParams()[n].getObject());
+	if (!object || (object->getType() == kObjectTypeInvalid))
+		return 0;
+
+	if (object->getType() == kObjectTypeSelf)
+		return ctx.getCaller();
+
+	return object;
+}
+
+void Functions::jumpTo(DragonAge2::Object *object, float x, float y, float z) {
+	object->setPosition(x, y, z);
+}
+
+} // End of namespace DragonAge2
+
+} // End of namespace Engines
---
+++ src/engines/dragonage2/script/functions.h
@@ -0,0 +1,97 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Dragon Age II engine functions.
+ */
+
+#ifndef ENGINES_DRAGONAGE2_SCRIPT_FUNCTIONS_H
+#define ENGINES_DRAGONAGE2_SCRIPT_FUNCTIONS_H
+
+#include "src/aurora/nwscript/types.h"
+
+namespace Aurora {
+	namespace NWScript {
+		class FunctionContext;
+		class Object;
+	}
+}
+
+namespace Engines {
+
+namespace DragonAge2 {
+
+class Game;
+class Object;
+
+class Functions {
+public:
+	Functions(Game &game);
+	~Functions();
+
+private:
+	typedef void (Functions::*funcPtr)(Aurora::NWScript::FunctionContext &ctx);
+
+	struct FunctionPointer {
+		uint32 id;
+		const char *name;
+		funcPtr func;
+	};
+
+	struct FunctionSignature {
+		uint32 id;
+		Aurora::NWScript::Type returnType;
+		Aurora::NWScript::Type parameters[15];
+	};
+
+	struct FunctionDefaults {
+		uint32 id;
+		const Aurora::NWScript::Variable *defaults[7];
+	};
+
+	static const FunctionPointer kFunctionPointers[];
+	static const FunctionSignature kFunctionSignatures[];
+	static const FunctionDefaults kFunctionDefaults[];
+
+
+	Game *_game;
+
+	void registerFunctions();
+
+	// .--- Utility methods
+	void jumpTo(DragonAge2::Object *object, float x, float y, float z);
+
+	static int32 getRandom(int min, int max, int32 n = 1);
+
+	static Common::UString formatFloat(float f, int width = 18, int decimals = 9);
+
+	static Aurora::NWScript::Object *getParamObject(const Aurora::NWScript::FunctionContext &ctx, size_t n);
+	// '---
+
+	// --- Engine functions ---
+
+	void unimplementedFunction(Aurora::NWScript::FunctionContext &ctx);
+};
+
+} // End of namespace DragonAge2
+
+} // End of namespace Engines
+
+#endif // ENGINES_DRAGONAGE2_SCRIPT_FUNCTIONS_H
