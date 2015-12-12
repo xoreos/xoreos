ca03dc9d1abc4118b8aff21f2890d4517789d111
Sven Hesse <drmccoy@drmccoy.de>
2015-12-12 01:12:48 +0100

    WITCHER: Use the exception dispatchers

--- src/engines/witcher/script/container.cpp
+++ src/engines/witcher/script/container.cpp
@@ -172,9 +172,8 @@ bool ScriptContainer::runScript(const Common::UString &script,
 
 		return true;
 
-	} catch (Common::Exception &e) {
-		e.add("Failed running script \"%s\"", script.c_str());
-		Common::printException(e, "WARNING: ");
+	} catch (...) {
+		Common::exceptionDispatcherWarning("Failed running script \"%s\"", script.c_str());
 		return false;
 	}
 
