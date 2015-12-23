3cf5144254f079c2308662d72702caf54b03cbb7
Sven Hesse <drmccoy@drmccoy.de>
2015-12-23 12:13:53 +0100

    GRAPHICS: Throw when a TBX mip map wouldn't fit the data size

--- src/graphics/images/txb.cpp
+++ src/graphics/images/txb.cpp
@@ -137,6 +137,10 @@ void TXB::readHeader(Common::SeekableReadStream &txb, bool &needDeSwizzle, uint3
 		throw Common::Exception("Unknown TXB encoding 0x%02X (%dx%d, %d, %d)",
 				encoding, width, height, mipMapCount, dataSize);
 
+	const size_t fullImageDataSize = getDataSize(_formatRaw, width, height);
+	if (dataSize < fullImageDataSize)
+		throw Common::Exception("Image wouldn't fit into data");
+
 	_mipMaps.reserve(mipMapCount);
 	for (uint32 i = 0; i < mipMapCount; i++) {
 		MipMap *mipMap = new MipMap(this);
@@ -154,7 +158,9 @@ void TXB::readHeader(Common::SeekableReadStream &txb, bool &needDeSwizzle, uint3
 
 		mipMap->data = 0;
 
-		if (dataSize < mipMap->size) {
+		const size_t mipMapDataSize = getDataSize(_formatRaw, mipMap->width, mipMap->height);
+
+		if ((dataSize < mipMap->size) || (mipMap->size < mipMapDataSize)) {
 			// Wouldn't fit
 			delete mipMap;
 			break;
