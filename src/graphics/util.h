28b5f5bf7ea2ea30dfee74158780e7260244ba16
Sven Hesse <drmccoy@drmccoy.de>
2015-12-23 16:55:54 +0100

    GRAPHICS: Add hasValidDimensions()
    Validate that the given image dimensions are valid for a certain pixel
    format.

--- src/graphics/util.h
+++ src/graphics/util.h
@@ -68,6 +68,32 @@ static inline uint32 getDataSize(PixelFormatRaw format, int32 width, int32 heigh
 	throw Common::Exception("Invalid pixel format %u", (uint) format);
 }
 
+/** Are these image dimensions valid for this format? */
+static inline bool hasValidDimensions(PixelFormatRaw format, int32 width, int32 height) {
+	if ((width < 0) || (height < 0))
+		return false;
+
+	switch (format) {
+		case kPixelFormatRGB8:
+		case kPixelFormatRGBA8:
+		case kPixelFormatRGB5A1:
+		case kPixelFormatRGB5:
+			return true;
+
+		case kPixelFormatDXT1:
+		case kPixelFormatDXT3:
+		case kPixelFormatDXT5:
+			/* The DXT algorithms work on 4x4 pixel blocks. Textures smaller than one
+			 * block will be padded, but larger textures need to be correctly aligned. */
+			return ((width < 4) && (height < 4)) || (((width % 4) == 0) && ((height % 4) == 0));
+
+		default:
+			break;
+	}
+
+	return false;
+}
+
 /** Flip an image horizontally. */
 static inline void flipHorizontally(byte *data, int width, int height, int bpp) {
 	int halfWidth = width / 2;
