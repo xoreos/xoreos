b4518b8da5ad08e6daa6d8ea64485f4bf3c89617
Sven Hesse <drmccoy@drmccoy.de>
2016-01-01 22:51:16 +0100

    SOUND: Extend the "based on FFmpeg code" blurb

--- src/sound/decoders/asf.cpp
+++ src/sound/decoders/asf.cpp
@@ -18,10 +18,33 @@
  * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Largely based on the ASF implementation found in FFmpeg.
-
 /** @file
  *  Decoding Microsoft's Advanced Streaming Format.
+ */
+
+/* Based on the ASF implementation in FFmpeg (<https://ffmpeg.org/)>,
+ * which is released under the terms of version 2 or later of the GNU
+ * Lesser General Public License.
+ *
+ * The original copyright note in libavformat/asf.c reads as follows:
+ *
+ * Copyright (c) 2000, 2001 Fabrice Bellard
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #include "src/common/error.h"
--- src/sound/decoders/wmadata.h
+++ src/sound/decoders/wmadata.h
@@ -22,6 +22,32 @@
  *  Static data used for decoding Microsoft's Windows Media Audio.
  */
 
+/* Based on the WMA implementation in FFmpeg (<https://ffmpeg.org/)>,
+ * which is released under the terms of version 2 or later of the GNU
+ * Lesser General Public License.
+ *
+ * The original copyright note in libavcodec/wma.c reads as follows:
+ *
+ * WMA compatible codec
+ * Copyright (c) 2002-2007 The FFmpeg Project
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 #ifndef SOUND_DECODERS_WMADATA_H
 #define SOUND_DECODERS_WMADATA_H
 
