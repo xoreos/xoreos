b03595cc09d3b124ff295d5c00389f425b0fc481
Sven Hesse <drmccoy@drmccoy.de>
2016-01-02 01:20:14 +0100

    GRAPHICS: Extend the "based on Tinke code" blurb

--- src/graphics/images/ncgr.cpp
+++ src/graphics/images/ncgr.cpp
@@ -29,6 +29,23 @@
  *
  * Tinke in turn is based on the NCGR documentation by lowlines
  * (<http://llref.emutalk.net/docs/?file=xml/ncgr.xml>).
+ *
+ * The original copyright note in Tinke reads as follows:
+ *
+ * Copyright (C) 2011  pleoNeX
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "src/common/util.h"
