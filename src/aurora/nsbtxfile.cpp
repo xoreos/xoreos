8bee6b0df47068b2a508bacbd83cfad0711e17f8
Sven Hesse <drmccoy@drmccoy.de>
2016-01-02 01:20:02 +0100

    AURORA: Extend the "based on Tinke code" blurb

--- src/aurora/nsbtxfile.cpp
+++ src/aurora/nsbtxfile.cpp
@@ -31,6 +31,23 @@
  * (<http://llref.emutalk.net/docs/?file=xml/btx0.xml>) and the
  * Nintendo DS technical information GBATEK by Martin Korth
  * (<http://problemkaputt.de/gbatek.htm>).
+ *
+ * The original copyright note in Tinke reads as follows:
+ *
+ * Copyright (C) 2011  pleoNeX
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <cassert>
