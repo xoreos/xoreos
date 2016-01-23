2ada083848fe4dc2e34bbcfc8afb0e3477246f92
Sven Hesse <drmccoy@drmccoy.de>
2016-01-24 00:18:52 +0100

    KOTOR: Remove unused include to aurora/types.h

--- src/engines/kotor/modelloader.cpp
+++ src/engines/kotor/modelloader.cpp
@@ -25,8 +25,6 @@
 #include "src/common/error.h"
 #include "src/common/readstream.h"
 
-#include "src/aurora/types.h"
-
 #include "src/graphics/aurora/model_kotor.h"
 
 #include "src/engines/kotor/modelloader.h"
