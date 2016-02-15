499fbcc3f29c6c421e4f9a3ed6fc3dc28a382a7a
Sven Hesse <drmccoy@drmccoy.de>
2016-02-16 00:11:12 +0100

    AURORA: Add a list of all currently known GDA column headers
    They're CRC32'd.
    
    Note: We're still missing about 45% (382 of 848) of the column headers
    for the GDAs found in Sonic.
    
    Syncing with xoreos-tools.

---
+++ src/aurora/gdaheaders.h
@@ -0,0 +1,36 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *   Resolve a GDA column header hash back to its string.
+ */
+
+#ifndef AURORA_GDAHEADERS_H
+#define AURORA_GDAHEADERS_H
+
+#include "src/common/types.h"
+
+namespace Aurora {
+
+const char *findGDAHeader(uint32 hash);
+
+} // End of namespace Aurora
+
+#endif // AURORA_GDAHEADERS_H
