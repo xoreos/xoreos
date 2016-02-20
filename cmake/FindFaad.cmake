4ea7f7c218d5b0a46902d240b54709b6708b5573
Christian Koch <cfkoch@sdf.lonestar.org>
2016-02-19 22:46:59 -0800

    BUILD: Some platforms install faad.h inside faad2/faad.h

--- cmake/FindFaad.cmake
+++ cmake/FindFaad.cmake
@@ -29,7 +29,7 @@ if(WIN32)
   find_library(FAAD_LIBRARY NAMES faad PATHS $ENV{PROGRAMFILES}/FAAD/lib DOC "The libfaad library")
 
 else(WIN32)
-  find_path(FAAD_INCLUDE_DIR faad.h DOC "The directory where faad.h resides")
+  find_path(FAAD_INCLUDE_DIR NAMES faad.h faad2/faad.h DOC "The directory where faad.h resides")
   find_library(FAAD_LIBRARY NAMES faad DOC "The libfaad library")
 
 endif(WIN32)
