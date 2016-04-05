0610e868fa59be3749565130065892bd882bde21
Sven Hesse <drmccoy@drmccoy.de>
2016-04-06 01:04:02 +0200

    SOUND: Keep unqueueing buffers when the stream ended
    Otherwise, the samples/time played will be off.

--- src/sound/sound.cpp
+++ src/sound/sound.cpp
@@ -615,7 +615,7 @@ void SoundManager::bufferData(size_t channel) {
 }
 
 void SoundManager::bufferData(Channel &channel) {
-	if (!channel.stream || channel.stream->endOfData())
+	if (!channel.stream)
 		return;
 
 	if (!_hasSound)
