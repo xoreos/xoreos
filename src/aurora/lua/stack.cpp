d937b00270d5e7703030fb9a091da6a2835060c9
Stanislav Bas <stanislav.m.bas@gmail.com>
2016-08-12 14:24:58 +0300

    AURORA: Add Lua stack wrapper

---
+++ src/aurora/lua/stack.cpp
@@ -0,0 +1,139 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  A Lua stack wrapper.
+ */
+
+extern "C" {
+
+#include "toluapp/tolua++.h"
+
+}
+
+#include "src/common/error.h"
+#include "src/common/ustring.h"
+
+#include "src/aurora/lua/stack.h"
+
+namespace Aurora {
+
+namespace Lua {
+
+Stack::Stack(lua_State &state) : _luaState(state) {
+
+}
+
+Stack::~Stack() {
+
+}
+
+int Stack::getSize() const {
+	return lua_gettop(&_luaState);
+}
+
+void Stack::pushNil() {
+	lua_pushnil(&_luaState);
+}
+
+void Stack::pushBoolean(bool value) {
+	lua_pushboolean(&_luaState, value ? 1 : 0);
+}
+
+void Stack::pushNumber(float value) {
+	lua_pushnumber(&_luaState, value);
+}
+
+void Stack::pushString(const char *value) {
+	lua_pushstring(&_luaState, value);
+}
+
+void Stack::pushString(const Common::UString &value) {
+	lua_pushstring(&_luaState, value.c_str());
+}
+
+bool Stack::getBooleanAt(int index) const {
+	if (!isBooleanAt(index)) {
+		throw Common::Exception("Failed to get a boolean value from the Lua stack (index: %d)", index);
+	}
+	return lua_toboolean(&_luaState, index);
+}
+
+float Stack::getNumberAt(int index) const {
+	if (!isNumberAt(index)) {
+		throw Common::Exception("Failed to get a number from the Lua stack (index: %d)", index);
+	}
+	return lua_tonumber(&_luaState, index);
+}
+
+Common::UString Stack::getStringAt(int index) const {
+	if (!isStringAt(index)) {
+		throw Common::Exception("Failed to get a string from the Lua stack (index: %d)", index);
+	}
+	return lua_tostring(&_luaState, index);
+}
+
+bool Stack::isNilAt(int index) const {
+	return checkIndex(index) && lua_isnil(&_luaState, index);
+}
+
+bool Stack::isBooleanAt(int index) const {
+	return checkIndex(index) && lua_isboolean(&_luaState, index);
+}
+
+bool Stack::isNumberAt(int index) const {
+	return checkIndex(index) && lua_isnumber(&_luaState, index) != 0;
+}
+
+bool Stack::isStringAt(int index) const {
+	return checkIndex(index) && lua_isstring(&_luaState, index) != 0;
+}
+
+bool Stack::isUserTypeAt(int index, const Common::UString &type) const {
+	tolua_Error error;
+	return checkIndex(index) && tolua_isusertype(&_luaState, index, type.c_str(), 0, &error) != 0;
+}
+
+lua_State &Stack::getLuaState() const {
+	return _luaState;
+}
+
+bool Stack::checkIndex(int index) const {
+	index = std::abs(index);
+	return index > 0 && index <= getSize();
+}
+
+void Stack::pushRawUserType(void *value, const Common::UString &type) {
+	tolua_pushusertype(&_luaState, value, type.c_str());
+}
+
+void *Stack::getRawUserTypeAt(int index, const Common::UString &type) const {
+	if (!isUserTypeAt(index, type)) {
+		throw Common::Exception(
+		    "Failed to get a usertype value from the Lua stack (type: %s, index: %d)",
+		    type.c_str(),
+		    index);
+	}
+	return tolua_tousertype(&_luaState, index, 0);
+}
+
+} // End of namespace Lua
+
+} // End of namespace Aurora
---
+++ src/aurora/lua/stack.h
@@ -0,0 +1,116 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  A Lua stack wrapper.
+ */
+
+#ifndef ENGINES_AURORA_LUA_STACK_H
+#define ENGINES_AURORA_LUA_STACK_H
+
+struct lua_State;
+
+namespace Common {
+	class UString;
+}
+
+namespace Aurora {
+
+namespace Lua {
+
+/** A Lua stack wrapper. */
+class Stack {
+public:
+	Stack(lua_State &state);
+	~Stack();
+
+	/** Return the number of elements in the stack. */
+	int getSize() const;
+
+	/** Push a nil value onto the stack. */
+	void pushNil();
+	/** Push a boolean @a value onto the stack. */
+	void pushBoolean(bool value);
+	/** Push a number onto the stack. */
+	void pushNumber(float value);
+	/** Push a raw C string onto the stack. */
+	void pushString(const char *value);
+	/** Push a string onto the stack. */
+	void pushString(const Common::UString &value);
+
+	/** Push a usertype value onto the stack.
+	 *  Expect that @a type is a name of the registered type in the script subsystem.
+	 */
+	template<typename T>
+	void pushUserType(T &value, const Common::UString &type);
+
+	/** Return a boolean value at the given @a index in the stack. */
+	bool getBooleanAt(int index) const;
+	/** Return a number at the given @a index in the stack. */
+	float getNumberAt(int index) const;
+	/** Return a string at the given @a index in the stack. */
+	Common::UString getStringAt(int index) const;
+
+	/** Return a usertype value at the given @a index in the stack.
+	 *  Expect that @a type is a name of the registered type in the script subsystem.
+	 */
+	template<typename T>
+	T *getUserTypeAt(int index, const Common::UString &type) const;
+
+	/** Check whether the value with the given @a index is a nil. */
+	bool isNilAt(int index) const;
+	/** Check whether the value at the given @a index is a boolean value. */
+	bool isBooleanAt(int index) const;
+	/** Check whether the value at the given @a index is a number. */
+	bool isNumberAt(int index) const;
+	/** Check whether the value at the given @a index is a string. */
+	bool isStringAt(int index) const;
+	/** Check whether the value at the given @a index is a usertype value with the @a given type. */
+	bool isUserTypeAt(int index, const Common::UString &type) const;
+
+	/** Return the underlying Lua state. */
+	lua_State &getLuaState() const;
+
+private:
+	/** The Lua state. */
+	lua_State &_luaState;
+
+	/** Check whether the given @a index is valid. */
+	bool checkIndex(int index) const;
+
+	void pushRawUserType(void *value, const Common::UString &type);
+	void *getRawUserTypeAt(int index, const Common::UString &type) const;
+};
+
+template<typename T>
+void Stack::pushUserType(T &value, const Common::UString &type) {
+	return pushRawUserType(&value, type);
+}
+
+template<typename T>
+T *Stack::getUserTypeAt(int index, const Common::UString &type) const {
+	return reinterpret_cast<T *>(getRawUserTypeAt(index, type));
+}
+
+} // End of namespace Lua
+
+} // End of namespace Aurora
+
+#endif // ENGINES_AURORA_LUA_STACK_H
