2532959776b5d6553325d43701846b08201b3620
patrick <nostritius@googlemail.com>
2017-06-15 21:56:59 +0200

    KOTOR: Add a base class for all character creation menus

---
+++ src/engines/kotor/gui/chargen/chargenbase.cpp
@@ -0,0 +1,48 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  A base class for all character creation menus.
+ */
+
+#include "src/engines/kotor/console.h"
+
+#include "src/engines/kotor/gui/chargen/chargenbase.h"
+
+namespace Engines {
+
+namespace KotOR {
+
+CharacterGenerationBaseMenu::CharacterGenerationBaseMenu(CharacterGenerationInfo &info,
+		Engines::Console *console) : GUI(console), _info(info), _accepted(false) {
+
+}
+
+bool CharacterGenerationBaseMenu::isAccepted() {
+	return _accepted;
+}
+
+void CharacterGenerationBaseMenu::accept() {
+	_accepted = true;
+}
+
+} // End of namespace KotOR
+
+} // End of namespace Engines
