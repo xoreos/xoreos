b0cc184f9d10372b91a99c641dc8564d22de44bb
patrick <nostritius@googlemail.com>
2017-08-30 16:11:22 +0200

    KOTOR: Add the typing possibility in the CharGenName class

--- src/engines/kotor/gui/chargen/chargenname.cpp
+++ src/engines/kotor/gui/chargen/chargenname.cpp
@@ -22,10 +22,13 @@
  *  The menu for modifying the name of the character.
  */
 
+#include "src/events/events.h"
+
 #include "src/engines/kotor/gui/widgets/kotorwidget.h"
 #include "src/engines/kotor/gui/widgets/label.h"
 
 #include "src/engines/kotor/gui/chargen/chargenname.h"
+#include "src/engines/kotor/gui/chargen/chargeninfo.h"
 
 namespace Engines {
 
@@ -38,21 +41,49 @@ CharacterGenerationNameMenu::CharacterGenerationNameMenu(CharacterGenerationInfo
 
 	addBackground(kBackgroundTypeMenu);
 
-	getLabel("NAME_BOX_EDIT")->setFont("fnt_d16x16b");
-	getLabel("NAME_BOX_EDIT")->setText("_");
+	_nameLabel = getLabel("NAME_BOX_EDIT");
+	_nameLabel->setFont("fnt_d16x16b");
+	_nameLabel->setText("_");
+
+	EventMan.enableTextInput(true);
+	EventMan.enableKeyRepeat(true);
 }
 
 void CharacterGenerationNameMenu::callbackActive(Widget &widget) {
 	if (widget.getTag() == "BTN_BACK") {
 		_returnCode = 1;
+		EventMan.enableTextInput(false);
+		EventMan.enableKeyRepeat(false);
 		return;
 	}
 
 	if (widget.getTag() == "END_BTN") {
 		_returnCode = 1;
 		accept();
+		EventMan.enableTextInput(false);
+		EventMan.enableKeyRepeat(false);
 		return;
 	}
+}
+
+void CharacterGenerationNameMenu::callbackKeyInput(const Events::Key &key, const Events::EventType &type) {
+	if (key == Events::kKeyBackspace && type == Events::kEventKeyDown) {
+		if (!_name.empty()) {
+			_name.erase(--_name.end());
+			_nameLabel->setText(_name + "_");
+		}
+	}
+}
+
+void CharacterGenerationNameMenu::callbackTextInput(const Common::UString &text) {
+	// The name should not be longer than 18 letters (according to the original game).
+	if (_name.size() == 18) {
+		return;
+	}
+
+	_name += text;
+	_info.setName(_name);
+	_nameLabel->setText(_name + "_");
 }
 
 } // End of namespace KotOR
