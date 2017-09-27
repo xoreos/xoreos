7f1f7000f797762cdab74da0fc91256ce558efe4
patrick <nostritius@googlemail.com>
2017-09-27 23:01:19 +0200

    KOTOR: Add make files for the ingame system

--- src/engines/kotor/gui/ingame/rules.mk
+++ src/engines/kotor/gui/ingame/rules.mk
@@ -17,20 +17,10 @@
 # You should have received a copy of the GNU General Public License
 # along with xoreos. If not, see <http://www.gnu.org/licenses/>.
 
-# GUI system in Star Wars: Knights of the Old Republic.
+# Ingame system in Star Wars: Knights of the Old Republic.
 
 src_engines_kotor_libkotor_la_SOURCES += \
-    src/engines/kotor/gui/gui.h \
-    src/engines/kotor/gui/guibackground.h \
     $(EMPTY)
 
 src_engines_kotor_libkotor_la_SOURCES += \
-    src/engines/kotor/gui/gui.cpp \
-    src/engines/kotor/gui/guibackground.cpp \
     $(EMPTY)
-
-include src/engines/kotor/gui/widgets/rules.mk
-include src/engines/kotor/gui/main/rules.mk
-include src/engines/kotor/gui/options/rules.mk
-include src/engines/kotor/gui/chargen/rules.mk
-include src/engines/kotor/gui/ingame/rules.mk
