444547f973d5b2f57a2b3052bc6920c4ea90c9e9
patrick <nostritius@googlemail.com>
2017-10-08 12:59:41 +0200

    KOTOR: Add GetLevelByClass function to scripts

---
+++ src/engines/kotor/script/functions_creatures.cpp
@@ -0,0 +1,53 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Star Wars: Knights of the Old Republic engine functions managing creatures
+ */
+
+#include "src/common/error.h"
+
+#include "src/aurora/nwscript/functioncontext.h"
+
+#include "src/engines/kotor/types.h"
+#include "src/engines/kotor/creature.h"
+#include "src/engines/kotor/objectcontainer.h"
+
+#include "src/engines/kotor/script/functions.h"
+
+namespace Engines {
+
+namespace KotOR {
+
+void Functions::getLevelByClass(Aurora::NWScript::FunctionContext &ctx) {
+	Class creatureClass = Class(ctx.getParams()[0].getInt());
+	Aurora::NWScript::Object *object = ctx.getParams()[1].getObject();
+
+	Creature *creature = ObjectContainer::toCreature(object);
+
+	if (!creature)
+		throw Common::Exception("Functions::getGender(): object is not a creature");
+
+	ctx.getReturn() = creature->getLevel(creatureClass);
+}
+
+} // End of namespace KotOR
+
+} // End of namespace Engines
