f5bf5c11d4060bfb48d44a47c1960ea5253e4a86
patrick <nostritius@googlemail.com>
2017-12-02 20:03:31 +0100

    AURORA: Add file loader for kotor savegame metadata

---
+++ src/aurora/nfofile.cpp
@@ -0,0 +1,85 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Loader for the savenfo.res file, storing savegame metadata for kotor 1 and 2.
+ */
+
+#include "src/common/error.h"
+#include "src/common/readstream.h"
+
+#include "src/aurora/gff3file.h"
+#include "src/aurora/nfofile.h"
+
+static const uint32 kNFOID = MKTAG('N', 'F', 'O', ' ');
+
+namespace Aurora {
+
+NFOFile::NFOFile(Common::SeekableReadStream *stream) : _timePlayed(0) {
+	GFF3File gff(stream, kNFOID);
+
+	load(gff.getTopLevel());
+}
+
+NFOFile::~NFOFile() {
+}
+
+const Common::UString &NFOFile::getPortrait0() const {
+	return _portrait0;
+}
+
+const Common::UString &NFOFile::getPortrait1() const {
+	return _portrait1;
+}
+
+const Common::UString &NFOFile::getPortrait2() const {
+	return _portrait2;
+}
+
+uint32 NFOFile::getTimePlayed() const {
+	return _timePlayed;
+}
+
+const Common::UString &NFOFile::getSaveName() const {
+	return _saveName;
+}
+
+const Common::UString &NFOFile::getAreaName() const {
+	return _areaName;
+}
+
+const Common::UString &NFOFile::getPCName() const {
+	return _pcName;
+}
+
+void NFOFile::load(const GFF3Struct &top) {
+	_portrait0 = top.getString("PORTRAIT0");
+	_portrait1 = top.getString("PORTRAIT1");
+	_portrait2 = top.getString("PORTRAIT2");
+
+	_timePlayed = top.getUint("TIMEPLAYED");
+
+	_areaName = top.getString("AREANAME");
+	_saveName = top.getString("SAVEGAMENAME");
+
+	_pcName = top.getString("PCNAME");
+}
+
+} // End of namespace Aurora
---
+++ src/aurora/nfofile.h
@@ -0,0 +1,71 @@
+/* xoreos - A reimplementation of BioWare's Aurora engine
+ *
+ * xoreos is the legal property of its developers, whose names
+ * can be found in the AUTHORS file distributed with this source
+ * distribution.
+ *
+ * xoreos is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * xoreos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file
+ *  Loader for the savenfo.res file, storing savegame metadata for kotor 1 and 2.
+ */
+
+#ifndef AURORA_NFOFILE_H
+#define AURORA_NFOFILE_H
+
+#include "src/aurora/gff3file.h"
+
+namespace Aurora {
+
+class NFOFile {
+public:
+	NFOFile(Common::SeekableReadStream *stream);
+	~NFOFile();
+
+	/** Get the player portrait. */
+	const Common::UString &getPortrait0() const;
+	/** Get the first companions portrait. */
+	const Common::UString &getPortrait1() const;
+	/** Get the second companions portrait. */
+	const Common::UString &getPortrait2() const;
+
+	/** Get the time played. */
+	uint32 getTimePlayed() const;
+
+	/** Get the area name. */
+	const Common::UString &getSaveName() const;
+	/** Get the area name. */
+	const Common::UString &getAreaName() const;
+
+	/** Get the pc name (kotor2 only). */
+	const Common::UString &getPCName() const;
+
+private:
+	void load(const GFF3Struct &top);
+
+	uint32 _timePlayed; ///< The time this save was actually played.
+
+	Common::UString _saveName; ///< The save name.
+	Common::UString _areaName; ///< The current area to load.
+	Common::UString _pcName; ///< The name of the pc (kotor2 only).
+
+	Common::UString _portrait0; ///< The portrait of the pc or solo companion.
+	Common::UString _portrait1; ///< The portrait of the first companion.
+	Common::UString _portrait2; ///< The portrait of the second companion.
+};
+
+} // End of namespace Aurora
+
+#endif // AURORA_NFOFILE_H
