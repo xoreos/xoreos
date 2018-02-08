ea1e22795300e017657c6e4886bad5ce2dda7557
patrick <nostritius@googlemail.com>
2018-02-08 11:22:03 +0100

    KOTOR: Add loadscreen make files

--- src/engines/kotor/gui/loadscreen/rules.mk
+++ src/engines/kotor/gui/loadscreen/rules.mk
@@ -17,26 +17,10 @@
 # You should have received a copy of the GNU General Public License
 # along with xoreos. If not, see <http://www.gnu.org/licenses/>.
 
-# GUI system in Star Wars: Knights of the Old Republic.
+# Load screen system in Star Wars: Knights of the Old Republic.
 
 src_engines_kotor_libkotor_la_SOURCES += \
-    src/engines/kotor/gui/gui.h \
-    src/engines/kotor/gui/guibackground.h \
-    src/engines/kotor/gui/saveload.h \
-    src/engines/kotor/gui/dialog.h \
     $(EMPTY)
 
 src_engines_kotor_libkotor_la_SOURCES += \
-    src/engines/kotor/gui/gui.cpp \
-    src/engines/kotor/gui/guibackground.cpp \
-    src/engines/kotor/gui/saveload.cpp \
-    src/engines/kotor/gui/dialog.cpp \
     $(EMPTY)
-
-include src/engines/kotor/gui/widgets/rules.mk
-include src/engines/kotor/gui/main/rules.mk
-include src/engines/kotor/gui/options/rules.mk
-include src/engines/kotor/gui/chargen/rules.mk
-include src/engines/kotor/gui/ingame/rules.mk
-include src/engines/kotor/gui/dialogs/rules.mk
-include src/engines/kotor/gui/loadscreen/rules.mk
