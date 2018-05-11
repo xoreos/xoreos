f5155885ce7f868e73b2a9632e6730cd93a08eae
Vsevolod Kremianskii <vkremianskii@gmail.com>
2018-05-11 17:36:01 +0700

    TESTS: Undefine SDL main macro for Google Test
    Otherwise main gets replaced with SDL_main which leads to tests
    compilation errors on Windows

--- tests/googletest/src/gtest_main.cc
+++ tests/googletest/src/gtest_main.cc
@@ -31,6 +31,8 @@
 
 #include "gtest/gtest.h"
 
+#undef main // negate -Dmain=SDL_main
+
 GTEST_API_ int main(int argc, char **argv) {
   printf("Running main() from gtest_main.cc\n");
   testing::InitGoogleTest(&argc, argv);
