1c9386907ae8c3f54dc21760e5ea1434d29b6d9e
Vsevolod Kremianskii <vkremianskii@gmail.com>
2018-05-17 21:45:17 +0700

    BUILD: Include GLM library
    Include files were modified to silence compiler warnings about variable
    shadowing

---
+++ glm/rules.mk
@@ -0,0 +1,58 @@
+# xoreos - A reimplementation of BioWare's Aurora engine
+#
+# xoreos is the legal property of its developers, whose names
+# can be found in the AUTHORS file distributed with this source
+# distribution.
+#
+# xoreos is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# xoreos is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with xoreos. If not, see <http://www.gnu.org/licenses/>.
+
+# GLM (OpenGL Mathematics) 0.9.8.5 (<https://glm.g-truc.net/>)
+
+noinst_HEADERS += \
+    glm/common.hpp \
+    glm/exponential.hpp \
+    glm/ext.hpp \
+    glm/fwd.hpp \
+    glm/geometric.hpp \
+    glm/glm.hpp \
+    glm/integer.hpp \
+    glm/mat2x2.hpp \
+    glm/mat2x3.hpp \
+    glm/mat2x4.hpp \
+    glm/mat3x2.hpp \
+    glm/mat3x3.hpp \
+    glm/mat3x4.hpp \
+    glm/mat4x2.hpp \
+    glm/mat4x3.hpp \
+    glm/mat4x4.hpp \
+    glm/matrix.hpp \
+    glm/packing.hpp \
+    glm/trigonometric.hpp \
+    glm/vec2.hpp \
+    glm/vec3.hpp \
+    glm/vec4.hpp \
+    glm/vector_relational.hpp \
+    $(EMPTY)
+
+EXTRA_DIST += \
+    glm/README.xoreos \
+    glm/copying.txt \
+    $(EMPTY)
+
+# Subdirectories
+
+include glm/detail/rules.mk
+include glm/gtc/rules.mk
+include glm/gtx/rules.mk
+include glm/simd/rules.mk
