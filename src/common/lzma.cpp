0c3abcf6662045678cff9442dcc279f3fd9ac875
Sven Hesse <drmccoy@drmccoy.de>
2018-05-18 14:47:24 +0200

    COMMON: Fix leak in decompressLZMA1()
    The decoded filter options aren't free'd by any of the liblzma
    functions.

--- src/common/lzma.cpp
+++ src/common/lzma.cpp
@@ -58,7 +58,8 @@ byte *decompressLZMA1(const byte *data, size_t inputSize, size_t outputSize) {
 	inputSize -= propsSize;
 
 	lzma_stream strm = LZMA_STREAM_INIT;
-	BOOST_SCOPE_EXIT( (&strm) ) {
+	BOOST_SCOPE_EXIT( (&strm) (&filters) ) {
+		free(filters[0].options);
 		lzma_end(&strm);
 	} BOOST_SCOPE_EXIT_END
 
