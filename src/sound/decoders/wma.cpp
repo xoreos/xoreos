xoreos/xoreos

commit be8e6df182ac4d12a1b43b2e4fd47c74ba5f107b

SOUND: Use the XOREOS_FALLTHROUGH macro to mark fallthroughs

src/sound/decoders/wma.cpp
@@ -1244,10 +1244,10 @@ bool WMACodec::decodeExpHuffman(Common::BitStream &bits, int ch) {
 		int n = *ptr++;
 
 		switch (n & 3) do {
-			case 0: *q++ = iv; // Fallthrough
-			case 3: *q++ = iv; // Fallthrough
-			case 2: *q++ = iv; // Fallthrough
-			case 1: *q++ = iv;
+			case 0: *q++ = iv; XOREOS_FALLTHROUGH;
+			case 3: *q++ = iv; XOREOS_FALLTHROUGH;
+			case 2: *q++ = iv; XOREOS_FALLTHROUGH;
+			case 1: *q++ = iv; break;
 		} while ((n -= 4) > 0);
 
 	} else
@@ -1276,10 +1276,10 @@ bool WMACodec::decodeExpHuffman(Common::BitStream &bits, int ch) {
 		int n = *ptr++;
 
 		switch (n & 3) do {
-			case 0: *q++ = iv; // Fallthrough
-			case 3: *q++ = iv; // Fallthrough
-			case 2: *q++ = iv; // Fallthrough
-			case 1: *q++ = iv;
+			case 0: *q++ = iv; XOREOS_FALLTHROUGH;
+			case 3: *q++ = iv; XOREOS_FALLTHROUGH;
+			case 2: *q++ = iv; XOREOS_FALLTHROUGH;
+			case 1: *q++ = iv; break;
 		} while ((n -= 4) > 0);
 
 	}

commit 5afa29d45da69ef986eeb8ba067bae0674b2691c

SOUND: Fix WMA decoding broken in be8e6df1
Because of Duff's Device, the break would be for the loop, not for the
switch. Putting a break there to shut the compiler up about the
fall-through is wrong. Instead, a fall-through annotation needs to go
before the first case.

src/sound/decoders/wma.cpp
@@ -1244,10 +1244,11 @@ bool WMACodec::decodeExpHuffman(Common::BitStream &bits, int ch) {
 		int n = *ptr++;
 
 		switch (n & 3) do {
+			XOREOS_FALLTHROUGH;
 			case 0: *q++ = iv; XOREOS_FALLTHROUGH;
 			case 3: *q++ = iv; XOREOS_FALLTHROUGH;
 			case 2: *q++ = iv; XOREOS_FALLTHROUGH;
-			case 1: *q++ = iv; break;
+			case 1: *q++ = iv;
 		} while ((n -= 4) > 0);
 
 	} else
@@ -1276,10 +1277,11 @@ bool WMACodec::decodeExpHuffman(Common::BitStream &bits, int ch) {
 		int n = *ptr++;
 
 		switch (n & 3) do {
+			XOREOS_FALLTHROUGH
 			case 0: *q++ = iv; XOREOS_FALLTHROUGH;
 			case 3: *q++ = iv; XOREOS_FALLTHROUGH;
 			case 2: *q++ = iv; XOREOS_FALLTHROUGH;
-			case 1: *q++ = iv; break;
+			case 1: *q++ = iv;
 		} while ((n -= 4) > 0);
 
 	}