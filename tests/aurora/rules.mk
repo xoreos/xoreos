02dcaa96c22bfa8c5f28c8503a1db124901807a8
patrick <nostritius@googlemail.com>
2018-06-20 15:52:59 +0200

    TESTS: Add unit tests for the ERF writer

--- tests/aurora/rules.mk
+++ tests/aurora/rules.mk
@@ -160,3 +160,8 @@ check_PROGRAMS                         += tests/aurora/test_actionscript
 tests_aurora_test_actionscript_SOURCES  = tests/aurora/actionscript.cpp
 tests_aurora_test_actionscript_LDADD    = $(aurora_LIBS)
 tests_aurora_test_actionscript_CXXFLAGS = $(test_CXXFLAGS)
+
+check_PROGRAMS                      += tests/aurora/test_erfwriter
+tests_aurora_test_erfwriter_SOURCES  = tests/aurora/erfwriter.cpp
+tests_aurora_test_erfwriter_LDADD    = $(aurora_LIBS)
+tests_aurora_test_erfwriter_CXXFLAGS = $(test_CXXFLAGS)
