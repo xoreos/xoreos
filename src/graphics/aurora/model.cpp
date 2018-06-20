85b97f6f2a4e7dc2af42de71b7ab0e4c8f369604
mirv <mirv.sillyfish@gmail.com>
2018-06-20 22:21:49 +0100

    GRAPHICS: create absolute position on finalize
    On call to finalize in Model, make sure createAbsolutePosition is
    called to ensure transformation matrices are up to date.

--- src/graphics/aurora/model.cpp
+++ src/graphics/aurora/model.cpp
@@ -833,6 +833,8 @@ void Model::finalize() {
 
 	AnimationChannelMap::iterator c = _animationChannels.begin();
 	c->second->playDefaultAnimation();
+
+	createAbsolutePosition();
 }
 
 void Model::createStateNamesList(std::list<Common::UString> *stateNames) {
