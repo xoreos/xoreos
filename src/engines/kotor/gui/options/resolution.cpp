f932daec5d503deaef4b36df283cc395c763247f
patrick <nostritius@googlemail.com>
2018-07-10 18:40:27 +0200

    KOTOR: Enable changing of resolutions in kotor menu

--- src/engines/kotor/gui/options/resolution.cpp
+++ src/engines/kotor/gui/options/resolution.cpp
@@ -22,23 +22,68 @@
  *  The graphics resolution menu.
  */
 
+#include "src/common/configman.h"
+#include "src/common/strutil.h"
+#include "src/common/ustring.h"
+
+#include "src/graphics/windowman.h"
+
 #include "src/engines/kotor/gui/options/resolution.h"
 
 #include "src/engines/aurora/kotorjadegui/panel.h"
+#include "src/engines/aurora/kotorjadegui/listbox.h"
 
 namespace Engines {
 
 namespace KotOR {
 
-OptionsResolutionMenu::OptionsResolutionMenu(Console *console) : GUI(console) {
+OptionsResolutionMenu::OptionsResolutionMenu(Console *console) : GUI(console), _newWidth(0), _newHeight(0) {
 	load("optresolution");
 
 	WidgetPanel *guiPanel = getPanel("TGuiPanel");
 	guiPanel->setPosition(-guiPanel->getWidth()/2, -guiPanel->getHeight()/2, 0);
+
+	int currentIndex = -1;
+	std::vector<Graphics::DisplayMode> modes = WindowMan.getDisplayModes();
+
+	// Filter every resolution smaller than 800x600
+	for (size_t i = 0; i < modes.size(); ++i) {
+		bool duplicate = false;
+		for (size_t j = 0; j < _modes.size(); ++j) {
+			if (_modes[j].w == modes[i].w && _modes[j].h == modes[i].h)
+				duplicate = true;
+		}
+
+		if (modes[i].w >= 800 && modes[i].h >= 600 && !duplicate) {
+			_modes.push_back(modes[i]);
+		}
+	}
+
+	WidgetListBox *listBox = getListBox("LB_RESOLUTIONS");
+	listBox->createItemWidgets(_modes.size());
+	for (size_t i = 0; i < _modes.size(); ++i) {
+		Graphics::DisplayMode mode = _modes[i];
+		listBox->addItem(Common::composeString(mode.w) + " x " + Common::composeString(mode.h));
+	}
+
+	listBox->setAdjustHeight(true);
+	listBox->setItemSelectionEnabled(true);
+	listBox->refreshItemWidgets();
 }
 
 void OptionsResolutionMenu::callbackActive(Widget &widget) {
-	// TODO: Add changing of resolutions
+	if (widget.getTag() == "BTN_OK") {
+		const int index = getListBox("LB_RESOLUTIONS")->getSelectedIndex();
+
+		if (index >= 0 && static_cast<size_t>(index) < _modes.size()) {
+			WindowMan.setWindowSize(_modes[index].w, _modes[index].h);
+			ConfigMan.setInt("width", _modes[index].w);
+			ConfigMan.setInt("height", _modes[index].h);
+		}
+
+		_returnCode = kReturnCodeAbort;
+		return;
+	}
 
 	if (widget.getTag() == "BTN_CANCEL") {
 		_returnCode = kReturnCodeAbort;
