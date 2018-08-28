4f024b2e93c5d87e039e6e39be1863ba28ea0b40
Vsevolod Kremianskii <vkremianskii@gmail.com>
2018-08-28 21:49:29 +0700

    WITCHER: Store object id, not pointer in delayed actions

--- src/engines/witcher/module.h
+++ src/engines/witcher/module.h
@@ -35,6 +35,8 @@
 
 #include "src/aurora/ifofile.h"
 
+#include "src/aurora/nwscript/objectref.h"
+
 #include "src/events/types.h"
 
 #include "src/engines/witcher/objectcontainer.h"
@@ -142,8 +144,8 @@ private:
 		Common::UString script;
 
 		Aurora::NWScript::ScriptState state;
-		Aurora::NWScript::Object *owner;
-		Aurora::NWScript::Object *triggerer;
+		Aurora::NWScript::ObjectReference owner;
+		Aurora::NWScript::ObjectReference triggerer;
 
 		uint32 timestamp;
 
