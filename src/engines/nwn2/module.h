47d6ef16945c952352a6172390610e288251157b
Vsevolod Kremianskii <vkremianskii@gmail.com>
2018-08-28 21:24:54 +0700

    NWN2: Store object id, not pointer in delayed actions

--- src/engines/nwn2/module.h
+++ src/engines/nwn2/module.h
@@ -36,6 +36,8 @@
 
 #include "src/aurora/ifofile.h"
 
+#include "src/aurora/nwscript/objectref.h"
+
 #include "src/events/types.h"
 
 #include "src/engines/nwn2/objectcontainer.h"
@@ -134,8 +136,8 @@ private:
 		Common::UString script;
 
 		Aurora::NWScript::ScriptState state;
-		Aurora::NWScript::Object *owner;
-		Aurora::NWScript::Object *triggerer;
+		Aurora::NWScript::ObjectReference owner;
+		Aurora::NWScript::ObjectReference triggerer;
 
 		uint32 timestamp;
 
