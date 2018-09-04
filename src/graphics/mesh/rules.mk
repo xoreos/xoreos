0679bcab307b4b00a34974a33cfdbc755ca4f6f6
mirv <mirv.sillyfish@gmail.com>
2018-09-04 10:57:45 +0700

    GRAPHICS: Add MeshFont and MeshQuad classes

--- src/graphics/mesh/rules.mk
+++ src/graphics/mesh/rules.mk
@@ -26,10 +26,14 @@ src_graphics_mesh_libmesh_la_SOURCES += \
     src/graphics/mesh/mesh.h \
     src/graphics/mesh/meshman.h \
     src/graphics/mesh/meshwirebox.h \
+    src/graphics/mesh/meshfont.h \
+    src/graphics/mesh/meshquad.h \
     $(EMPTY)
 
 src_graphics_mesh_libmesh_la_SOURCES += \
     src/graphics/mesh/mesh.cpp \
     src/graphics/mesh/meshman.cpp \
     src/graphics/mesh/meshwirebox.cpp \
+    src/graphics/mesh/meshfont.cpp \
+    src/graphics/mesh/meshquad.cpp \
     $(EMPTY)
