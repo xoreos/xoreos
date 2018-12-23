97ea507f35f69c47f1326ce023c2dc2ce800d524
Sven Hesse <drmccoy@drmccoy.de>
2018-12-23 21:50:16 +0100

    SOUND: Partially implement ASCII XACT SoundBank loading

--- src/sound/xactsoundbank_ascii.cpp
+++ src/sound/xactsoundbank_ascii.cpp
@@ -22,15 +22,169 @@
  *  An ASCII XACT SoundBank, found in the non-Xbox versions of Jade Empire as _xsb.txt files.
  */
 
+#include "src/common/strutil.h"
+#include "src/common/readstream.h"
+#include "src/common/streamtokenizer.h"
+
 #include "src/sound/xactsoundbank_ascii.h"
 
 namespace Sound {
+
+XACTSoundBank_ASCII::Command::Command(const std::vector<Common::UString> &parameters) {
+	if (parameters.size() > 0) {
+		command = parameters[0];
+	}
+}
+
 
 XACTSoundBank_ASCII::XACTSoundBank_ASCII(Common::SeekableReadStream &xsb) {
 	load(xsb);
 }
 
-void XACTSoundBank_ASCII::load(Common::SeekableReadStream &UNUSED(xsb)) {
+static Common::UString stripZeros(Common::UString str) {
+	while ((str.size() > 1) && (*str.begin() == '0'))
+		str.erase(str.begin());
+
+	return str;
+}
+
+static int64 getNumber(Common::UString str) {
+	int64 number = 0;
+
+	Common::parseString(stripZeros(str), number);
+
+	return number;
+}
+
+static size_t getAmount(Common::UString str) {
+	size_t amount = 0;
+
+	Common::parseString(stripZeros(str), amount);
+
+	return amount;
+}
+
+static size_t getAmount(Common::StreamTokenizer &tokenizer, Common::SeekableReadStream &stream) {
+	return getAmount(tokenizer.getToken(stream));
+}
+
+void XACTSoundBank_ASCII::load(Common::SeekableReadStream &xsb) {
+	Common::StreamTokenizer tokenizer(Common::StreamTokenizer::kRuleIgnoreAll);
+	tokenizer.addSeparator(' ');
+	tokenizer.addChunkEnd('\n');
+	tokenizer.addIgnore('\r');
+	tokenizer.addQuote('\"');
+
+	_name = tokenizer.getToken(xsb);
+	tokenizer.nextChunk(xsb);
+
+	const size_t categoryCount = getAmount(tokenizer, xsb);
+	tokenizer.nextChunk(xsb);
+
+	_categories.resize(categoryCount);
+	for (Categories::iterator it = _categories.begin(); it != _categories.end(); ++it) {
+		it->name = tokenizer.getToken(xsb);
+		tokenizer.nextChunk(xsb);
+	}
+
+	const size_t soundCount = getAmount(tokenizer, xsb);
+	tokenizer.nextChunk(xsb);
+
+	_sounds.resize(soundCount);
+	for (Sounds::iterator sound = _sounds.begin(); sound != _sounds.end(); ++sound) {
+		std::vector<Common::UString> tokens;
+		tokenizer.getTokens(xsb, tokens, 2);
+		tokenizer.nextChunk(xsb);
+
+		sound->name = tokens[0];
+		sound->category = 0;
+
+		const int64 categoryIndex = getNumber(tokens[1]);
+		if ((categoryIndex > 0) && (static_cast<size_t>(categoryIndex) < _categories.size()))
+			sound->category = &_categories[categoryIndex];
+
+		_soundMap[sound->name] = &*sound;
+
+		size_t waveCount = 0;
+
+		while (!xsb.eos()) {
+			tokenizer.getTokens(xsb, tokens);
+			tokenizer.nextChunk(xsb);
+
+			if (tokens.empty())
+				break;
+
+			if ((tokens.size() == 1) && (tokens[0].size() == 3)) {
+				waveCount = getAmount(tokens[0]);
+				break;
+			}
+
+			sound->commands.push_back(Command(tokens));
+		}
+
+		sound->waves.resize(waveCount);
+		for (Waves::iterator wave = sound->waves.begin(); wave != sound->waves.end(); ++wave) {
+			tokenizer.getTokens(xsb, tokens, 3);
+			tokenizer.nextChunk(xsb);
+
+			wave->name = tokens[1];
+			wave->bank = tokens[0];
+
+			wave->index = getNumber(tokens[2]);
+		}
+	}
+
+	const size_t cueCount = getAmount(tokenizer, xsb);
+	tokenizer.nextChunk(xsb);
+
+	_cues.resize(cueCount);
+	for (Cues::iterator cue = _cues.begin(); cue != _cues.end(); ++cue) {
+		std::vector<Common::UString> tokens;
+		tokenizer.getTokens(xsb, tokens, 3);
+		tokenizer.nextChunk(xsb);
+
+		cue->name = tokens[0];
+		const bool hasTransitions = tokens[2] == "1";
+
+		const size_t soundRefCount = getAmount(tokenizer, xsb);
+		tokenizer.nextChunk(xsb);
+
+		cue->sounds.resize(soundRefCount);
+		for (SoundRefs::iterator soundRef = cue->sounds.begin(); soundRef != cue->sounds.end(); ++soundRef) {
+			tokenizer.getTokens(xsb, tokens, 4);
+			tokenizer.nextChunk(xsb);
+
+			soundRef->soundName = tokens[1];
+			soundRef->soundIndex = getNumber(tokens[1]);
+
+			soundRef->state = getNumber(tokens[3]);
+
+			cue->states[soundRef->state] = &*soundRef;
+		}
+
+		if (!hasTransitions)
+			continue;
+
+		const size_t transitionCount = getAmount(tokenizer, xsb);
+		tokenizer.nextChunk(xsb);
+
+		cue->transitions.resize(transitionCount);
+		for (Transitions::iterator transition = cue->transitions.begin(); transition != cue->transitions.end(); ++transition) {
+			tokenizer.getTokens(xsb, tokens, 8);
+			tokenizer.nextChunk(xsb);
+
+			transition->from = tokens[0];
+			transition->to = tokens[1];
+
+			transition->style = tokens[3];
+
+			transition->parameters[0] = tokens[2];
+			transition->parameters[1] = tokens[4];
+			transition->parameters[2] = tokens[5];
+			transition->parameters[3] = tokens[6];
+			transition->parameters[4] = tokens[7];
+		}
+	}
 }
 
 } // End of namespace Sound
--- src/sound/xactsoundbank_ascii.h
+++ src/sound/xactsoundbank_ascii.h
@@ -25,6 +25,9 @@
 #ifndef SOUND_XACTSOUNDBANK_ASCII_H
 #define SOUND_XACTSOUNDBANK_ASCII_H
 
+#include <vector>
+#include <map>
+
 #include "src/common/types.h"
 #include "src/common/ustring.h"
 
@@ -44,7 +47,83 @@ public:
 	const Common::UString &getName() const { return _name; }
 
 private:
+	struct Category {
+		Common::UString name;
+	};
+
+	struct Wave {
+		Common::UString name;
+		Common::UString bank;
+
+		size_t index;
+	};
+
+	typedef std::vector<Wave> Waves;
+
+	struct Command {
+		Common::UString command;
+
+		Command(const std::vector<Common::UString> &parameters);
+	};
+
+	typedef std::vector<Command> Commands;
+
+	struct Sound {
+		Common::UString name;
+
+		Category *category;
+
+		Commands commands;
+		Waves waves;
+	};
+
+	struct SoundRef {
+		Common::UString soundName;
+		size_t soundIndex;
+
+		size_t state;
+	};
+
+	typedef std::vector<SoundRef> SoundRefs;
+
+	struct Transition {
+		Common::UString from;
+		Common::UString to;
+
+		Common::UString style;
+
+		Common::UString parameters[5];
+	};
+
+	typedef std::vector<Transition> Transitions;
+	typedef std::map<size_t, SoundRef *> States;
+
+	struct Cue {
+		Common::UString name;
+
+		SoundRefs sounds;
+		States states;
+
+		Transitions transitions;
+	};
+
+	typedef std::vector<Category> Categories;
+	typedef std::vector<Sound> Sounds;
+	typedef std::vector<Cue> Cues;
+
+	typedef std::map<Common::UString, Sound *> SoundMap;
+	typedef std::map<Common::UString, Cue *> CueMap;
+
+
 	Common::UString _name;
+
+	Categories _categories;
+	Sounds _sounds;
+	Cues _cues;
+
+	SoundMap _soundMap;
+	CueMap _cueMap;
+
 
 	void load(Common::SeekableReadStream &xsb);
 };
