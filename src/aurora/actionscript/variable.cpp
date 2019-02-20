03c537ac74cc4de83ecea032572dbca8734fb89f
patrick <nostritius@googlemail.com>
2019-02-20 14:38:25 +0100

    ACTIONSCRIPT: On + return a string if one of the variables is a string

--- src/aurora/actionscript/variable.cpp
+++ src/aurora/actionscript/variable.cpp
@@ -234,7 +234,7 @@ Variable Variable::operator-(Variable v) {
 Variable Variable::operator+(Variable v) {
 	if (v._type == kTypeNumber && _type == kTypeNumber)
 		return asNumber() + v.asNumber();
-	else if (v._type == kTypeString && _type == kTypeString)
+	else if (v._type == kTypeString || _type == kTypeString)
 		return asString() + v.asString();
 	else
 		return 0.0;
