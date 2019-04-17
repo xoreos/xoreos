1ad6ea771ae1e4eaadfaebd7e1206617dfc1f8db
patrick <nostritius@googlemail.com>
2019-04-17 23:17:21 +0200

    KOTORBASE: Add faction loading for creatures

--- src/engines/kotorbase/creature.cpp
+++ src/engines/kotorbase/creature.cpp
@@ -306,6 +306,9 @@ void Creature::loadProperties(const Aurora::GFF3Struct &gff) {
 
 	_minOneHitPoint = gff.getBool("Min1HP", _minOneHitPoint);
 
+	// Faction
+	_faction = Faction(gff.getUint("FactionID"));
+
 	// Scripts
 	readScripts(gff);
 
