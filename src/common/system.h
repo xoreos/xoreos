b8d60c5c8ab654197eff3b9a1a93ef739dbd60aa
Matthew Hoops <clone2727@gmail.com>
2024-11-11 09:11:42 -0500

    COMMON: Remove legacy MSVC [v]snprintf macro definition

--- src/common/system.h
+++ src/common/system.h
@@ -64,33 +64,6 @@
 
 	#define FORCEINLINE __forceinline
 
-	#if _MSC_VER < 1900
-		#define snprintf c99_snprintf
-		#define vsnprintf c99_vsnprintf
-
-		static FORCEINLINE int c99_vsnprintf(char *str, size_t size, const char *format, va_list ap) {
-			int count = -1;
-
-			if (size != 0)
-				count = _vsnprintf_s(str, size, _TRUNCATE, format, ap);
-			if (count == -1)
-				count = _vscprintf(format, ap);
-
-			return count;
-		}
-
-		static FORCEINLINE int c99_snprintf(char *str, size_t size, const char *format, ...) {
-			int count;
-			va_list ap;
-
-			va_start(ap, format);
-			count = c99_vsnprintf(str, size, format, ap);
-			va_end(ap);
-
-			return count;
-		}
-	#endif
-
 	#if !defined(XOREOS_LITTLE_ENDIAN) && !defined(XOREOS_BIG_ENDIAN)
 		#define XOREOS_LITTLE_ENDIAN 1
 	#endif
